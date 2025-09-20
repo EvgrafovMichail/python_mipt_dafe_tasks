a2a7b01bfdefb4de77f4af7cbc60bfb1a066b867
Max-Gosling <137826179+Max-Gosling@users.noreply.github.com>
2025-09-20 09:43:38 +0300

    Update task4.py

--- solutions/lesson02/task4.py
+++ solutions/lesson02/task4.py
@@ -1,4 +1,17 @@
 def get_multiplications_amount(num: int) -> int:
     multiplications_amount = 0
-    # ваш код
+    while num > 1:
+        
+        if num % 2 == 0:
+            multiplications_amount += 1
+            num //= 2
+        
+        else:
+            multiplications_amount += 1
+            num -= 1
+        
     return multiplications_amount
+
+
+for i in range(1, 1001):
+    print(i, get_multiplications_amount(i))
