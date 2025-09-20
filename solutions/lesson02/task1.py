1a35d9e2ed0f9f66240676f37adbaa1eb5bb54f9
Vyacheslav A <aleksakhin.viu@phystech.edu>
2025-09-20 13:33:17 +0300

    Solution for task 1

--- solutions/lesson02/task1.py
+++ solutions/lesson02/task1.py
@@ -1,4 +1,10 @@
 def get_factorial(num: int) -> int:
-    factorial = 1
-    # ваш код
-    return factorial
+    
+    factorial_of_num = 1
+
+    for i in range(1, num + 1):
+        factorial_of_num *= i
+
+    
+    return factorial_of_num
+    
