5a754250ba192f83895dc17b1b926ada93e7c37c
Poco-dev <vueprojector5@gmail.com>
2025-09-20 12:36:41 +0300

    task2

--- solutions/lesson02/task2.py
+++ solutions/lesson02/task2.py
@@ -1,7 +1,9 @@
 def get_doubled_factorial(num: int) -> int:
     factorial = 1
-    if num>1:
-        return num*get_doubled_factorial(num-2)
-    if num<=1:
-        return 1
+    for i in range(num,1,-2):
+        if i>1:
+            factorial*=i
+        if i<=1:
+            factorial *= 1
     return factorial
+print(get_doubled_factorial(4))
