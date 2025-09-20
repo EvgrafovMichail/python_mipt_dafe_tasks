b8f438fb1f8bd3494c0ec83b203f92e1ee058899
Serge <yanchevskiy07@gmail.com>
2025-09-20 11:33:59 +0300

    solution 1

--- solutions/lesson02/task1.py
+++ solutions/lesson02/task1.py
@@ -1,4 +1,7 @@
-def get_factorial(num: int) -> int:
-    factorial = 1
-    # ваш код
-    return factorial
+num = 5
+factorial_of_num = 1
+
+for i in range(1, num + 1):
+    factorial_of_num *= i
+
+print(f"{factorial_of_num = }")
