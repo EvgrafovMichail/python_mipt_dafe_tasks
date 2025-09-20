be77ea28cdd2828a93d59accdbf8e3d084a544cb
Denis1325 <penkovdenis271@gmail.com>
2025-09-20 13:42:06 +0300

    sol1

--- solutions/lesson02/task3.py
+++ solutions/lesson02/task3.py
@@ -1,4 +1,9 @@
 def get_amount_of_ways_to_climb(stair_amount: int) -> int:
     step_prev, step_curr = 1, 1
-    # ваш код
+
+    stairs = [1, 1, 2]
+    for i in range(2, stair_amount):
+        stairs.append(stairs[-1] + stairs[-2])
+    step_curr = stairs[stair_amount]
+
     return step_curr
--- solutions/lesson02/task5.py
+++ solutions/lesson02/task5.py
@@ -1,3 +1,10 @@
 def get_gcd(num1: int, num2: int) -> int:
-    # ваш код
+
+    num1, num2 = min(num1, num2), max(num1, num2)
+    while num2 % num1 != 0:
+        num2, num1 = num1, num2 % num1
+
     return num1
+
+
+print(get_gcd(75, 125))
