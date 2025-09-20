692b4154428c0c3d2a255b2d0ae9c353f4a11124
Poco-dev <vueprojector5@gmail.com>
2025-09-20 11:51:36 +0300

    7zadacha

--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -1,5 +1,7 @@
 def is_palindrome(num: int) -> bool:
     num_reversed = 0
     num_origin = num
-    # ваш код
+    if str(num)==str(num)[::-1]:
+        return True
+    return False
     return num_origin == num_reversed
