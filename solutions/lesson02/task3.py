cc71ac9a06a41306addd22eb4231a54c4a05098c
Poco-dev <vueprojector5@gmail.com>
2025-09-20 11:47:36 +0300

    Update task3.py

--- solutions/lesson02/task3.py
+++ solutions/lesson02/task3.py
@@ -1,4 +1,8 @@
 def get_amount_of_ways_to_climb(stair_amount: int) -> int:
     step_prev, step_curr = 1, 1
-    
+    for i in range(stair_amount-1):
+        temp = step_curr
+        step_curr += step_prev
+        step_prev = temp
     return step_curr
+print(get_amount_of_ways_to_climb(3))
