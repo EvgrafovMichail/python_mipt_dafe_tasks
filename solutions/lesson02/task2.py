9e026efed81a592dbb8485d3658436ca9745c3e0
kalium-cyanide <mrrrosssi@gmail.com>
2025-09-20 13:44:51 +0300

    tak2

--- solutions/lesson02/task2.py
+++ solutions/lesson02/task2.py
@@ -1,4 +1,11 @@
 def get_doubled_factorial(num: int) -> int:
+        
     factorial = 1
-    # ваш код
+    
+    for n in range(1, num + 1):
+        
+        if n % 2 != num % 2: continue
+        print(n)
+        factorial *= n
+        
     return factorial
