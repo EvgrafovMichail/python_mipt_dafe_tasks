6054e39ef56698c6d3b7683c7f810fee55760159
Акбулатов Артур Фаилевич <akbulatov_2007@mail.ru>
2025-09-20 15:45:17 +0300

    Сделал функцию вычисления двойного факториала

--- solutions/lesson02/task2.py
+++ solutions/lesson02/task2.py
@@ -1,4 +1,4 @@
 def get_doubled_factorial(num: int) -> int:
-    factorial = 1
-    # ваш код
-    return factorial
+    if num <= 1: return 1
+    if num > 1: return num * get_doubled_factorial(num - 2)
+print(get_doubled_factorial(4))
