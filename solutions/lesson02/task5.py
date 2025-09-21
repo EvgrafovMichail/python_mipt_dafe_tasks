f5aab90a14d4696e1d4c08085e7bb3d7dc6198d3
MechMine <rozomazan069@gmail.com>
2025-09-21 11:43:23 +0300

    Update task5.py

--- solutions/lesson02/task5.py
+++ solutions/lesson02/task5.py
@@ -1,3 +1,9 @@
 def get_gcd(num1: int, num2: int) -> int:
-    # ваш код
-    return num1
+    
+    if num1>num2:
+        num1,num2=num2,num1
+    
+    while num1>0 and num2>0:
+        num1,num2=num2%num1,num1
+    
+    return num2
