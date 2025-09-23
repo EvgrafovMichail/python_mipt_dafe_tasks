f1e5445563bb5f4a4ff8812ff2bf00832255a3e3
varlag <varya_lagutina@mail.ru>
2025-09-23 11:05:38 +0000

    task 4

--- solutions/lesson02/task4.py
+++ solutions/lesson02/task4.py
@@ -1,4 +1,11 @@
-def get_multiplications_amount(num: int) -> int:
-    multiplications_amount = 0
-    # ваш код
-    return multiplications_amount
+def get_multiplications_amount(num: int, multiplications_amount = 0) -> int:
+    if num == 1: 
+        return multiplications_amount
+    else: 
+        if num % 2 == 0: 
+            return get_multiplications_amount(num // 2, multiplications_amount + 1)
+        else:
+            return get_multiplications_amount(num - 1, multiplications_amount + 1)
+        
+print(get_multiplications_amount(10))
+
