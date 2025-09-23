a052661fd950a97151500d0b56222e40e388dd8c
Vladimir <vl.mukhin21@gmail.com>
2025-09-24 01:54:08 +0300

    Сделано задание 2

--- solutions/lesson02/task2.py
+++ solutions/lesson02/task2.py
@@ -1,8 +1,8 @@
 def get_doubled_factorial(num: int) -> int:
     double_fact_of_num = 1
-    
     pre_fact = 1
     pre_pre_fact = 1
+    
     for n in range(2, num + 1):
         double_fact_of_num = n * pre_pre_fact
         pre_pre_fact = pre_fact
