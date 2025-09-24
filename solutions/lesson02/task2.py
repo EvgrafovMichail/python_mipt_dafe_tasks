cd0de2064d5b0ae0c629fe566b911c75cc261864
Kaitashi <vaich18022888@gmail.com>
2025-09-25 02:19:34 +0300

    По аналогии с 1 таском сделал факториал, только для н-2

--- solutions/lesson02/task2.py
+++ solutions/lesson02/task2.py
@@ -1,4 +1,12 @@
+num = int(input())#опять же хз, но если это поставить похзже, то оно не работает
+#в целом тут просто n(n-2)(n-4) и т.д. поэтому ограничить надо ток 1 и 0, а остальное с условием моим вроде норм работает
 def get_doubled_factorial(num: int) -> int:
-    factorial = 1
-    # ваш код
-    return factorial
+    factorial = 1 
+    if num <= 1:
+        return 1 
+    else: 
+        while 0<=num-2:
+            factorial *= num
+            num = num - 2
+    return factorial 
+print(get_doubled_factorial(num))
