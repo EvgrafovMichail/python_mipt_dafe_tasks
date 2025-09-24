0634672676fe2298bf2b01eb9859927f0c7c49e8
Vyacheslav A <aleksakhin.viu@phystech.edu>
2025-09-24 18:43:11 +0300

    Solution for task 7

--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -2,4 +2,9 @@ def is_palindrome(num: int) -> bool:
     num_reversed = 0
     num_origin = num
     # ваш код
+    num_origin = str(num)
+
+    num_reversed = num_origin[::-1]
+
+
     return num_origin == num_reversed
