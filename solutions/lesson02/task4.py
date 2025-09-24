1a9682b141cb0f775a3f101355c3d5bf31ec5d92
kiritonator <kir.shishkin.shkolnik@yandex.ru>
2025-09-25 00:29:16 +0300

    Do the fourth task

--- solutions/lesson02/task4.py
+++ solutions/lesson02/task4.py
@@ -1,4 +1,10 @@
 def get_multiplications_amount(num: int) -> int:
     multiplications_amount = 0
-    # ваш код
-    return multiplications_amount
+    while num > 0:
+        if num%2 == 0:
+            num = num//2
+            multiplications_amount += 1
+        else:
+            num = num - 1
+            multiplications_amount += 1
+    return multiplications_amount-1
