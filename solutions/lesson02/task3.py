387781ff7395f4d8aec5a0921e4b112bf31123d9
Shanechka <grisacernyh@gmail.com>
2025-09-24 10:25:54 +0300

    Commented task 3

--- solutions/lesson02/task3.py
+++ solutions/lesson02/task3.py
@@ -7,4 +7,5 @@ def get_amount_of_ways_to_climb(stair_amount: int) -> int:
             temp = step_curr
             step_curr = step_curr + step_prev
             step_prev = temp
+            #Stair amount equals to element with index n+1 of fibonacci sequence
     return step_curr
