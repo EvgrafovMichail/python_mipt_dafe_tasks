1a0b369e14b39687cbb8bcd583165deb79cb83e0
Vyacheslav A <aleksakhin.viu@phystech.edu>
2025-09-24 19:24:33 +0300

    Solution for task 4

--- solutions/lesson02/task4.py
+++ solutions/lesson02/task4.py
@@ -1,4 +1,14 @@
 def get_multiplications_amount(num: int) -> int:
     multiplications_amount = 0
     # ваш код
+    while (num != 1):
+        if num % 2 != 0:
+            multiplications_amount += 2
+            num = (num - 1) / 2
+
+        else:
+            multiplications_amount += 1
+            num = num / 2
+
+
     return multiplications_amount
