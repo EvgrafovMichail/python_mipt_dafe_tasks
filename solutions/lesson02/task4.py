545d1919e0b3cb33d979a95115f5c65ae77b7ef7
Kodik Igor <ikodik34@gmail.com>
2025-09-24 17:27:39 +0300

    get_multiplications_amount

--- solutions/lesson02/task4.py
+++ solutions/lesson02/task4.py
@@ -1,4 +1,10 @@
-def get_multiplications_amount(num: int) -> int:
+def get_multiplications_amount(deg: int) -> int:
     multiplications_amount = 0
-    # ваш код
+    while deg > 1:
+        if deg % 2 == 0:
+            multiplications_amount += 1
+            deg = deg//2
+        else:
+            multiplications_amount += 1
+            deg -= 1
     return multiplications_amount
