37fe6208bc5891aacd3c5d14d933e0da1a51f552
Kaitashi <vaich18022888@gmail.com>
2025-09-25 02:55:23 +0300

    добавил решение задачи через цикл вайл и услоавие >1

--- solutions/lesson02/task4.py
+++ solutions/lesson02/task4.py
@@ -1,4 +1,12 @@
+num = int(input())
 def get_multiplications_amount(num: int) -> int:
-    multiplications_amount = 0
-    # ваш код
+    multiplications_amount = 0 #я сначала не понимал, что с 1 делать, но у нас изначально 0 и можно просто >1 в условие поставить
+    while num >1:
+        if num%2 == 0:
+            multiplications_amount +=1
+            num = num/2
+        else:
+            num = num -1
+            multiplications_amount +=1     
     return multiplications_amount
+print(get_multiplications_amount(num))
