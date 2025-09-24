880d400dabccccffd937ad84041264237f7277ba
Vyacheslav A <aleksakhin.viu@phystech.edu>
2025-09-24 17:20:31 +0300

    Solution for task 5, final version

--- solutions/lesson02/task5.py
+++ solutions/lesson02/task5.py
@@ -1,3 +1,19 @@
 def get_gcd(num1: int, num2: int) -> int:
     # ваш код
+    if num2 > num1:
+
+        while (num2 % num1 != 0):
+            remainder = num2 % num1
+            num2 = num1
+            num1 = remainder
+
+    elif num1 > num2:
+
+        while (num1 % num2 != 0):
+            remainder = num1 % num2
+            num1 = num2
+            num2 = remainder
+        num1 = num2    
+
+
     return num1
