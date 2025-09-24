27fa8706116d8b5591d05919e85503b4beab48be
Vladimir <vl.mukhin21@gmail.com>
2025-09-25 00:19:25 +0300

    Сделано задание 7

--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -1,5 +1,5 @@
 def is_palindrome(num: int) -> bool:
-    num_reversed = 0
-    num_origin = num
-    # ваш код
+    num_origin = str(num)
+    num_reversed = num_origin[::-1]
+
     return num_origin == num_reversed
