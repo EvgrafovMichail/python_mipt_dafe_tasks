857b68bc296528a62bafa86a0824dc0d84ef5a54
Vladimir <vl.mukhin21@gmail.com>
2025-09-24 23:25:50 +0300

    Сделано задание 5

--- solutions/lesson02/task5.py
+++ solutions/lesson02/task5.py
@@ -1,3 +1,15 @@
 def get_gcd(num1: int, num2: int) -> int:
-    # ваш код
-    return num1
+    while min(num1, num2) > 1:
+        if num1 > num2:
+            ost = num1 % num2
+            if ost == 0:
+                return num2
+            else:
+                num1 = ost
+        else:
+            ost = num2 % num1
+            if ost == 0:
+                return num1
+            else:
+                num2 = ost
+    return 1
