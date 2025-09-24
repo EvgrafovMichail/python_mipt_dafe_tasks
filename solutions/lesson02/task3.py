a50a8837625b2a338cb48c96eb39b4222b3a9282
kiritonator <kir.shishkin.shkolnik@yandex.ru>
2025-09-24 22:50:37 +0300

    Do third task

--- solutions/lesson02/task3.py
+++ solutions/lesson02/task3.py
@@ -1,4 +1,9 @@
 def get_amount_of_ways_to_climb(stair_amount: int) -> int:
-    step_prev, step_curr = 1, 1
-    # ваш код
-    return step_curr
+    steps = [0] * (stair_amount + 1)
+    steps[0] = 1
+    steps[1] = 1
+
+    for i in range(2, stair_amount + 1):
+        steps[i] = steps[i - 1] + steps[i - 2]
+    return steps[stair_amount]
+
