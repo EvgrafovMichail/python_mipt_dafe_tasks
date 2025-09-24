6e704845bea66da6d0ee9ab7bb85ea1ba9251e3f
Vladimir <vl.mukhin21@gmail.com>
2025-09-25 00:54:17 +0300

    Сделано задание 6

--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -1,4 +1,14 @@
 def get_sum_of_prime_divisors(num: int) -> int:
     sum_of_divisors = 0
-    # ваш код
+    i = 2
+    while i < num + 1:
+        f = False
+
+        while num % i == 0:
+            if f == False:
+                f = True
+                sum_of_divisors += i
+            num //= i
+        i+=1
+        
     return sum_of_divisors
