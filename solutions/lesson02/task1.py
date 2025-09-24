0cb0259b056f9c132815f9416e8184e1dd687935
Kaitashi <vaich18022888@gmail.com>
2025-09-25 01:54:29 +0300

    Добавлен цикл, решающий задачу с факториалом

--- solutions/lesson02/task1.py
+++ solutions/lesson02/task1.py
@@ -1,4 +1,7 @@
+num = int(input())#Я вписал это до кода ибо иначе это не работает, я хз почему
 def get_factorial(num: int) -> int:
     factorial = 1
-    # ваш код
+    for i in range(1, num+1):
+        factorial *=i
     return factorial
+print(get_factorial(num))
