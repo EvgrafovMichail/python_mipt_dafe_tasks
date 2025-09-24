6f85e96afbad960005699d2bf72e47e4fdcff075
Kodik Igor <ikodik34@gmail.com>
2025-09-24 17:47:50 +0300

    is_palindrome

--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -1,5 +1,6 @@
 def is_palindrome(num: int) -> bool:
-    num_reversed = 0
-    num_origin = num
-    # ваш код
-    return num_origin == num_reversed
+    if str(num) == str(num)[::-1]:
+        return True
+    else:
+        return False
+print(is_palindrome(13))
