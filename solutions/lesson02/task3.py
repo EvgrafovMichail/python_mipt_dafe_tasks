00770d43d79e60423bc7269afb42240ff2c9e4ec
varlag <varya_lagutina@mail.ru>
2025-09-25 18:11:55 +0000

    modified task3

--- solutions/lesson02/task3.py
+++ solutions/lesson02/task3.py
@@ -1,11 +1,10 @@
 def get_amount_of_ways_to_climb(stair_amount: int) -> int:
     step_prev, step_curr, step_sum = 1, 2, 0
-    if stair_amount < 4: 
+
+    if stair_amount < 4:
         return stair_amount
-    
     for step in range(stair_amount - 2):
         step_sum = step_curr + step_prev
         step_prev = step_curr
         step_curr = step_sum
     return step_sum
-
