14d631181669a4329a1f715a794d2ef99ce83d0a
Kaitashi <vaich18022888@gmail.com>
2025-09-25 05:04:53 +0300

    Добавлен алгоритм определения просытх делителей и отбоор одинаковых простых делителей

--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -1,4 +1,15 @@
+num = int(input())#по факту самое важное, что если число составное, то будет множитель до корня из нам(я это вот только что узнал) и то, что 2 покрывае все четные числа(ну это очев)
 def get_sum_of_prime_divisors(num: int) -> int:
+
     sum_of_divisors = 0
-    # ваш код
+    if num%2 ==0:
+        sum_of_divisors+=2 
+    x =3
+    while x**2 <=num:
+        if num%x == 0:
+            sum_of_divisors += x
+            while num%x == 0:
+                num/=x
+        x = x+2
     return sum_of_divisors
+print(get_sum_of_prime_divisors(num))
