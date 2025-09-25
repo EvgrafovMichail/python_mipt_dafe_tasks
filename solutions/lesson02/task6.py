8895428a3210faa0ea366aad215e1d24f5a6cdd6
BogdanYeti <unrelise_original@mail.ru>
2025-09-25 18:39:30 +0300

    Formatted all solutions

--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -1,13 +1,13 @@
 def get_sum_of_prime_divisors(num: int) -> int:
     sum_of_divisors = 0
-    for i in range(2, int(num ** 0.5) + 1):
+    for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
-            for j in range(2, int(i ** 0.5) + 1):
+            for j in range(2, int(i**0.5) + 1):
                 if i % j == 0:
                     break
             else:
                 sum_of_divisors += i
-    for i in range(2, int(num ** 0.5) + 1):
+    for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
             break
     else:
