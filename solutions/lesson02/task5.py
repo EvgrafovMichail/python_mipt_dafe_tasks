87f1dd99697aba00a0d832e319b90d27f3a16e4c
U3SU <u3.su@yandex.ru>
2025-09-25 15:52:32 +0300

    таск5

--- solutions/lesson02/task5.py
+++ solutions/lesson02/task5.py
@@ -1,3 +1,11 @@
 def get_gcd(num1: int, num2: int) -> int:
-    # ваш код
+    if num1 == 0 or num2 == 0:
+        num1 = max(num1, num2)
+    elif num2 >= num1:
+        while num2 % num1 != 0:
+            num1 = num2 % num1
+    elif num1 >= num2:
+        while num1 % num2 != 0:
+            num2 = num1 % num2
+        num1 = num2
     return num1
