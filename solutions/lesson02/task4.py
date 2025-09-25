504da059c0e82a3bee66647fa7683b4ef0af7a72
Kaitashi <104273157+Vaa1S@users.noreply.github.com>
2025-09-25 06:11:38 +0300

    Update task4.py

--- solutions/lesson02/task4.py
+++ solutions/lesson02/task4.py
@@ -1,12 +1,16 @@
 num = int(input())
+
+
 def get_multiplications_amount(num: int) -> int:
-    multiplications_amount = 0 
-    while num >1:
-        if num%2 == 0:
-            multiplications_amount +=1
-            num = num/2
+    multiplications_amount = 0
+    while num > 1:
+        if num % 2 == 0:
+            multiplications_amount += 1
+            num = num / 2
         else:
-            num = num -1
-            multiplications_amount +=1     
+            num = num - 1
+            multiplications_amount += 1
     return multiplications_amount
 
+
+print(get_multiplications_amount(num))
