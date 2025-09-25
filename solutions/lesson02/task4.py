3883a4ec9c45f7fafe860a8367a35ebca205ae2a
U3SU <u3.su@yandex.ru>
2025-09-25 15:05:23 +0300

    таск 4

--- solutions/lesson02/task4.py
+++ solutions/lesson02/task4.py
@@ -1,4 +1,11 @@
 def get_multiplications_amount(num: int) -> int:
     multiplications_amount = 0
-    # ваш код
+    i = 0
+    while num > 1:
+        if num % 2 == 0:
+            num = num//2
+            multiplications_amount += 1
+        else:
+            num = num - 1
+            multiplications_amount += 1
     return multiplications_amount
