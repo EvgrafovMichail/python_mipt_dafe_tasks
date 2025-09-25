06f230eee9d626194d908fa03734613831202522
danberx <danberx490@gmail.com>
2025-09-25 18:23:44 +0300

    task7 without string

--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -1,6 +1,10 @@
 def is_palindrome(num: int) -> bool:
     if num < 0:
         return False
-    num_reversed = int(str(num)[::-1])
+    num_reversed = 0
     num_origin = num
+    while num != 0:
+        num_reversed *= 10
+        num_reversed += num % 10
+        num //= 10
     return num_origin == num_reversed
