a5158465c8a51b3540b6223d3603853169b438a5
Shanechka <grisacernyh@gmail.com>
2025-09-26 01:52:35 +0300

    Homework 26.09.2025 no lists fix

--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -1,24 +1,13 @@
-def IsPrime(num: int) -> bool:
-    d = 2
-    while num % d != 0:
-        d += 1
-    return d == num
-
-
 def get_sum_of_prime_divisors(num: int) -> int:
+    n = num
+    if n == 1:
+        return 0
     sum_of_divisors = 0
-    divisors = []
-    i = 2
-    while num != 1:
+    for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
-            if IsPrime(i):
-                divisors.append(i)
+            sum_of_divisors += i
+            while num % i == 0:
                 num //= i
-                i = 2
-
-        else:
-            i += 1
-    divisors = set(divisors)
-    for i in divisors:
-        sum_of_divisors += i
+    if sum_of_divisors == 0:
+        return n
     return sum_of_divisors
--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -1,7 +1,12 @@
 def is_palindrome(num: int) -> bool:
     if num < 0:
         return False
-    num_reversed = str(abs(num))
-    num_reversed = num_reversed[::-1]
-    num_reversed = int(num_reversed)
+    num_reversed = 0
+    num_reversed += num % 10
+    num_current = num
+    num_current //= 10
+    while num_current > 0:
+        num_reversed *= 10
+        num_reversed += num_current % 10
+        num_current //= 10
     return num == num_reversed
