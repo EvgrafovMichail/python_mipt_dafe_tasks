188772382eb970958d3965b12e483f64960d938f
Kaitashi <104273157+Vaa1S@users.noreply.github.com>
2025-09-25 06:12:37 +0300

    Update task6.py

--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -1,14 +1,18 @@
 num = int(input())
-def get_sum_of_prime_divisors(num: int) -> int:
 
+
+def get_sum_of_prime_divisors(num: int) -> int:
     sum_of_divisors = 0
-    if num%2 ==0:
-        sum_of_divisors+=2 
-    x =3
-    while x**2 <=num:
-        if num%x == 0:
+    if num % 2 == 0:
+        sum_of_divisors += 2
+    x = 3
+    while x**2 <= num:
+        if num % x == 0:
             sum_of_divisors += x
-            while num%x == 0:
-                num/=x
-        x = x+2
+            while num % x == 0:
+                num /= x
+        x = x + 2
     return sum_of_divisors
+
+
+print(get_sum_of_prime_divisors(num))
