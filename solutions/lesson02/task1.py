d0e8e7ef19f82f2333e3ff0fcb262f03c0014206
Kaitashi <104273157+Vaa1S@users.noreply.github.com>
2025-09-25 06:16:37 +0300

    Update task1.py

--- solutions/lesson02/task1.py
+++ solutions/lesson02/task1.py
@@ -1,11 +1,5 @@
-num = int(input())
-
-
 def get_factorial(num: int) -> int:
     factorial = 1
     for i in range(1, num + 1):
         factorial *= i
     return factorial
-
-
-print(get_factorial(num))
