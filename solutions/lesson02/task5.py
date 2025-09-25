15f488266e0a73d875519d97f48b98029ac7d509
Kaitashi <104273157+Vaa1S@users.noreply.github.com>
2025-09-25 06:12:09 +0300

    Update task5.py

--- solutions/lesson02/task5.py
+++ solutions/lesson02/task5.py
@@ -1,15 +1,20 @@
-num1=int(input())
-num2=int(input())
+num1 = int(input())
+num2 = int(input())
+
+
 def get_gcd(num1: int, num2: int) -> int:
-    if num1>num2:
-        a=num1
-        b=num2
+    if num1 > num2:
+        a = num1
+        b = num2
     else:
-        a=num2
-        b=num1
-    while a%b!=0:
-        c=a%b
-        a=b
-        b=c
+        a = num2
+        b = num1
+    while a % b != 0:
+        c = a % b
+        a = b
+        b = c
     num1 = b
-    return num1 
+    return num1
+
+
+print(get_gcd(num1, num2))
