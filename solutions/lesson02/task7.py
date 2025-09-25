7e1f2b30d40b509cfcc956238fd76d98f956cee1
Poco-dev <vueprojector5@gmail.com>
2025-09-25 18:25:58 +0300

    Update task7.py

--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -1,4 +1,8 @@
 def is_palindrome(num: int) -> bool:
-    num_reversed = str(num)[::-1]
-    num_origin = str(num)
+    num_origin = num
+    num_reversed = 0
+    while num>0:
+        num_reversed *= 10
+        num_reversed += num%10
+        num = num//10
     return num_origin == num_reversed
