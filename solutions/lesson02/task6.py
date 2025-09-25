df1c2152e218f862da54fc83d384b4aa19d3df67
melanikot <kotova.mv@phystech.edu>
2025-09-25 15:47:04 +0300

    solutions sem2

--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -7,9 +7,13 @@ def get_sum_of_prime_divisors(num: int) -> int:
     while i * i <= temporary:
         if temporary % i == 0:
             sum_of_divisors += i
+
             while temporary % i == 0:
                 temporary /= i
+
         i += 1
+
     if temporary > 1:
         sum_of_divisors += temporary
+
     return sum_of_divisors
