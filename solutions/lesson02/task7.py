0371452b9f00e7a23a948afbdfa9d18aa7fc0b4a
Kaitashi <vaich18022888@gmail.com>
2025-09-25 07:29:03 +0300

    fix roof

--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -3,12 +3,12 @@ def is_palindrome(num: int) -> bool:
         return False
     if num < 10:
         return True
-    
+
     num_reversed = 0
     original_num = num
-    
+
     while num > 0:
         num_reversed = num_reversed * 10 + num % 10
         num //= 10
-    
+
     return original_num == num_reversed
