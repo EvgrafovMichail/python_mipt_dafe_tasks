521db613a8bcfc8c0b6ee309739cf09680635095
Denis1325 <penkovdenis271@gmail.com>
2025-09-25 17:41:16 +0300

    sol2

--- solutions/lesson02/task3.py
+++ solutions/lesson02/task3.py
@@ -1,9 +1,9 @@
 def get_amount_of_ways_to_climb(stair_amount: int) -> int:
     step_prev, step_curr = 1, 1
 
-    stairs = [1, 1, 2]
-    for i in range(2, stair_amount):
-        stairs.append(stairs[-1] + stairs[-2])
-    step_curr = stairs[stair_amount]
+    for i in range(1, stair_amount):
+        step_next = step_prev + step_curr
+        step_prev = step_curr
+        step_curr = step_next
 
     return step_curr
