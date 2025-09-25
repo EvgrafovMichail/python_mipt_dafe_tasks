1ede8c378c95d37ada69e9115702e3de7c8d8185
Kaitashi <vaich18022888@gmail.com>
2025-09-25 07:02:35 +0300

    fix all and new fork

--- solutions/lesson02/task5.py
+++ solutions/lesson02/task5.py
@@ -1,3 +1,13 @@
 def get_gcd(num1: int, num2: int) -> int:
-    # ваш код
+    if num1 > num2:
+        a = num1
+        b = num2
+    else:
+        a = num2
+        b = num1
+    while a % b != 0:
+        c = a % b
+        a = b
+        b = c
+    num1 = b
     return num1
--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -1,5 +1,14 @@
 def is_palindrome(num: int) -> bool:
+    if num < 0:
+        return False
+    if num < 10:
+        return True
+    
     num_reversed = 0
-    num_origin = num
-    # ваш код
-    return num_origin == num_reversed
+    original_num = num
+    
+    while num > 0:
+        num_reversed = num_reversed * 10 + num % 10
+        num //= 10
+    
+    return original_num == num_reversed
