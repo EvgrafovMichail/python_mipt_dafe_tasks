a7860e34660704610c80ac96e39865ec7643a3a9
melanikot <kotova.mv@phystech.edu>
2025-09-25 15:36:42 +0300

    Trying to make git work~

--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -1,17 +1,15 @@
 def get_sum_of_prime_divisors(num: int) -> int:
     sum_of_divisors = 0
-    prime = True
 
-    if num == 1:
-        get_sum_of_prime_divisors = 0
-    else:
-        for i in range(2, num + 1):
-            prime = True
-            if num % i == 0:
-                for j in range(2, i):
-                    if i % j == 0:
-                        prime = False
-                        break
-                if prime:
-                    sum_of_divisors += i
+    i = 2
+    temporary = num
+
+    while i * i <= temporary:
+        if temporary % i == 0:
+            sum_of_divisors += i
+            while temporary % i == 0:
+                temporary /= i
+        i += 1
+    if temporary > 1:
+        sum_of_divisors += temporary
     return sum_of_divisors
