887e0c812ab7ffd5930071ff893bbba82dc33a23
dandandil <daniil182403@gmail.com>
2025-09-25 22:29:21 +0000

    formatted t1, added t34567

--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -1,4 +1,18 @@
+def is_prime(n: int) -> bool:
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return 0
+    return 1
+
+
 def get_sum_of_prime_divisors(num: int) -> int:
     sum_of_divisors = 0
-    # ваш код
+    if is_prime(num) and num != 1:
+        return num
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            if is_prime(i):
+                sum_of_divisors += i
+            if is_prime(num // i):
+                sum_of_divisors += num // i
     return sum_of_divisors
--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -1,5 +1,22 @@
+def chlen(n):
+    if n == 0:
+        return 1
+    else:
+        cnt = 0
+        while n:
+            n //= 10
+            cnt += 1
+        return cnt
+
+
 def is_palindrome(num: int) -> bool:
     num_reversed = 0
     num_origin = num
-    # ваш код
+    if num < 0:
+        return 0
+    a = chlen(num)
+    while a > 0:
+        num_reversed += (num % 10) * (10 ** (a - 1))
+        num //= 10
+        a -= 1
     return num_origin == num_reversed
