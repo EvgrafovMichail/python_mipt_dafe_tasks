03d10b1968912c6ebab9a48137a0b9cd94f9f7cb
Denis1325 <penkovdenis271@gmail.com>
2025-09-25 17:50:19 +0300

    sol4

--- solutions/lesson02/task5.py
+++ solutions/lesson02/task5.py
@@ -1,5 +1,4 @@
 def get_gcd(num1: int, num2: int) -> int:
-
     num1, num2 = min(num1, num2), max(num1, num2)
     while num2 % num1 != 0:
         num2, num1 = num1, num2 % num1
--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -2,7 +2,7 @@ def is_prime(num: int) -> int:
     if num <= 1:
         return False
     else:
-        for i in range(2, int(num**(1/2)) + 1):
+        for i in range(2, int(num ** (1 / 2)) + 1):
             if num % i == 0:
                 return False
     return True
@@ -11,9 +11,9 @@ def is_prime(num: int) -> int:
 def get_sum_of_prime_divisors(num: int) -> int:
     sum_of_divisors = 0
 
-    for i in range(1, int(num**(1/2)) + 1):
+    for i in range(1, int(num ** (1 / 2)) + 1):
         if num % i == 0:
-            sum_of_divisors += i*is_prime(i)
-            sum_of_divisors += (num//i)*is_prime(num//i)
+            sum_of_divisors += i * is_prime(i)
+            sum_of_divisors += (num // i) * is_prime(num // i)
 
     return sum_of_divisors
