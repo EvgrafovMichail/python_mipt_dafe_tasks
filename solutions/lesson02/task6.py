9280218306a9bce5bba76e19181cb30638ac10cc
Poco-dev <vueprojector5@gmail.com>
2025-09-25 18:31:39 +0300

    changed

--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -11,3 +11,4 @@ def get_sum_of_prime_divisors(num: int) -> int:
                 sum_of_divisors+=num
             break
     return sum_of_divisors
+    
--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -6,3 +6,4 @@ def is_palindrome(num: int) -> bool:
         num_reversed += num%10
         num = num//10
     return num_origin == num_reversed
+    
