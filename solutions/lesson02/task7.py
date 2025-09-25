c3cfaf7a9daf7921dfb08af69105500b7656ecf0
Kaitashi <104273157+Vaa1S@users.noreply.github.com>
2025-09-25 06:20:23 +0300

    Update task7.py

--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -1,14 +1,14 @@
-num = int(input())
-
 def is_palindrome(num: int) -> bool:
+    if num < 0:
+        return False
+    if num < 10:
+        return True
+    
     num_reversed = 0
     num_origin = num
-    if num < 10:
-        num_origin = num_reversed
-    elif num < 0:
-        num_origin != num_reversed
-    else:
-        while num > 0:
-            num_reversed = num_reversed * 10 + num % 10
-            num //= 10
+    
+    while num > 0:
+        num_reversed = num_reversed * 10 + num % 10
+        num //= 10
+    
     return num_origin == num_reversed
