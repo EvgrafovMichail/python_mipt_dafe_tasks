6e0174f7be6e77b4aff519e23194c4a7203aff17
kirill <qwerty.0045@inbox.ru>
2025-09-25 23:36:45 +0300

    hometask2

--- solutions/lesson02/task3.py
+++ solutions/lesson02/task3.py
@@ -1,4 +1,11 @@
 def get_amount_of_ways_to_climb(stair_amount: int) -> int:
-    step_prev, step_curr = 1, 1
-    # ваш код
+    step_prev = 1
+    step_curr = 1
+    t = 0
+    i = 1
+    while i < stair_amount:
+        t = step_curr
+        step_curr = step_prev + t
+        step_prev = t
+        i += 1
     return step_curr
