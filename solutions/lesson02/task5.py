2b7a7c39a1a0b5f325243a0198b5076815de4f7c
melanikot <kotova.mv@phystech.edu>
2025-09-25 12:03:34 +0300

    Trying to make git work

--- solutions/lesson02/task5.py
+++ solutions/lesson02/task5.py
@@ -1,3 +1,10 @@
 def get_gcd(num1: int, num2: int) -> int:
-    # ваш код
-    return num1
+    x, y = num1, num2
+
+    while max(x, y) % min(x, y) != 0 and y != 0:
+        temporary_val = x
+        x = min(x, y)
+        y = max(temporary_val, y) % min(temporary_val, y)
+
+    gcd = min(x, y)
+    return gcd
--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -1,4 +1,17 @@
 def get_sum_of_prime_divisors(num: int) -> int:
     sum_of_divisors = 0
-    # ваш код
+    prime = True
+
+    if num == 1:
+        get_sum_of_prime_divisors = 0
+    else:
+        for i in range(2, num + 1):
+            prime = True
+            if num % i == 0:
+                for j in range(2, i):
+                    if i % j == 0:
+                        prime = False
+                        break
+                if prime:
+                    sum_of_divisors += i
     return sum_of_divisors
