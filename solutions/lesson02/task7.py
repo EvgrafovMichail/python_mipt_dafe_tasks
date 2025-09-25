ccb6e88991b13b874d1a6331fc11e6478da42293
Kaitashi <vaich18022888@gmail.com>
2025-09-25 04:38:02 +0300

    добавлен алгоритм определяющий политропные числа

--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -1,5 +1,14 @@
+num=int(input()) # задание звучит легко, его вроде через строку просто решить можно, но до этого алгоритма я часа 2 пытался додуматься
 def is_palindrome(num: int) -> bool:
     num_reversed = 0
     num_origin = num
-    # ваш код
+    if num <10:
+        num_origin=num_reversed
+    elif num<0:
+        num_origin!=num_reversed
+    else: 
+        while num>0: 
+            num_reversed = num_reversed*10 + num%10 # по факту алгоритм просто берет последнее цифру числа сдвигает до этого собранные числа влево и опять берет последнее число, отбрасывая ушедшее
+            num//=10 
     return num_origin == num_reversed
+print(is_palindrome(num))
