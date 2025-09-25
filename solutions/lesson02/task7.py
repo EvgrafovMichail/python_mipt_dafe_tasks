a5a305e630effffc557c5140ac92472c3b6689fd
Kaitashi <104273157+Vaa1S@users.noreply.github.com>
2025-09-25 05:35:43 +0300

    Update task7.py

--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -1,4 +1,4 @@
-num=int(input()) # задание звучит легко, его вроде через строку просто решить можно, но до этого алгоритма я часа 2 пытался додуматься
+num=int(input()) 
 def is_palindrome(num: int) -> bool:
     num_reversed=0
     num_origin=num
@@ -8,7 +8,7 @@ def is_palindrome(num: int) -> bool:
         num_origin!=num_reversed
     else: 
         while num>0: 
-            num_reversed=num_reversed*10+num%10 # по факту алгоритм просто берет последнее цифру числа сдвигает до этого собранные числа влево и опять берет последнее число, отбрасывая ушедшее
+            num_reversed=num_reversed*10+num%10 
             num//=10 
     return num_origin==num_reversed
 print(is_palindrome(num))
