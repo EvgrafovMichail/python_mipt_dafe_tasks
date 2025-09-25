84d26adf014de574ee3348f8b4c4c113ed3512c0
Kaitashi <vaich18022888@gmail.com>
2025-09-25 12:01:36 +0300

    Fix task 3

--- solutions/lesson02/task3.py
+++ solutions/lesson02/task3.py
@@ -1,7 +1,18 @@
+stair_amount = int(input())
+
+
 def get_amount_of_ways_to_climb(stair_amount: int) -> int:
     if stair_amount <= 1:
         return 1
     step_prev, step_curr = 1, 1
-    for _ in range(2, stair_amount + 1):
-        step_prev, step_curr = step_curr, step_prev + step_curr
+    for i in range(2, stair_amount + 1):
+        old_step_prev = step_prev
+        old_step_curr = step_curr
+        new_step_prev = old_step_curr
+        new_step_curr = old_step_prev + old_step_curr
+        step_prev = new_step_prev
+        step_curr = new_step_curr
     return step_curr
+
+
+print(get_amount_of_ways_to_climb(stair_amount))
