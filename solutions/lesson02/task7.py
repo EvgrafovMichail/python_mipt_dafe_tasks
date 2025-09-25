9e4d585db9b3f07a58af3159e14842f1315510d5
varlag <varya_lagutina@mail.ru>
2025-09-25 21:19:51 +0000

    task7

--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -1,6 +1,17 @@
-def is_palindrome(num: int) -> bool:
-    num = str((num))
-    for i in range(len(num) // 2 + 1):
-        if num[i] != num[len(num) - i - 1]:
+def len_num(num):
+    cnt = 0
+    while num > 0:
+        num //= 10
+        cnt += 1
+    return cnt
+
+def is_palindrome(num):
+    if num < 0: 
+        return False
+    n = len_num(num)
+    while num > 0 and n > 0:
+        if num // 10 ** (n - 1) % 10 != num % 10: 
             return False
+        num //= 10
+        n -= 2
     return True
