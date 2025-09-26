9c8f9d4248ed2591506c26dc3490f13663ce451d
Robert <robert.ibr0407@gmail.com>
2025-09-26 23:33:11 +0300

    Hello, it is my homework

--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -1,5 +1,11 @@
 def is_palindrome(num: int) -> bool:
     num_reversed = 0
     num_origin = num
-    # ваш код
+
+    temp = num
+    while temp > 0:
+        digit = temp % 10
+        num_reversed = num_reversed * 10 + digit
+        temp = temp // 10
+
     return num_origin == num_reversed
