46ac18726851290b76e207037024570f576832a5
Victory <kostrykova.v.a.12@gmail.com>
2025-09-26 08:57:00 +0300

    Кострюкова Урок 2 тот

--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -1,7 +1,12 @@
 def is_palindrome(num: int) -> bool:
-    num_reversed = 0
-    num_origin = num
-    s = str(num)
-    num_origin = s  
-    num_reversed = s[::-1]
-    return num_origin == num_reversed
+    if num < 0:
+        return False
+    if num < 10:
+        return True
+    reversed_num = 0
+    num_copy = num
+    while num > 0:
+        digit = num % 10
+        reversed_num = reversed_num * 10 + digit
+        num = num // 10
+    return num_copy == reversed_num
