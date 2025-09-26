5ba2872bc275212e305c301bf73c0ffc8d804026
kiritonator <kir.shishkin.shkolnik@yandex.ru>
2025-09-26 20:02:12 +0300

    Do 3 task 100%

--- solutions/lesson02/task3.py
+++ solutions/lesson02/task3.py
@@ -2,7 +2,6 @@ def get_amount_of_ways_to_climb(stair_amount: int) -> int:
     prev_step = 1
     before_prev_step = 1
     if stair_amount == 1:
-
         return 1
     else:
         for i in range(2, stair_amount + 1):
