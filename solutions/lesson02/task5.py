9128e25e51f022fba8b617ea5287aac8d440883d
Kemal <f9ka9@mail.ru>
2025-09-26 22:32:33 +0300

    Homework_lesson2

--- solutions/lesson02/task5.py
+++ solutions/lesson02/task5.py
@@ -1,3 +1,7 @@
 def get_gcd(num1: int, num2: int) -> int:
-    # ваш код
+    while num2 != 0:
+        temp = num2
+        num2 = num1%num2
+        num1=temp
     return num1
+    
--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -1,5 +1,13 @@
 def is_palindrome(num: int) -> bool:
-    num_reversed = 0
-    num_origin = num
-    # ваш код
-    return num_origin == num_reversed
+        if num < 0:
+            return False
+
+        num_reversed = 0
+        num_origin = num
+        
+        while num>0 :
+            num_reversed=num_reversed*10+num%10
+            num//=10
+        return num_reversed==num_origin
+
+
