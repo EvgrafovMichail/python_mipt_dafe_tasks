fd1f78a6a8f30792d1045bd67340e3af6ad22ba3
Prosto_Max_xX <maxim.nevinsky@yandex.ru>
2025-09-26 18:05:41 +0300

    Remade task 6

--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -1,16 +1,18 @@
 def get_sum_of_prime_divisors(num: int) -> int:
     sum_of_divisors = 0
     for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0 and num_is_simple(i):
-            sum_of_divisors += i
+        if num % i == 0:
+            for j in range(2, int(i ** 0.5) + 1):
+                if i % j == 0:
+                    break
+            else:
+                sum_of_divisors += i
+            i2 = num // i
+            for k in range(2, int(i2 ** 0.5) + 1):
+                if i2 % k == 0:
+                    break
+            else:
+                sum_of_divisors += i2
     if sum_of_divisors == 0 and num != 1:
         sum_of_divisors += num
     return sum_of_divisors
-
-
-def num_is_simple(num: int) -> bool:
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    else:
-        return True
