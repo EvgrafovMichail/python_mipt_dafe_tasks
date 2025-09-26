ce84c75ba6026c1018fce211cbf684cb0557fe38
kiritonator <kir.shishkin.shkolnik@yandex.ru>
2025-09-26 21:13:44 +0300

    Do all tasks

--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -1,14 +1,22 @@
 def get_sum_of_prime_divisors(num: int) -> int:
     sum_of_divisors = 0
-    deliteli = []
-    for i in range(2, num + 1):
+    k = 0
+    if num <= 1:
+        return 0
+    for i in range(2, num):
         if num % i == 0:
-            deliteli.append(i)
-    for i in range(len(deliteli)):
-        for j in range(2, deliteli[i]):
-            if deliteli[i] % j == 0:
-                deliteli.pop(i)
-    for i in range(len(deliteli)):
-        sum_of_divisors += deliteli[i]
+            k = 1
+            break
+    if k == 0:
+        sum_of_divisors = num
+    for i in range(2, int(num**0.5) + 1):
+        c = 0
+        if num % i == 0:
+            for j in range(2, int(i**0.5) + 1):
+                if i % j == 0:
+                    c = 1
+                    break
+            if c == 0:
+                sum_of_divisors += i
 
     return sum_of_divisors
