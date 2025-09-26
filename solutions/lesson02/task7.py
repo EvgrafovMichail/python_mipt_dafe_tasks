e7da3b6ad961731bb58290a2e2dbbcf70331044c
Prosto_Max_xX <maxim.nevinsky@yandex.ru>
2025-09-26 17:51:31 +0300

    Remade code format again...

--- solutions/lesson02/task7.py
+++ solutions/lesson02/task7.py
@@ -1,6 +1,6 @@
 def is_palindrome(num: int) -> bool:
     num_origin = num
-    if num < 0: 
+    if num < 0:
         return False
     length = 0
     num_test = num
@@ -10,6 +10,6 @@ def is_palindrome(num: int) -> bool:
     num_reversed = 0
     num_test = num
     for i in range(1, length + 1):
-        num_reversed += num_test % 10  * 10 ** (length - i)
+        num_reversed += num_test % 10 * 10 ** (length - i)
         num_test //= 10
     return num_origin == num_reversed
