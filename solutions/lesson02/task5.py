c14be7b3687c4e6317587405a86bfd96b781311e
Prosto_Max_xX <maxim.nevinsky@yandex.ru>
2025-09-26 19:50:32 +0300

    Fix endless while

--- solutions/lesson02/task5.py
+++ solutions/lesson02/task5.py
@@ -1,10 +1,9 @@
 def get_gcd(num1: int, num2: int) -> int:
     while True:
-        if num2 > num1:
+        if num2 >= num1:
             if num2 % num1 == 0:
                 return num1
             else:
                 num2 = num2 % num1
         else:
             num2, num1 = num1, num2
-    return num2
