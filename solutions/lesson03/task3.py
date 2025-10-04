7f435cee829ee3ab8c5f521de137471bf890517a
danberx <danberx490@gmail.com>
2025-10-04 11:20:03 +0300

    task3.py changed

--- solutions/lesson03/task3.py
+++ solutions/lesson03/task3.py
@@ -1,9 +1,7 @@
 def get_nth_digit(num: int) -> int:
     if num <= 5:
         return (num - 1) * 2
-    i = 1
     num -= 6
-    res = 0
     for i in range(1, 10):
         cnt = 5 * 10**i * (i + 1) - (i + 1) * 5 * 10 ** (i - 1)
         if num - cnt > 0:
