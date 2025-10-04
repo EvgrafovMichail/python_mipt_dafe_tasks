e8503caa0267a5261c381858eea571c11e28b0e5
dandandil <daniil182403@gmail.com>
2025-10-04 09:34:55 +0000

    edited task6

--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -9,10 +9,14 @@ def get_sum_of_prime_divisors(num: int) -> int:
     sum_of_divisors = 0
     if is_prime(num) and num != 1:
         return num
+    cnt = 0
     for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
+            cnt += 1
             if is_prime(i):
                 sum_of_divisors += i
-            if is_prime(num // i):
-                sum_of_divisors += num // i
+            # if is_prime(num // i):
+            #     sum_of_divisors += num // i
+    if cnt == 1:
+        sum_of_divisors += num // sum_of_divisors 
     return sum_of_divisors
