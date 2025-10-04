8759620f73536b2d684c0bb1504480bfcc12d19d
Vera <verakondurina@gmail.com>
2025-10-04 11:31:47 +0300

    check branch task1_lesson03

--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -4,6 +4,7 @@ def is_prime(num):
     if num <= 1 or num % 2 == 0:
         return False
     for i in range(3, int(num**0.5) + 1, 2):
+        print(num/i)
         if num % i == 0:
             return False
     return True
@@ -13,8 +14,13 @@ def get_sum_of_prime_divisors(num: int) -> int:
     sum_of_divisors = 0
     if num >= 2:
         for i in range(2, int(num**0.5) + 1):
+            print(is_prime(num))
             if num % i == 0 and is_prime(i):
                 sum_of_divisors += i
     if is_prime(num):
         sum_of_divisors += num
     return sum_of_divisors
+
+
+n = int(input())
+print(get_sum_of_prime_divisors(n))
