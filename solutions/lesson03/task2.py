981098724cd7b3e8cbcd1f9eddf8a2151e07b1a0
gubanovily66-beep <gubanovily66@gmail.com>
2025-10-04 23:08:08 +0300

    Lesson 3 tasks

--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -1,3 +1,19 @@
 def get_cube_root(n: float, eps: float) -> float:
-    # ваш код
+    if n == 0:
+        return 0.0
+    if n > 0: 
+        left = 0.0
+        right = max (1.0, n)
+    else:
+        left = min (-1.0, n)
+        right = 0.0 
+    while right - left > eps:
+        mid = (left + right) / 2
+        mid_cub = mid * mid * mid
+        
+    if mid_cub < n:
+        left = mid
+    else:
+         right = mid
+    n =  (left + right) / 2
     return n
--- solutions/lesson03/task3.py
+++ solutions/lesson03/task3.py
@@ -1,3 +1,29 @@
 def get_nth_digit(num: int) -> int:
-    # ваш код
+    if num <= 5:
+        return (num - 1) * 2
+    num -= 5
+    rank = 2 
+    left = 10
+    right = 100
+    total_digits_so_far = 0
+    while True:
+        count = (right - left) // 2
+        total_digits = count * rank
+
+        if total_digits_so_far + total_digits < num:
+            total_digits_so_far += total_digits
+            rank += 1
+            left = right
+            right *= 10
+
+        else:
+            pos_in_rank = num - total_digits_so_far - 1
+            number_index = pos_in_rank // rank
+            digit_pos = pos_in_rank % rank
+            number = left + number_index * 2
+            
+            for _ in range(rank - digit_pos - 1):
+                number //= 10
+            return number % 10
     return 0
+
