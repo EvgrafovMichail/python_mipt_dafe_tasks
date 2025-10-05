3849881d39e42e0848973ec56694b77dba44fbf9
Rafk123 <tabunanov.rr@phystech.edu>
2025-10-05 15:51:50 +0300

    the_second_contest

--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -1,3 +1,23 @@
 def get_cube_root(n: float, eps: float) -> float:
-    # ваш код
-    return n
+    if (abs(n) < 1):
+        left = 0
+        right = abs(n + 1)
+        middle = (left + right) / 2
+    elif (abs(n) > 1):
+        left = 1
+        right = abs(n)
+        middle = (left + right) / 2
+    else:
+        return n
+    
+    while (abs(middle**3 - abs(n)) >= eps):
+        if (middle**3 - abs(n) >= eps):
+            right = middle
+        else:
+            left = middle
+        middle = (right + left) / 2
+
+    if (n != 0):
+        return middle * (n / abs(n))
+    else:
+        return 0
