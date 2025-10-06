1cc11e960989e417dca7a0af7213f5ddae3505f6
Oleg <rozomazan069@gmail.com>
2025-10-06 18:52:49 +0300

    Changed "r" to "right" and "l" to "left"

--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -1,24 +1,24 @@
 def get_cube_root(n: float, eps: float) -> float:
     if n < 0:
         n = -n
-        r = 0
-        while r * r * r < n:
-            r += 1
-        l = 0
-        while r * r * r - n >= eps:
-            if ((r + l) * (r + l) * (r + l) / 8 - n) < 0:
-                l = (r + l) / 2
+        right = 0
+        while right**3 < n:
+            right += 1
+        left = 0
+        while right**3 - n >= eps:
+            if ((right + left) ** 3 / 8 - n) < 0:
+                left = (right + left) / 2
             else:
-                r = (r + l) / 2
+                right = (right + left) / 2
 
-        return -r
-    r = 0
-    while r * r * r < n:
-        r += 1
-    l = 0
-    while r * r * r - n >= eps:
-        if ((r + l) * (r + l) * (r + l) / 8 - n) < 0:
-            l = (r + l) / 2
+        return -right
+    right = 0
+    while right**3 < n:
+        right += 1
+    left = 0
+    while right**3 - n >= eps:
+        if ((right + left) ** 3 / 8 - n) < 0:
+            left = (right + left) / 2
         else:
-            r = (r + l) / 2
-    return r
+            right = (right + left) / 2
+    return right
