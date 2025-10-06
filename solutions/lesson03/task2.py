62a27a5ebb2039bbac6d6f13751eb3ede137797e
Oleg <rozomazan069@gmail.com>
2025-10-06 18:45:21 +0300

    Add solutions for all tasks

--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -1,3 +1,24 @@
 def get_cube_root(n: float, eps: float) -> float:
-    # ваш код
-    return n
+    if n < 0:
+        n = -n
+        r = 0
+        while r * r * r < n:
+            r += 1
+        l = 0
+        while r * r * r - n >= eps:
+            if ((r + l) * (r + l) * (r + l) / 8 - n) < 0:
+                l = (r + l) / 2
+            else:
+                r = (r + l) / 2
+
+        return -r
+    r = 0
+    while r * r * r < n:
+        r += 1
+    l = 0
+    while r * r * r - n >= eps:
+        if ((r + l) * (r + l) * (r + l) / 8 - n) < 0:
+            l = (r + l) / 2
+        else:
+            r = (r + l) / 2
+    return r
