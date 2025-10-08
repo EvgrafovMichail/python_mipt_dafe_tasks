b63f1de6d44454b676285f260a80f49e8b8a8ed2
Vera <verakondurina@gmail.com>
2025-10-08 19:34:34 +0300

    Add solution for task 2

--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -1,3 +1,29 @@
 def get_cube_root(n: float, eps: float) -> float:
-    # ваш код
-    return n
+    if n == 0:
+        return 0.0
+    if n > 0:
+        left = 0.0
+        if n > 1:
+            right = n
+        else:
+            right = 1.0
+    else:
+        right = 0.0
+        if n < -1:
+            left = n
+        else:
+            left = -1.0
+    ave = (right + left) / 2
+    ave_cube = ave * ave * ave
+    while abs(ave_cube - n) >= eps:
+        if ave_cube > n:
+            right = ave
+        else:
+            left = ave
+        ave = (right + left) / 2
+        ave_cube = ave * ave * ave
+    return (right + left) / 2
+
+
+# n, eps = float(input()), float(input())
+# print(get_cube_root(n, eps))
