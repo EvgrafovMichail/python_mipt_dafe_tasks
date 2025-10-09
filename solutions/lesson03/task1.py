b710690323547ab26b24b4ee3c6be19acb896051
Prosto_Max_xX <maxim.nevinsky@yandex.ru>
2025-10-09 16:05:27 +0300

    remade first task again

--- solutions/lesson03/task1.py
+++ solutions/lesson03/task1.py
@@ -7,14 +7,14 @@ def flip_bits_in_range(num: int, left_bit: int, right_bit: int) -> int:
         num //= 2
     num = new_num
     for i in range(right_bit - 1, left_bit - 2, -1):
-        if num // 10 ** i % 10 == 1:
-            num -= 10 ** i
+        if num // 10**i % 10 == 1:
+            num -= 10**i
         else:
-            num += 10 ** i
+            num += 10**i
     new_num = 0
     p = 0
     while num > 0:
-        new_num += num % 10 * 2 ** p
+        new_num += num % 10 * 2**p
         p += 1
         num //= 10
     return new_num
