5a2f5663574ebb555ced50689bac8d799e52e43e
Victory <kostrykova.v.a.12@gmail.com>
2025-10-09 15:22:39 +0300

    tasks

--- solutions/lesson03/task3.py
+++ solutions/lesson03/task3.py
@@ -1,3 +1,24 @@
 def get_nth_digit(num: int) -> int:
-    # ваш код
-    return 0
+    k = 1
+    total_digits = 5
+    while num > total_digits:
+        num -= total_digits
+        k += 1
+        if k == 2:
+            total_digits = 45 * 2
+        else:
+            total_digits = (9 * 10**(k-1) // 2) * k
+    
+    pos = num
+    if k == 1:
+        first = 0
+    else:
+        first = 10**(k-1)
+    
+    num_index = (pos - 1) // k
+    digit_index = (pos - 1) % k
+    number = first + 2 * num_index
+    
+    power = 10**(k - 1 - digit_index)
+    digit = (number // power) % 10
+    return digit
