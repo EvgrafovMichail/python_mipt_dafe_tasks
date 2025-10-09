375101000c8faa1f4e1898e8007c25cb847250e3
Kodik Igor <ikodik34@gmail.com>
2025-10-09 23:42:21 +0300

    improved task2

--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -9,9 +9,9 @@ def get_cube_root(num: float, pogreshnost: float) -> float:
     if num < 0:
         flag = True
     num = abs(num)
-    left = 0
+    left = 0.0
     right = 2 + num // 3
-    while left <= right:
+    while left < right:
         m = (right + left) / 2
         if abs(num - m * m * m) < pogreshnost:
             if flag:
@@ -21,7 +21,3 @@ def get_cube_root(num: float, pogreshnost: float) -> float:
             right = m - pogreshnost / maxi
         elif m * m * m - num < pogreshnost:
             left = m + pogreshnost / maxi
-    if abs(num - m * m * m) < pogreshnost:
-        if flag:
-            return -1 * m
-        return m
