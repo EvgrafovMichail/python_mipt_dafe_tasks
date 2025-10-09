2a36d7235dd2cd234c4420754950647051f6c16c
Vandam-create <beliakov.va@phystech.edu>
2025-10-09 20:41:55 +0300

    1

--- solutions/lesson03/task1.py
+++ solutions/lesson03/task1.py
@@ -1,3 +1,28 @@
 def flip_bits_in_range(num: int, left_bit: int, right_bit: int) -> int:
-    # ваш код
-    return num
+    bit = 0
+    counter = 1
+    result = 0
+
+    while counter <= right_bit:
+        bit = num & 1 if num != 0 else 0
+
+        if right_bit >= counter >= left_bit:
+            bit = 1 - bit
+
+        print(result, bit)
+        result = result + (bit << (counter - 1))
+        print(result)
+        num >>= 1
+        counter += 1
+
+    while num > 0:
+        print(counter, result, bit)
+        bit = num & 1
+        result = result + (bit << (counter - 1))
+        num >>= 1
+        counter += 1
+
+    return result
+
+
+print(flip_bits_in_range(11, 1, 1))
--- solutions/lesson03/task3.py
+++ solutions/lesson03/task3.py
@@ -1,3 +1,21 @@
 def get_nth_digit(num: int) -> int:
-    # ваш код
-    return 0
+    if num > 5:
+        multiplication = 90
+        counter = 2
+        num2 = num - 5
+
+        while num2 > multiplication:
+            counter += 1
+            num2 = num2 - multiplication
+            multiplication = counter * multiplication * 10 / (counter - 1)
+
+        n = (num2 + num2 % counter) // counter
+        res_num = 10 ** (counter - 1) + 2 * (n - 1)
+        if num2 % counter != 0:
+            res_num = int(res_num // (10 ** (counter - num2 % counter))) % 10
+        else:
+            res_num = res_num % 10
+    else:
+        res_num = 2 * (num - 1)
+
+    return res_num
