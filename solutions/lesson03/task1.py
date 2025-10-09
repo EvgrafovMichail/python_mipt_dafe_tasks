f248059a4757e3ab16528560089164ccb9424953
Denis1325 <penkovdenis271@gmail.com>
2025-10-10 02:10:05 +0300

    sol2

--- solutions/lesson03/task1.py
+++ solutions/lesson03/task1.py
@@ -1,3 +1,3 @@
 def flip_bits_in_range(num: int, left_bit: int, right_bit: int) -> int:
-    num ^= 2**(right_bit-left_bit+1)-1 << (left_bit-1)
+    num ^= 2 ** (right_bit - left_bit + 1) - 1 << (left_bit - 1)
     return num
--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -1,11 +1,11 @@
 def get_cube_root(n: float, eps: float) -> float:
-    left, right = (n+1)*(n>0), (n-1)*(n<0)
+    left, right = (n + 1) * (n > 0), (n - 1) * (n < 0)
     target = n
-    n = (left + right)/2
-    while -eps > n*n*n - target or n*n*n - target > eps:
-        if target > n*n*n:
+    n = (left + right) / 2
+    while -eps > n * n * n - target or n * n * n - target > eps:
+        if target > n * n * n:
             right = n
         else:
             left = n
-        n = (left + right)/2
+        n = (left + right) / 2
     return n
