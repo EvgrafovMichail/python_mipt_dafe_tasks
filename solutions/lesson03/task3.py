d690bc095818c731d0009ba243caddff7d85db47
BogdanYeti <unrelise_original@mail.ru>
2025-10-10 00:25:20 +0300

    Finally formatted

--- solutions/lesson03/task3.py
+++ solutions/lesson03/task3.py
@@ -2,7 +2,7 @@ def get_nth_digit(num: int) -> int:
     le = 1
     while True:
         first = 10 ** (le - 1) * int(le != 1)
-        if num <= (b := (10 ** le - first) // 2 * le):
+        if num <= (b := (10**le - first) // 2 * le):
             m = (num - 1) % le
             number = first + 2 * ((num - 1) // le)
             while number >= 0:
