d2a862584f23b379ae2d185ad3c9b74e6d4d5b1d
Provoda78 <nikbel.2007@mail.ru>
2025-10-10 22:35:19 +0300

    Update task1.py

--- solutions/lesson03/task1.py
+++ solutions/lesson03/task1.py
@@ -1,3 +1,5 @@
 def flip_bits_in_range(num: int, left_bit: int, right_bit: int) -> int:
-    # ваш код
+    range = (1 << right_bit - left_bit + 1) - 1
+    mask = range << left_bit - 1
+    num ^= mask
     return num
