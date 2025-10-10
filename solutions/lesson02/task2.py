06ad954bd05af0b891caba5a636353b9f0b04ad1
Акбулатов Артур Фаилевич <akbulatov_2007@mail.ru>
2025-10-10 23:57:07 +0300

    украл задания за вторую неделю

--- solutions/lesson02/task2.py
+++ solutions/lesson02/task2.py
@@ -1,7 +1,10 @@
 def get_doubled_factorial(num: int) -> int:
-    a, b = 2, 1
-    if num == 0 or num == 1:
-        return 1
-    for n in range(3, num + 1):
-        a, b = n * b, a
-    return a
+    factorial = 1
+    f0 = 1  # значение (n-2)!!
+    f1 = 1  # значение (n-1)!!
+    if num > 1:
+        for i in range(2, num + 1):
+            factorial = i * f0
+            f0, f1 = f1, factorial
+
+    return factorial
