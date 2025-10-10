944ba3d50be795dffcc3c7fa89fa015e2ad8d14d
4gera4 <geraskinctepan@gmail.com>
2025-10-10 19:29:34 +0300

    3e задание ГЕраськин

--- solutions/lesson03/task3.py
+++ solutions/lesson03/task3.py
@@ -1,3 +1,43 @@
-def get_nth_digit(num: int) -> int:
-    # ваш код
-    return 0
+def get_nth_digit(num: int):
+    if num == 0:
+        return 0
+    else:
+        num -= 1
+        n = num
+        d = 1 
+
+        while True:
+            start = 10**(d - 1)
+            if start % 2 != 0:
+                start += 1
+            end = 10**d - 1
+            if end % 2 != 0:
+                end -= 1
+
+            if start <= end:
+                count_numbers = ((end - start) // 2) + 1
+                total_digits = count_numbers * d
+            else:
+                total_digits = 0
+
+            if n <= total_digits:
+                index_in_group = (n - 1) // d
+                left = (n - 1) % d               
+                target_number = start + index_in_group * 2
+
+                p = d - 1 - left                
+                power = 1
+                for _ in range(p):
+                    power *= 10                  
+                digit = (target_number // power) % 10
+
+                right = d - left - 1
+
+                # print(target_number)
+                # print(left)
+                # print(right)
+                # print(digit)
+                return digit
+            else:
+                n -= total_digits
+                d += 1
