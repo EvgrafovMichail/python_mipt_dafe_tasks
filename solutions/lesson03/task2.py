7cd2ff81cdf79f3e44c7fa9653f44ece6669cc1c
U3SU <u3.su@yandex.ru>
2025-10-10 19:48:07 +0300

    task1 and task2

--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -1,3 +1,36 @@
 def get_cube_root(n: float, eps: float) -> float:
-    # ваш код
-    return n
+    if n > 0:
+        if n >= 1:
+            left = 0
+            right = n
+        else:
+            left = 0
+            right = 1
+        middle = (right + left) / 2
+        while abs(middle**3 - n) > eps:
+            if middle**3 == n:
+                return middle
+            elif middle**3 < n:
+                left = middle
+            elif middle**3 > n:
+                right = middle
+            middle = (right + left) / 2
+    elif n < 0:
+        if n <= -1:
+            left = n
+            right = 0
+        else:
+            left = -1
+            right = 0
+        middle = (right + left) / 2
+        while abs(middle**3 - n) > eps:
+            if middle**3 == n:
+                return middle
+            elif middle**3 < n:
+                left = middle
+            elif middle**3 > n:
+                right = middle
+            middle = (right + left) / 2
+    elif n == 0:
+        return 0
+    return middle
