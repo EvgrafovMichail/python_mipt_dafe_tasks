83ca4ccbc5d6dd835e6538d9cdda8dfb435a6d3e
Акбулатов Артур Фаилевич <akbulatov_2007@mail.ru>
2025-10-10 23:07:04 +0300

    исправился

--- solutions/lesson02/task2.py
+++ solutions/lesson02/task2.py
@@ -1,6 +1,7 @@
 def get_doubled_factorial(num: int) -> int:
-    a, b = 2, 1  # вычислим 2!!, 1!! и 0!!
-    if num == 0 or num == 1: return 1
+    a, b = 2, 1 
+    if num == 0 or num == 1: 
+        return 1
     for n in range(3, num + 1):
         a, b = n * b, a
     return a
