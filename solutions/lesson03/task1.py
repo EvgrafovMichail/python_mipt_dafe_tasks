7e540dbb03b0e1136a90dba82b4d3962bdb482f5
gvidon-7 <aleksandrlarionov71@icloud.com>
2025-10-10 23:04:37 +0300

    Александр Ларионов Б12-512
    применил команду ruff format к этим двум файлам

--- solutions/lesson03/task1.py
+++ solutions/lesson03/task1.py
@@ -1,5 +1,4 @@
 def flip_bits_in_range(num: int, left_bit: int, right_bit: int) -> int:
-
     mask1 = 1 << left_bit - 1
     mask2 = 1 << right_bit - 1
     extra_bit = 0
@@ -7,12 +6,12 @@ def flip_bits_in_range(num: int, left_bit: int, right_bit: int) -> int:
         maskprom = mask2
     elif right_bit - left_bit - 1 == 1:
         extra_bit = 1 << left_bit
-        maskprom = extra_bit|mask2
+        maskprom = extra_bit | mask2
     else:
         for i in range(left_bit, right_bit):
-            extra_bit|= 1 << i
-        maskprom = extra_bit|mask2
-  
-    maskfin = mask1|maskprom
-    num ^= maskfin 
+            extra_bit |= 1 << i
+        maskprom = extra_bit | mask2
+
+    maskfin = mask1 | maskprom
+    num ^= maskfin
     return num
--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -1,5 +1,4 @@
 def get_cube_root(n: float, eps: float) -> float:
-
     if eps <= 0:
         print("Ошибка Ввода")
         return None
@@ -7,19 +6,19 @@ def get_cube_root(n: float, eps: float) -> float:
         return 0
     if n > 0:
         low = 0
-        high = max(1,n)
+        high = max(1, n)
     else:
         low = min(-1, n)
         high = 0
-    
-    while high - low > eps*eps*eps:
+
+    while high - low > eps * eps * eps:
         middle = (high + low) / 2
         cube = middle * middle * middle
         if cube > n:
             high = middle
         elif cube < n:
-            low = middle 
+            low = middle
         elif cube == n:
             return middle
-    
+
     return middle
