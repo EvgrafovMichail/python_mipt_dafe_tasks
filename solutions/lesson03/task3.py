8981f15358f99b852d9aa70de125af4b8385935f
Shanechka <grisacernyh@gmail.com>
2025-10-10 19:22:38 +0300

    Homework for 11.10.2025

--- solutions/lesson03/task3.py
+++ solutions/lesson03/task3.py
@@ -1,3 +1,12 @@
 def get_nth_digit(num: int) -> int:
-    # ваш код
-    return 0
+    kolv_cifr = 0
+    digit_sum = 5
+    if num <= digit_sum:
+        return (num - 1) * 2
+    while num > digit_sum:
+        digit_sum += 45 * (10**kolv_cifr) * (kolv_cifr + 2)
+        kolv_cifr += 1
+    k = num - (digit_sum - 45 * (10 ** (kolv_cifr - 1)) * (kolv_cifr + 1)) - 1
+    k1 = k // (kolv_cifr + 1)
+    finall_humber = 10**kolv_cifr + k1 * 2
+    return (finall_humber // (10 ** ((kolv_cifr) - (k % (kolv_cifr + 1))))) % 10
