9bf9967ccef3d781ad01436b69e65bc58c0815f3
kiritonator <kir.shishkin.shkolnik@yandex.ru>
2025-10-10 23:56:36 +0300

    Done all tasks lesson03

--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -1,3 +1,17 @@
 def get_cube_root(n: float, eps: float) -> float:
-    # ваш код
-    return n
+    if abs(n) >= 1:
+        left = 0
+        right = n
+    else:
+        left = n
+        right = 1
+    x = n
+
+    while abs(x * x * x - n) > eps:
+        x = (left + right) / 2
+        if abs(x * x * x) > abs(n):
+            right = x
+        else:
+            left = x
+
+    return x
--- solutions/lesson03/task3.py
+++ solutions/lesson03/task3.py
@@ -1,3 +1,22 @@
 def get_nth_digit(num: int) -> int:
-    # ваш код
-    return 0
+    length = 1
+
+    while True:
+        if length == 1:
+            first_in_group = 0
+        else:
+            first_in_group = 10 ** (length - 1)
+        amount = (10**length - first_in_group) // 2 * length
+
+        if amount >= num:
+            pos_in_num = (num - 1) % length
+            in_number = first_in_group + ((num - 1) // length) * 2
+
+            while in_number >= 0:
+                length -= 1
+
+                if pos_in_num == length:
+                    return in_number % 10
+                in_number = in_number // 10
+        length += 1
+        num -= amount
