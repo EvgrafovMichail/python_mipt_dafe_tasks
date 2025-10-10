bc0343afc1f0efe5013266788d750756c504e4cd
Ruslan Bateryakov <ryslanbateryakov@gmail.com>
2025-10-10 20:38:13 +0300

    lesson3_homeWork_04

--- solutions/lesson03/task1.py
+++ solutions/lesson03/task1.py
@@ -1,11 +1,11 @@
 def flip_bits_in_range(num: int, left_bit: int, right_bit: int) -> int:
-    while (left_bit<=right_bit):
-        n = num // 2 ** (left_bit-1) % 2
-        if n==1:
-            num -= 2 ** (left_bit-1)
+    while left_bit <= right_bit:
+        n = num // 2 ** (left_bit - 1) % 2
+        if n == 1:
+            num -= 2 ** (left_bit - 1)
         else:
-            num += 2 ** (left_bit-1)
+            num += 2 ** (left_bit - 1)
 
         left_bit += 1
-        
+
     return num
