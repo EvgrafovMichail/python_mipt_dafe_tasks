efb4a1098fe1a50abdb26e7f5b974649995da3ab
Poco-dev <vueprojector5@gmail.com>
2025-10-11 02:50:44 +0300

    1+2

--- solutions/lesson04/task1.py
+++ solutions/lesson04/task1.py
@@ -1,6 +1,9 @@
 def is_arithmetic_progression(lst: list[list[int]]) -> bool:
-    print(list)
+    if len(lst)==1 or len(lst)==0:
+        return True
     lst.sort()
-    print(lst)
-    return False
-print(is_arithmetic_progression(1,2,3,9,5))
+    k = lst[1]-lst[0]
+    for i in range(len(lst)-1):
+        if (lst[i+1]-lst[i])!=k:
+            return False
+    return True
