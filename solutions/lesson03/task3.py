7fd7246e5eacb8272941b524e7f2e71c57c30da9
Vladimir <vl.mukhin21@gmail.com>
2025-10-11 00:05:56 +0300

    Отформатировано решение

--- solutions/lesson03/task3.py
+++ solutions/lesson03/task3.py
@@ -4,14 +4,14 @@ def get_nth_digit(num):
 
     if num <= digit_sum:
         return (num - 1) * 2
-    
+
     while num > digit_sum:
         digit_sum += 45 * (10**cnt_of_nums) * (cnt_of_nums + 2)
         cnt_of_nums += 1
-    
+
     k = num - (digit_sum - 45 * (10 ** (cnt_of_nums - 1)) * (cnt_of_nums + 1)) - 1
     k1 = k // (cnt_of_nums + 1)
-    
+
     out_humber = 10**cnt_of_nums + k1 * 2
-    
+
     return (out_humber // (10 ** ((cnt_of_nums) - (k % (cnt_of_nums + 1))))) % 10
