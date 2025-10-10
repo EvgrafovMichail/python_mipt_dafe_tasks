30bd2e525e9262c0a9ba84d7b5bc3116410b7c37
Alina <undel.xo17@gmail.com>
2025-10-10 21:40:27 +0300

    Изделеева Алина Б512-513

--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -1,3 +1,18 @@
 def get_cube_root(n: float, eps: float) -> float:
-    # ваш код
-    return n
+    if abs(n) < 1:
+        L, R = -1, 1
+    elif n > 0:
+        L, R = 0, n
+    else:
+        L, R = n, 0
+
+    while R - L > eps:  
+        middle = (L + R) / 2
+        middle3 = middle * middle * middle
+
+        if middle3 < n:
+            L = middle
+        else:
+            R = middle
+
+    return (L + R) / 2
--- solutions/lesson03/task3.py
+++ solutions/lesson03/task3.py
@@ -1,3 +1,18 @@
-def get_nth_digit(num: int) -> int:
-    # ваш код
-    return 0
+def get_nth_digit(n: int) -> int:
+    length = 1          
+    while True:
+        digits_in_block = count * length
+        if n > digits_in_block:
+            n -= digits_in_block
+            length += 1
+            start = 10 ** (length - 1)
+            count = (9 * 10 ** (length - 1)) // 2  
+        else:
+            break   
+    index = (n - 1) // length
+    num = (start + (index * 2))  
+    pos_from_left = (n - 1) % length
+    divisor = 10 ** (length - pos_from_left - 1)
+    digit = (num // divisor) % 10
+
+    return digit
