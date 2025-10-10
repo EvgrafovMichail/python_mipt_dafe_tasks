c2b14032199ba3e80ee08c8d412bb2a2730e2cd0
gvidon-7 <aleksandrlarionov71@icloud.com>
2025-10-10 21:37:16 +0300

    Александр Ларионов Б12-512
    Решение 1 и 2 задач из последнего контест, 3-ю пока не придумал, как решать

--- solutions/lesson03/task1.py
+++ solutions/lesson03/task1.py
@@ -1,3 +1,18 @@
 def flip_bits_in_range(num: int, left_bit: int, right_bit: int) -> int:
-    # ваш код
+
+    mask1 = 1 << left_bit - 1
+    mask2 = 1 << right_bit - 1
+    extra_bit = 0
+    if right_bit - left_bit - 1 == 0:
+        maskprom = mask2
+    elif right_bit - left_bit - 1 == 1:
+        extra_bit = 1 << left_bit
+        maskprom = extra_bit|mask2
+    else:
+        for i in range(left_bit, right_bit):
+            extra_bit|= 1 << i
+        maskprom = extra_bit|mask2
+  
+    maskfin = mask1|maskprom
+    num ^= maskfin 
     return num
--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -1,3 +1,25 @@
 def get_cube_root(n: float, eps: float) -> float:
-    # ваш код
-    return n
+
+    if eps <= 0:
+        print("Ошибка Ввода")
+        return None
+    if n == 0:
+        return 0
+    if n > 0:
+        low = 0
+        high = max(1,n)
+    else:
+        low = min(-1, n)
+        high = 0
+    
+    while high - low > eps*eps*eps:
+        middle = (high + low) / 2
+        cube = middle * middle * middle
+        if cube > n:
+            high = middle
+        elif cube < n:
+            low = middle 
+        elif cube == n:
+            return middle
+    
+    return middle
