462565fe44114ed097ccadd7061f6961b060b7c1
AntonLodygin <tonylodygin123@gmail.com>
2025-10-10 16:02:33 +0300

    lesson03 solution

--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -1,3 +1,20 @@
 def get_cube_root(n: float, eps: float) -> float:
-    # ваш код
-    return n
+    if n == 0:
+        return 0
+
+    abs_n = abs(n)
+
+    low = 0
+    high = abs_n if abs_n >= 1 else 1
+    mid = (low + high) / 2
+    mid3 = mid * mid * mid
+    while abs(mid3 - abs_n) > eps:
+        if mid3 > abs_n:
+            high = mid
+        else:
+            low = mid
+
+        mid = (low + high) / 2
+        mid3 = mid * mid * mid
+
+    return mid * abs_n / n
