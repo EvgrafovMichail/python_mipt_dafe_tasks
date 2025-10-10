5fffb98012651b4e2a0b4ee53e9633863a44cec9
kirill <qwerty.0045@inbox.ru>
2025-10-10 19:51:40 +0300

    Hometask3

--- solutions/lesson02/task5.py
+++ solutions/lesson02/task5.py
@@ -1,3 +1,12 @@
 def get_gcd(num1: int, num2: int) -> int:
-    # ваш код
+    a = num1
+    b = num2
+    t = 0
+    while a % b != 0:
+        if a < b:
+            t = a
+            a = b
+            b = t
+        a = a % b
+    num1 = b
     return num1
--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -1,3 +1,29 @@
+
+def mod(a: float):
+    if a < 0:
+        return -a
+    else:
+        return a
+
+
 def get_cube_root(n: float, eps: float) -> float:
-    # ваш код
-    return n
+    left = 0
+    sec = 0
+    ans = 0
+    min = 0
+    if n < 0:
+        n = -n
+        min = 1
+    right = n
+    while 1:
+        sec = (right + left) / 2
+        if sec * sec * sec > n:
+            right = sec
+        else:
+            left = sec
+            right = sec * 2
+        ans = right * right * right
+        if mod(ans - n) < eps:
+            if min == 1:
+                right = -right
+            return right
