589b18a0fe154f6535fea09c0e55d8c8976f5a7e
Poco-dev <vueprojector5@gmail.com>
2025-10-11 01:34:12 +0300

    Update task1.py

--- solutions/lesson04/task1.py
+++ solutions/lesson04/task1.py
@@ -1,3 +1,6 @@
 def is_arithmetic_progression(lst: list[list[int]]) -> bool:
-    # ваш код
+    print(list)
+    lst.sort()
+    print(lst)
     return False
+print(is_arithmetic_progression(1,2,3,9,5))
