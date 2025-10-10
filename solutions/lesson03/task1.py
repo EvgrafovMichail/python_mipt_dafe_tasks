3a055b956877cbf15b8047d884b2e1d631abc466
Kaitashi <vaich18022888@gmail.com>
2025-10-10 23:22:17 +0300

    task1 and task2 in 3 less

--- solutions/lesson03/task1.py
+++ solutions/lesson03/task1.py
@@ -1,3 +1,7 @@
 def flip_bits_in_range(num: int, left_bit: int, right_bit: int) -> int:
-    # ваш код
+    A = 0
+    for i in range(left_bit - 1, right_bit):
+        edinichki = 1 << i
+        A = A | edinichki
+    num = num ^ A
     return num
