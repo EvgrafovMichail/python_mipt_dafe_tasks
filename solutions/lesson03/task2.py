752ef4f8e7be40c98d64864dd328f0ce58fd25f9
Robert <robert.ibr0407@gmail.com>
2025-10-10 13:52:57 +0300

    My homework

--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -1,3 +1,25 @@
 def get_cube_root(n: float, eps: float) -> float:
-    # ваш код
-    return n
+    if n == 0:
+        return 0.0
+
+    sign = 1 if n >= 0 else -1
+    n_abs = abs(n)
+    
+
+    if n_abs >= 1:
+        left, right = 0.0, n_abs
+    else:
+        left, right = 0.0, 1.0
+    
+
+    while True:
+        mid = (left + right) / 2
+        mid_cubed = mid * mid * mid
+        
+        if abs(mid_cubed - n_abs) <= eps:
+            return sign * mid
+        
+        if mid_cubed < n_abs:
+            left = mid
+        else:
+            right = mid
