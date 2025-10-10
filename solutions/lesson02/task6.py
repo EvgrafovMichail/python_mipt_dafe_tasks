f4bc75fbb8acbaec594a73822c96748b086acc80
Mihail Barinov <dmemsmdmemsm@gmail.com>
2025-10-10 23:58:02 +0300

    Again fix linter errors

--- solutions/lesson02/task6.py
+++ solutions/lesson02/task6.py
@@ -1,7 +1,7 @@
 def get_sum_of_prime_divisors(num: int) -> int:
     result = 0
     if num >= 2:
-        for i in range(2, int(num ** 0.5) + 1):
+        for i in range(2, int(num**0.5) + 1):
             if num % i == 0:
                 result += i
                 while num % i == 0:
--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -5,10 +5,10 @@ def get_cube_root(n: float, eps: float) -> float:
     result = 0
     a = 0
     abs_n = abs(n)
-    b = abs_n if abs_n >= 1 else 1/abs_n
-    while abs(result*result*result - abs_n) > eps / 10:
+    b = abs_n if abs_n >= 1 else 1 / abs_n
+    while abs(result * result * result - abs_n) > eps / 10:
         result = (a + b) / 2
-        if result*result*result >= abs_n:
+        if result * result * result >= abs_n:
             b = result
         else:
             a = result
