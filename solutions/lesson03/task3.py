f734ccea8ea8019d363b77bb0348643504191392
dandandil <daniil182403@gmail.com>
2025-10-10 17:12:41 +0000

    sth reformatted, les03/task1 ready

--- solutions/lesson03/task3.py
+++ solutions/lesson03/task3.py
@@ -1,10 +1,12 @@
 def get_nth_digit(num: int) -> int:
-    for r in range(1, 10):
-        if num - (9 * 10 ** (r - 1) * 5 * (r + 1) + 5) > 0:
-            s = 9 * 10**r - (num - (9 * 10 ** (r - 1) * 5 * (r + 1) + 5)) // (r + 1)
-            c = 10**r + s // r + s % (r + 1)
-            d = c // 10 ** (s % (r + 1))
+    for r in range(10, 0, -1):
+        t = num - ((9 * 10 ** (r - 1) / 2) * r + 5)
+        if t > 0:
+            s = 10**r / 2 + t // (r + 1)
+            n = t % (r + 1)
+            c = 2 * s - 1
+            d = (c + 1) % 10**n
             return d
 
 
-print(get_nth_digit(10))
+print(get_nth_digit(1000))
