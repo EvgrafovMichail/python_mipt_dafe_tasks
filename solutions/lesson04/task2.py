1e00b5370866856ca872779e54650d5637f1b9bd
Poco-dev <vueprojector5@gmail.com>
2025-10-11 05:36:23 +0300

    lesson4

--- solutions/lesson04/task2.py
+++ solutions/lesson04/task2.py
@@ -1,19 +1,23 @@
 def merge_intervals(intervals: list[list[int, int]]) -> list[list[int, int]]:
-    time = [0]*(10**6+1)
+    time = [0] * (10**6 + 1)
     for interval in intervals:
-        time[interval[0]]+=1
-        time[interval[1]]-=1
+        time[interval[0]] += 1
+        time[interval[1]] -= 1
     k = 0
-    st=-1
+    st = -1
     answer = []
-    for i in range(0,10**6+1):
-        if time[i]>0:
-            k+=time[i]
-            if st==-1:
-                st=i
-        if time[i]<0:
-            k+=time[i]
-            if k==0:
-                answer.append([st,i])
-                st=-1
+    for i in range(0, 10**6 + 1):
+        if time[i] > 0:
+            k += time[i]
+            if st == -1:
+                st = i
+        if time[i] < 0:
+            k += time[i]
+            if k == 0:
+                answer.append([st, i])
+                st = -1
     return answer
+
+
+# алгоритм не оптимален для маленького количества интервалов
+# но если их количество будет большим, то должен отлично подходить
