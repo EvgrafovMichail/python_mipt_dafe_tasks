5b7121c354c0001bb19feb3e46f5bad0438a138e
danberx <danberx490@gmail.com>
2025-10-11 10:35:54 +0300

    solved all tasks for lesson04

--- solutions/lesson04/task5.py
+++ solutions/lesson04/task5.py
@@ -1,3 +1,13 @@
 def find_row_with_most_ones(matrix: list[list[int]]) -> int:
-    # ваш код
-    return 0
+    if len(matrix) == 0:
+        return 0
+    ans = 0
+    i = 0
+    j = len(matrix[0]) - 1
+    while i < len(matrix) and j >= 0:
+        if matrix[i][j] == 1:
+            ans = i
+            j -= 1
+        else:
+            i += 1
+    return ans
--- solutions/lesson04/task6.py
+++ solutions/lesson04/task6.py
@@ -1,3 +1,11 @@
-def count_cycles(arr: list[int]) -> int: 
-    # ваш код
-    return 0
+def count_cycles(arr: list[int]) -> int:
+    cnt_cycles = 0
+    used = [False] * len(arr)
+    for i in range(len(arr)):
+        if not used[i]:
+            cnt_cycles += 1
+            used[i] = True
+            while not used[arr[i]]:
+                i = arr[i]
+                used[i] = True
+    return cnt_cycles
