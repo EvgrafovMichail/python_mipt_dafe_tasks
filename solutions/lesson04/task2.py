9af881dadf893291b17091b26e7a1a3121d71a51
bulka0007 <bulat.ramazanov.2000@bk.ru>
2025-10-14 22:16:19 +0300

    KonTest3 Ramazanov B 12-512

--- solutions/lesson04/task2.py
+++ solutions/lesson04/task2.py
@@ -1,3 +1,14 @@
 def merge_intervals(intervals: list[list[int, int]]) -> list[list[int, int]]:
-    # ваш код
-    return [[0,0]]
+    if intervals == []:
+        return []
+    intervals.sort()
+    result = []
+    start = intervals[0][0]
+    mx = intervals[0][1]
+    for i in range(1, len(intervals)):
+        if mx < intervals[i][0]:
+            result.append([start, mx])
+            start = intervals[i][0]
+        mx = max(mx, intervals[i][1])
+    result.append([start, mx])
+    return result
