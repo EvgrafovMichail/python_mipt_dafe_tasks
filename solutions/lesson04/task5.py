226c1f05f945c24560fce35af5f22d7c1f1b1e8e
BogdanYeti <unrelise_original@mail.ru>
2025-10-15 17:52:37 +0300

    Soltuions for lesson 4

--- solutions/lesson04/task5.py
+++ solutions/lesson04/task5.py
@@ -1,3 +1,19 @@
 def find_row_with_most_ones(matrix: list[list[int]]) -> int:
-    # ваш код
-    return 0
+    ind, mi = 0, float("inf")
+    i = 0
+    for elem in matrix:
+        j = 0
+        for num in elem:
+            if num:
+                if j < mi:
+                    mi = j
+                    ind = i
+                break
+            j += 1
+        i += 1
+    return ind
+
+
+if __name__ == "__main__":
+    print(find_row_with_most_ones([[0, 0, 1, 1], [0, 1, 1, 1], [1, 1, 1, 1], [0, 0, 0, 1]]))
+    print(find_row_with_most_ones([[0, 0, 0], [0, 0, 0], [0, 0, 0]]))
