88935c59d401c6fd91b5994b51c72bbcba322c42
AntonLodygin <tonylodygin123@gmail.com>
2025-10-15 18:56:57 +0300

    lesson04 solutions

--- solutions/lesson04/task1.py
+++ solutions/lesson04/task1.py
@@ -1,3 +1,6 @@
-def is_arithmetic_progression(lst: list[list[int]]) -> bool:
-    # ваш код
-    return False
+def is_arithmetic_progression(lst: list[int]) -> bool:
+    lst.sort()
+    for i in range(len(lst) - 2):
+        if lst[i] - lst[i + 1] != lst[i + 1] - lst[i + 2]:
+            return False
+    return True
