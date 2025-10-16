797c29e3ffb35918ec56a95380b60d6b2527694f
melanikot <kotova.mv@phystech.edu>
2025-10-16 21:45:20 +0300

    less4

--- solutions/lesson04/task2.py
+++ solutions/lesson04/task2.py
@@ -1,3 +1,27 @@
 def merge_intervals(intervals: list[list[int, int]]) -> list[list[int, int]]:
-    # ваш код
-    return [[0,0]]
+    if len(intervals) == 0:
+        return intervals
+
+    intervals = sorted(intervals)
+    length = len(intervals)
+
+    i = 0
+
+    while i != length - 1:
+        if intervals[i][1] >= intervals[i + 1][0] and intervals[i][1] < intervals[i + 1][1]:
+            intervals.append([intervals[i][0], intervals[i + 1][1]])
+            intervals.pop(i)
+            intervals.pop(i)
+            intervals = sorted(intervals)
+            length = len(intervals)
+            i = 0
+            continue
+        elif intervals[i][1] >= intervals[i + 1][1]:
+            intervals.pop(i + 1)
+            length = len(intervals)
+            i = 0
+            continue
+
+        i += 1
+
+    return intervals
