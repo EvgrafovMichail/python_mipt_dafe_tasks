9822e8148417e18b1c2bc91e45394563251b465e
Kodik Igor <ikodik34@gmail.com>
2025-10-16 15:17:36 +0300

    tasks for lesson04

--- solutions/lesson04/task3.py
+++ solutions/lesson04/task3.py
@@ -1,3 +1,9 @@
 def find_single_number(nums: list[int]) -> int:
-    # ваш код
-    return 0
+    binary_form = 0
+    for num in nums:
+        if num < 0:
+            binary_form ^= ~(num - 1)
+            binary_form *= -1
+        else:
+            binary_form ^= num
+    return binary_form
