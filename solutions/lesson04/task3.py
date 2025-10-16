317f9de2d575f9cc03517dc86fc65f5931d8fb40
Kodik Igor <ikodik34@gmail.com>
2025-10-16 16:24:22 +0300

    update task3

--- solutions/lesson04/task3.py
+++ solutions/lesson04/task3.py
@@ -1,9 +1,5 @@
 def find_single_number(nums: list[int]) -> int:
     binary_form = 0
     for num in nums:
-        if num < 0:
-            binary_form ^= ~(num - 1)
-            binary_form *= -1
-        else:
-            binary_form ^= num
+        binary_form ^= num
     return binary_form
