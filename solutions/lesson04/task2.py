3c9e5512832f92f44666940f06eca0ea59b3d4e1
Kaitashi <vaich18022888@gmail.com>
2025-10-17 22:31:31 +0300

    dobavil less4

--- solutions/lesson04/task2.py
+++ solutions/lesson04/task2.py
@@ -1,3 +1,22 @@
 def merge_intervals(intervals: list[list[int, int]]) -> list[list[int, int]]:
-    # ваш код
-    return [[0, 0]]
+    if len(intervals) < 1:
+        return []
+    else:
+        intervals_sort = sorted(intervals)
+        noviy_spisok = [intervals_sort[0]]
+        for i in range(1, len(intervals_sort)):
+            vtoroy_spisok = intervals_sort[i]
+            perviy_spisok = noviy_spisok[-1]
+            left_2 = vtoroy_spisok[0]
+            right_2 = vtoroy_spisok[1]
+            right_1 = perviy_spisok[1]
+            left_1 = perviy_spisok[0]
+            if left_2 <= right_1:
+                if right_2 >= right_1:
+                    right = right_2
+                else:
+                    right = right_1
+                noviy_spisok[-1] = [left_1, right]
+            else:
+                noviy_spisok += [vtoroy_spisok]
+    return noviy_spisok
--- solutions/lesson04/task3.py
+++ solutions/lesson04/task3.py
@@ -1,3 +1,19 @@
 def find_single_number(nums: list[int]) -> int:
-    # ваш код
-    return 0
+    if len(nums) < 2:
+        return nums[0]
+    nums_sort = sorted(nums)
+    if len(nums) == 3:
+        if nums_sort[1] - nums_sort[0] != 0:
+            return nums_sort[0]
+        else:
+            return nums_sort[2]
+    a = int(len(nums) - 3)
+    fst_num = nums_sort[0]
+    scnd_num = nums_sort[1]
+    for i in range(0, a):
+        if fst_num - scnd_num != 0:
+            return fst_num
+        else:
+            fst_num = nums_sort[i + 2]
+            scnd_num = nums_sort[i + 3]
+    return nums_sort[-1]
