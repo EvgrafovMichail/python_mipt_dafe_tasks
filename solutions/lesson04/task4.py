4115ee1e4f0f346dbbfa12314ad452773ece774d
Oleg <rozomazan069@gmail.com>
2025-10-17 21:17:31 +0300

    completed homework on 18.10.25

--- solutions/lesson04/task4.py
+++ solutions/lesson04/task4.py
@@ -1,3 +1,13 @@
 def move_zeros_to_end(nums: list[int]) -> list[int]:
-    # ваш код
-    return 0
+    i = 0
+
+    not_trash = len(nums)
+    while i < not_trash:
+        if nums[i] == 0:
+            nums.append(0)
+            nums.pop(i)
+            not_trash -= 1
+        else:
+            i += 1
+
+    return not_trash
--- solutions/lesson04/task5.py
+++ solutions/lesson04/task5.py
@@ -1,3 +1,14 @@
 def find_row_with_most_ones(matrix: list[list[int]]) -> int:
-    # ваш код
-    return 0
+    length = len(matrix)
+    if length < 1:
+        return 0
+    n = 0
+    i, j = 0, len(matrix[0]) - 1
+    while i < length and j > -1:
+        if matrix[i][j] == 1:
+            j -= 1
+            n = i
+        else:
+            i += 1
+
+    return n
