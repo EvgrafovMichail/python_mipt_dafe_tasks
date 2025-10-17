e729afa00ef8b6314c7621b183f496655306aaf3
Ssssserge <yanchevskiy07@gmail.com>
2025-10-17 23:37:57 +0300

    Задания к 4 занятию

--- solutions/lesson04/task2.py
+++ solutions/lesson04/task2.py
@@ -1,3 +1,21 @@
 def merge_intervals(intervals: list[list[int, int]]) -> list[list[int, int]]:
-    # ваш код
-    return [[0,0]]
+    if not intervals:
+        return []
+
+    intervals.sort()
+
+    merged = []
+    current_start, current_end = intervals[0]
+
+    for i in range(1, len(intervals)):
+        next_start, next_end = intervals[i]
+
+        if next_start <= current_end:
+            current_end = max(current_end, next_end)
+        else:
+            merged.append([current_start, current_end])
+            current_start, current_end = next_start, next_end
+
+    merged.append([current_start, current_end])
+
+    return merged
--- solutions/lesson04/task6.py
+++ solutions/lesson04/task6.py
@@ -1,3 +1,16 @@
-def count_cycles(arr: list[int]) -> int: 
-    # ваш код
-    return 0
+def count_cycles(arr: list[int]) -> int:
+    if len(arr) == 0:
+        return 0
+
+    cycles = 0
+    passed = []
+
+    for start_index in range(len(arr)):
+        if start_index not in passed:
+            cycles += 1
+            current_index = start_index
+            while current_index not in passed:
+                passed.append(current_index)
+                current_index = arr[current_index]
+
+    return cycles
