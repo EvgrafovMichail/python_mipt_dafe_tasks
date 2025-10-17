c08aa48bfeba12ab7ba1491a83aa93c98d0a95e2
Mihail Barinov <dmemsmdmemsm@gmail.com>
2025-10-17 23:25:20 +0300

    Fix linter warnings

--- solutions/lesson04/task1.py
+++ solutions/lesson04/task1.py
@@ -5,7 +5,7 @@ def is_arithmetic_progression(lst: list[int]) -> bool:
     lst.sort()
     d = lst[1] - lst[0]
     for i in range(len(lst) - 1):
-        sub = lst[i+1] - lst[i]
+        sub = lst[i + 1] - lst[i]
         if sub != d:
             return False
         d = sub
