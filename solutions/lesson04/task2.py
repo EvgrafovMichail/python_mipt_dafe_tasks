d34319f5cb0baffd46433b4eac010fd6726cb23a
murmassa <egorgalkin909@gmail.com>
2025-10-18 00:01:09 +0300

    lesson04 dz

--- solutions/lesson04/task2.py
+++ solutions/lesson04/task2.py
@@ -1,3 +1,13 @@
 def merge_intervals(intervals: list[list[int, int]]) -> list[list[int, int]]:
-    # ваш код
-    return [[0,0]]
+    if len(intervals) == 0 or len(intervals) == 1: return intervals
+    else:
+        i = 0
+        while i < len(intervals) -1:
+            if intervals[i+1][0] <= intervals[i][1]:
+                intervals[i+1][0] = intervals[i][0]
+                if intervals[i][1] > intervals[i+1][1]:
+                    intervals[i+1][1] = intervals[i][1]
+                intervals.pop(i)
+            else:
+                i+=1
+    return intervals
--- solutions/lesson04/task3.py
+++ solutions/lesson04/task3.py
@@ -1,3 +1,12 @@
 def find_single_number(nums: list[int]) -> int:
-    # ваш код
-    return 0
+    output = []
+    j = 0
+    for i in range(len(nums)):
+        if nums[i] not in output:
+            output.append(nums[i])
+            j+=1
+        else:
+            output.remove(nums[i])
+            j-=1
+    finale = output[0]
+    return finale
