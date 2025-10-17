25a7a8431971ce508858c7da3aa9cee31b1368cf
Vyacheslav A <aleksakhin.viu@phystech.edu>
2025-10-17 11:20:33 +0300

    Solutions for every task

--- solutions/lesson04/task6.py
+++ solutions/lesson04/task6.py
@@ -1,3 +1,27 @@
-def count_cycles(arr: list[int]) -> int: 
+def count_cycles(arr: list[int]) -> int:
     # ваш код
-    return 0
+
+    if len(arr) == 0:
+        return 0
+
+    start_index, index = 0, 0
+    cycle_count = 0
+    while cycle_count == 0:
+        if arr[index] != 0:
+            start_index = index
+            index = arr[index]
+            arr[start_index] = 0
+        else:
+            cycle_count = 1
+
+    for index in range(1, len(arr)):
+        if arr[index] == 0:
+            continue
+        else:
+            while arr[index] != 0:
+                start_index = index
+                index = arr[index]
+                arr[start_index] = 0
+            cycle_count += 1
+
+    return cycle_count
