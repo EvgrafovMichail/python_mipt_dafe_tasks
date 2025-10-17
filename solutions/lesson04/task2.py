eb84f5f52ab2df93e717a1c7c6c3641588bb5e94
Savely <savvatrin@gmail.com>
2025-10-17 19:57:41 +0300

    gg

--- solutions/lesson04/task2.py
+++ solutions/lesson04/task2.py
@@ -1,3 +1,11 @@
 def merge_intervals(intervals: list[list[int, int]]) -> list[list[int, int]]:
-    # ваш код
+    left = []
+    right = []
+    for i in intervals:
+        left.append(i[0])
+        right.append(i[1])
+    left.sort()
+    right.sort()
+    for g in left:
+        for     
     return [[0,0]]
