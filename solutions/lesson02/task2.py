7d1c06352d91a3b999eb9522a55372beb7629d0b
kalium-cyanide <mrrrosssi@gmail.com>
2025-10-17 23:49:30 +0300

    hw4+lesson2fix

--- solutions/lesson02/task2.py
+++ solutions/lesson02/task2.py
@@ -1,10 +1,11 @@
 def get_doubled_factorial(num: int) -> int:
     factorial = 1
-
-    for n in range(1, num + 1):
-        if n % 2 != num % 2:
-            continue
-        print(n)
-        factorial *= n
-
+    if num == 0:
+        factorial = 1
+    elif num % 2 == 0:
+        for i in range(2, num + 2, 2):
+            factorial *= i
+    else:
+        for i in range(1, num + 2, 2):
+            factorial *= i
     return factorial
