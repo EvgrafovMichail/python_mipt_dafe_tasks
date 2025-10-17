5fd9886570806c3f31bf701238e7d82bea776479
4gera4 <geraskinctepan@gmail.com>
2025-10-17 23:46:08 +0300

    Stepan Geraski 4th homework

--- solutions/lesson04/task6.py
+++ solutions/lesson04/task6.py
@@ -1,3 +1,16 @@
 def count_cycles(arr: list[int]) -> int: 
-    # ваш код
-    return 0
+    mask = [True]*len(arr)
+    res = 0
+    for i in range(0, len(arr)):
+        if mask[i]:
+            head = arr[i]
+            mask[head]=False
+            current = arr[head]
+            mask[current]=False 
+            next = 0
+            while head != current:
+                next = arr[current]
+                mask[current]=False
+                current = next
+            res += 1
+    return res
