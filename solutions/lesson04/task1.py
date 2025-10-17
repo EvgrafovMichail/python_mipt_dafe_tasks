4c4c79f267b34e3b94e7f66fab5fe39b686bbcb6
Denis1325 <penkovdenis271@gmail.com>
2025-10-18 00:02:34 +0300

    sol1

--- solutions/lesson04/task1.py
+++ solutions/lesson04/task1.py
@@ -1,3 +1,11 @@
 def is_arithmetic_progression(lst: list[list[int]]) -> bool:
-    # ваш код
-    return False
+    lst.sort()
+    if len(lst) <= 2:
+        return True
+    d = lst[1] - lst[0]
+    for i in range(1, len(lst)):
+        if lst[i] - lst[i-1] != d:
+            return False
+
+
+    return True
--- solutions/lesson04/task5.py
+++ solutions/lesson04/task5.py
@@ -1,3 +1,19 @@
 def find_row_with_most_ones(matrix: list[list[int]]) -> int:
-    # ваш код
-    return 0
+    if len(matrix) == 0 or len(matrix[0]) == 0:
+        return 0
+    n = len(matrix)
+    m = len(matrix[0])-1
+    i = 0
+    j = m
+    num = 0
+    res = 0
+    while i != n:
+        if matrix[i][j] == 1 and j != 0 and matrix[i][j-1] == 1:
+            j -= 1
+            num = i
+        else:
+            i += 1
+    return num
+
+
+print(find_row_with_most_ones([[0, 0, 1, 1], [0, 1, 1, 1], [0, 0, 0, 1], [1, 1, 1, 1], [0, 1, 1, 1]]))
