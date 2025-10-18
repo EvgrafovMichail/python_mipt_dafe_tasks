0b5bbfd28ba19715d1c94b0632dba881acf499bc
U3SU <u3.su@yandex.ru>
2025-10-18 22:41:31 +0300

    Tasks 1-3

--- solutions/lesson05/task2.py
+++ solutions/lesson05/task2.py
@@ -1,3 +1,7 @@
 def are_anagrams(word1: str, word2: str) -> bool:
-    # ваш код
+    word1 = list(word1)
+    word2 = list(word2)
+    if len(word1) == len(word2):
+        if sorted(word1) == sorted(word2):
+            return True
     return False
