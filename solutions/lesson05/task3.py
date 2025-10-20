f193a27ac3079f018da81eeea74a4f462b32396f
U3SU <u3.su@yandex.ru>
2025-10-20 14:28:39 +0300

    Task4

--- solutions/lesson05/task3.py
+++ solutions/lesson05/task3.py
@@ -1,6 +1,6 @@
 def is_punctuation(text: str) -> bool:
     text = list(text)
-    a = "!#$%&'()*+,-./:;<=>?@[\]^_{|}~`" + '"'
+    a = r"!#$%&'()*+,-./:;<=>?@[\]^_{|}~`" + '"'
     a = list(a)
     if len(text) != 0:
         for i in range(0, len(text)):
--- solutions/lesson05/task5.py
+++ solutions/lesson05/task5.py
@@ -1,3 +1,2 @@
 def reg_validator(reg_expr: str, text: str) -> bool:
-    # ваш код
     return False
