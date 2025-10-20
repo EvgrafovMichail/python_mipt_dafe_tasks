28ffd3dc7a490aa9eacf11fc32ae58b18859c10f
Vyacheslav A <aleksakhin.viu@phystech.edu>
2025-10-20 18:02:04 +0300

    Solutions for tasks 1, 2 and 3 from lesson05 attempt 2

--- solutions/lesson05/task3.py
+++ solutions/lesson05/task3.py
@@ -1,11 +1,11 @@
 def is_punctuation(text: str) -> bool:
     # ваш код
     punctuation_symbols = "!\"#$%&'()*+,-./:;<=>?@[\\]^_{|}~`"
-    if text == "": 
+    if text == "":
         return False
-    for i in range(len(text)): 
-        if text[i] in punctuation_symbols: 
-            continue  
+    for i in range(len(text)):
+        if text[i] in punctuation_symbols:
+            continue
         else:
             return False
-    return True   
+    return True
