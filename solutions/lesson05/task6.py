6d3111ea8aca922d03b5193613e6a27c12d95ba4
minskiyilja-dot <pooneee819@gmail.com>
2025-10-22 23:55:17 +0300

    homework lesson4

--- solutions/lesson05/task6.py
+++ solutions/lesson05/task6.py
@@ -1,3 +1,19 @@
 def simplify_path(path: str) -> str:
-    # ваш код
-    return path
+    new_text=[]
+    path=path.split("/")
+    text=""
+    for i in range(len(path)):
+        if path[i]=="." or  path[i]=="":
+            continue   
+        elif path[i]=="..":
+            if new_text:
+                new_text=new_text[:-1]
+            else:
+                return ""
+        else:
+            new_text.append(path[i])
+    if not(new_text):
+        return "/"
+    for i in new_text:
+        text=text+"/"+i
+    return text
