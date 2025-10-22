28c985227977b858f01fdde94615073ef2aae40a
Vandam-create <beliakov.va@phystech.edu>
2025-10-22 20:20:42 +0300

    Ver_3

--- solutions/lesson05/task2.py
+++ solutions/lesson05/task2.py
@@ -2,11 +2,11 @@ def are_anagrams(word1: str, word2: str) -> bool:
     a = True
 
     for i in word1:
-        if not (i in word1):
+        if i not in word1:
             a = False
 
     for i in word2:
-        if not (i in word1):
+        if i not in word1:
             a = False
 
     return a
