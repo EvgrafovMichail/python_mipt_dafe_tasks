191878b57ef9903362ab3e346d9602e60c3400c8
BogdanYeti <unrelise_original@mail.ru>
2025-10-23 00:54:10 +0300

    formated task 5

--- solutions/lesson05/task2.py
+++ solutions/lesson05/task2.py
@@ -1,7 +1,7 @@
 def are_anagrams(word1, word2):
     counts = [0] * 52
     for char in word1:
-        counts[ord(char) - ord('A') if 'A' <= char <= 'Z' else 26 + (ord(char) - ord('a'))] += 1
+        counts[ord(char) - ord("A") if "A" <= char <= "Z" else 26 + (ord(char) - ord("a"))] += 1
     for char in word2:
-        counts[ord(char) - ord('A') if 'A' <= char <= 'Z' else 26 + (ord(char) - ord('a'))] -= 1
-    return not(any(counts))
+        counts[ord(char) - ord("A") if "A" <= char <= "Z" else 26 + (ord(char) - ord("a"))] -= 1
+    return not (any(counts))
