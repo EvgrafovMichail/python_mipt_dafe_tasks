e2f18d66c19a57c0602e97b12f1feb51c0960558
Vandam-create <beliakov.va@phystech.edu>
2025-10-22 20:16:50 +0300

    Ver_1

--- solutions/lesson05/task2.py
+++ solutions/lesson05/task2.py
@@ -1,3 +1,12 @@
 def are_anagrams(word1: str, word2: str) -> bool:
-    # ваш код
-    return False
+    a = True
+
+    for i in word1:
+        if not (i in word1):
+            a = False
+
+    for i in word2:
+        if not (i in word1):
+            a = False
+
+    return a
--- solutions/lesson05/task5.py
+++ solutions/lesson05/task5.py
@@ -1,3 +1,39 @@
-def reg_validator(reg_expr: str, text: str) -> bool:  
-    # ваш код
-    return False
+def reg_validator(reg_expr: str, text: str) -> bool:
+    if len(reg_expr) == 0:
+        return len(text) == 0
+
+    reg_i = 0
+    text_i = 0
+
+    while reg_i < len(reg_expr) and text_i < len(text):
+        current_reg = reg_expr[reg_i]
+
+        if current_reg == "d":
+            if text_i >= len(text) or not text[text_i].isdigit():
+                return False
+
+            while text_i < len(text) and text[text_i].isdigit():
+                text_i += 1
+
+        elif current_reg == "w":
+            if text_i >= len(text) or not text[text_i].isalpha():
+                return False
+
+            while text_i < len(text) and text[text_i].isalpha():
+                text_i += 1
+
+        elif current_reg == "s":
+            if text_i >= len(text) or not text[text_i].isalnum():
+                return False
+
+            while text_i < len(text) and text[text_i].isalnum():
+                text_i += 1
+
+        else:
+            if text_i >= len(text) or text[text_i] != current_reg:
+                return False
+            text_i += 1
+
+        reg_i += 1
+
+    return reg_i == len(reg_expr) and text_i == len(text)
