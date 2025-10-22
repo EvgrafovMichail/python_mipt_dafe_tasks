809564ffca0bb9505690db31d01eafe848569c53
gvidon-7 <aleksandrlarionov71@icloud.com>
2025-10-22 21:23:39 +0300

    Ларионов Алесандр Б12-512
    Все 6 задач из контеста за 5-ю неделю

--- solutions/lesson05/task4.py
+++ solutions/lesson05/task4.py
@@ -1,3 +1,12 @@
 def unzip(compress_text: str) -> str:
     # ваш код
-    return compress_text
+    els = compress_text.split()
+    compress_text_reskin = []
+    for el in els:
+        if "*" in el:
+            text, num = el.split("*")
+            compress_text_reskin.append(text * int(num))
+        else:
+            compress_text_reskin.append(el)
+    compress_text_reskin = "".join(compress_text_reskin)
+    return compress_text_reskin
