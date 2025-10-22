134eaede4204d556bcf529569c8596a158705bf3
Vladimir <vl.mukhin21@gmail.com>
2025-10-22 11:58:51 +0300

    Сделано задание 3

--- solutions/lesson05/task3.py
+++ solutions/lesson05/task3.py
@@ -1,3 +1,15 @@
 def is_punctuation(text: str) -> bool:
-    # ваш код
-    return False
+    if text == "":
+        return False
+
+    for symb in text:
+        if (
+            (33 <= ord(symb) <= 47)
+            or (58 <= ord(symb) <= 64)
+            or (91 <= ord(symb) <= 96)
+            or (123 <= ord(symb) <= 126)
+        ):
+            pass
+        else:
+            return False
+    return True
