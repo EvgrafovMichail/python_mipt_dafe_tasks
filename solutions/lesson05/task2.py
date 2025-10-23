3d7fadf6b49d82dd40fd92f5e96dcf0f9784094f
Vladimir <vl.mukhin21@gmail.com>
2025-10-24 00:31:08 +0300

    Сделано задание 2

--- solutions/lesson05/task2.py
+++ solutions/lesson05/task2.py
@@ -1,3 +1,22 @@
 def are_anagrams(word1: str, word2: str) -> bool:
-    # ваш код
-    return False
+    if len(word1) != len(word2):
+        return False
+
+    cnt_chars = [0] * 52
+
+    for symb in word1:
+        if symb.islower():
+            cnt_chars[ord(symb) - 97] += 1
+        else:
+            cnt_chars[ord(symb) - 39] += 1  # -65+26
+
+    for symb in word2:
+        if symb.islower():
+            cnt_chars[ord(symb) - 97] -= 1
+        else:
+            cnt_chars[ord(symb) - 39] -= 1  # -65+26
+
+    for i in cnt_chars:
+        if i != 0:
+            return False
+    return True
