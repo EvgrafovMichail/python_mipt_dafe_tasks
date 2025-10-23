9a4f9cd56120290a134a470a367567980c8ba792
4gera4 <geraskinctepan@gmail.com>
2025-10-24 01:07:25 +0300

    Gera 5th hw

--- solutions/lesson05/task2.py
+++ solutions/lesson05/task2.py
@@ -1,3 +1,39 @@
 def are_anagrams(word1: str, word2: str) -> bool:
-    # ваш код
-    return False
+    
+
+    if len(word1) != len(word2):
+        return False
+    else:
+        xor_mask=0
+        arith_mask=0
+        mult_mask =1
+        for i in range(0, len(word1)):
+            if ord(word1[i])>=ord('a'):
+                xor_mask ^= (ord(word1[i])+1000)
+            else:
+                xor_mask ^= ord(word1[i]) 
+            if ord(word2[i])>=ord('a'):
+                xor_mask ^= (ord(word2[i])+1000)
+            else:
+                xor_mask ^= ord(word2[i]) 
+
+
+            if ord(word1[i])>=ord('a'):
+                arith_mask += (ord(word1[i])+1000)
+            else:
+                arith_mask += ord(word1[i]) 
+            if ord(word2[i])>=ord('a'):
+                arith_mask -= (ord(word2[i])+1000)
+            else:
+                arith_mask -= ord(word2[i])
+
+
+            if ord(word1[i])>=ord('a'):
+                mult_mask *= (ord(word1[i])+1000)
+            else:
+                mult_mask *= ord(word1[i]) 
+            if ord(word2[i])>=ord('a'):
+                mult_mask /= (ord(word2[i])+1000)
+            else:
+                mult_mask /= ord(word2[i])  
+    return 0==arith_mask and 0 == xor_mask and (mult_mask -1 <1e-6)
--- solutions/lesson05/task5.py
+++ solutions/lesson05/task5.py
@@ -1,3 +1,42 @@
-def reg_validator(reg_expr: str, text: str) -> bool:  
-    # ваш код
-    return False
+def reg_validator(reg_expr: str, text: str) -> bool: 
+    if len(reg_expr)>len(text):
+        return False
+    for i in range(0, len(reg_expr)):
+        if reg_expr[i]=="d":
+            if not text[0].isdigit():
+               return False
+            while text[0].isdigit():
+                text = text[1:]
+                if len(text)==0:
+                    if len(reg_expr)-1-i !=0:
+                        return False
+                    return True
+
+        elif reg_expr[i]=="w":
+            if not text[0].isalpha():
+               return False
+            while text[0].isalpha():
+                text = text[1:]
+                if len(text)==0:
+                    if len(reg_expr)-1-i !=0:
+                        return False
+                    return True
+
+        elif reg_expr[i]=="s":
+            if not ( text[0].isalpha() or text[0].isdigit()) :
+               return False
+            while text[0].isalpha() or text[0].isdigit():
+                text = text[1:]
+                if len(text)==0:
+                    if len(reg_expr)-1-i !=0:
+                        return False
+                    return True
+        else:
+            if text[0] != reg_expr[i]:
+                return False
+            text = text[1:]
+            if len(text)==0:
+                    if len(reg_expr)-1-i !=0:
+                        return False
+                    return True
+    return len(text)==0  
