153385c0221f109e00819667a9d1c8dd5a620c95
Prosto_Max_xX <maxim.nevinsky@yandex.ru>
2025-10-23 18:56:45 +0300

    made tasks

--- solutions/lesson05/task2.py
+++ solutions/lesson05/task2.py
@@ -1,3 +1,10 @@
 def are_anagrams(word1: str, word2: str) -> bool:
-    # ваш код
-    return False
+    n = len(word1)
+    m = len(word2)
+    if n != m:
+        return False
+    for i in range(n):
+        if (word1[i] not in word2) or (word1.count(word1[i]) != word2.count(word2[i])):
+            return False
+    else:
+        return True
--- solutions/lesson05/task4.py
+++ solutions/lesson05/task4.py
@@ -1,3 +1,10 @@
 def unzip(compress_text: str) -> str:
-    # ваш код
+    strings = compress_text.split()
+    compress_text = ""
+    for elem in strings:
+        if "*" in elem:
+            string, mult = elem.split("*")
+            compress_text += string * int(mult)
+        else:
+            compress_text += elem
     return compress_text
