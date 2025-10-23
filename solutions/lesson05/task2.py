cbe3e482ff447ebab2c5e3140fcce1784e5736b2
Oleg <rozomazan069@gmail.com>
2025-10-23 14:13:04 +0300

    Complete tasks for lesson05

--- solutions/lesson05/task2.py
+++ solutions/lesson05/task2.py
@@ -1,3 +1,12 @@
 def are_anagrams(word1: str, word2: str) -> bool:
-    # ваш код
+    alph1 = [0] * 52
+    alph2 = [0] * 52
+    for i in range(65, 91):
+        alph1[i - 65] = word1.count(chr(i))
+        alph2[i - 65] = word2.count(chr(i))
+    for i in range(97, 123):
+        alph1[i - 97] = word1.count(chr(i))
+        alph2[i - 97] = word2.count(chr(i))
+    if alph1 == alph2:
+        return True
     return False
