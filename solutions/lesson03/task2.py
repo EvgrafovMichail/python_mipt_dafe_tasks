7768f428220c9de9214fae327671eaa59198e7dd
Ilya Ryabokon <ryabokon_ilya_2007@mail.ru>
2025-10-24 21:37:42 +0300

    Ryabokon Ilya Group B12-514 lesson5 tasks 1-6 fix2

--- solutions/lesson03/task2.py
+++ solutions/lesson03/task2.py
@@ -1,33 +1,34 @@
 def get_cube_root(n: float, eps: float) -> float:
-    if n == 0:
+    if n == 0.0:
         return 0.0
 
-    if n > 0:
-        if n < 1:
-            left = 0.0
-            right = 1.0
-        else:
-            left = 0.0
-            right = max(1.0, n)
+    if n > 0.0:
+        left = 0.0
+        right = max(1.0, n)
     else:
-        if n > -1:
-            left = -1.0
-            right = 0.0
-        else:
-            left = min(-1.0, n)
-            right = 0.0
+        left = min(-1.0, n)
+        right = 0.0
 
-    # Бинарный поиск
-    while abs(right - left) > eps:
-        mid = (left + right) / 2
+    max_iters = 2000
+    iteration = 0
+    while iteration < max_iters:
+        mid = (left + right) / 2.0
         mid_3 = mid * mid * mid
+
+        if abs(mid_3 - n) <= eps:
+            return mid
 
         if mid_3 < n:
             left = mid
         else:
             right = mid
 
-    return (left + right) / 2
+        if abs(right - left) <= 1e-15:
+            return (left + right) / 2.0
+
+        it += 1
+
+    return (left + right) / 2.0
 
 
 # print(get_cube_root(float(input("n = ")), float(input("eps = "))))
