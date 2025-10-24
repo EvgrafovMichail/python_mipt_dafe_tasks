106407c98893668061a3f9b60c98e15792cba03b
Ssssserge <yanchevskiy07@gmail.com>
2025-10-24 23:51:50 +0300

    Контест 5

--- solutions/lesson05/task4.py
+++ solutions/lesson05/task4.py
@@ -1,3 +1,12 @@
 def unzip(compress_text: str) -> str:
-    # ваш код
-    return compress_text
+    text = ""
+    for token in compress_text.split():
+        if "*" in token:
+            word, count = token.split("*")
+            text += word * int(count)
+        else:
+            text += token
+    return text
+
+
+print(unzip("a*3 b*2"))
