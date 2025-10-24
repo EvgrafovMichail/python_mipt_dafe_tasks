a368751eff38a670c7176f5eeeb83654db9a9038
Kemal <f9ka9@mail.ru>
2025-10-25 00:42:26 +0300

    Home_work_lesson5_Kamaleev_Kemal

--- solutions/lesson03/task3.py
+++ solutions/lesson03/task3.py
@@ -1,14 +1,14 @@
 def get_nth_digit(num: int) -> int:
     n = num
     length = 1
-    count = 5  
+    count = 5
     start = 0
 
     while n > length * count:
         n -= length * count
         length += 1
         start = 10 ** (length - 1)
-        end = 10 ** length - 1
+        end = 10**length - 1
         count = ((end - start) // 2) + 1
 
     idx = (n - 1) // length
