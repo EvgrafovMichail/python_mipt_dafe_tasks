0f9e7d95fcb35a57afd8ca9449edacb93a6d17af
murmassa <egorgalkin909@gmail.com>
2025-10-24 23:29:54 +0300

    dz lesson05

--- solutions/lesson05/task6.py
+++ solutions/lesson05/task6.py
@@ -1,3 +1,28 @@
 def simplify_path(path: str) -> str:
-    # ваш код
-    return path
+    # разделять строку по символам / и потом пустые строки удалять, точки заменять и все скреплять через матч кейс
+    # чситка списка 
+    list = path.split("/")
+    i = 0
+    while i < len(list):
+        match list[i]:
+            case ""|".": list.pop(i)
+            case "..":
+                if i == 0: return ""
+                else:
+                    list.pop(i)
+                    list.pop(i-1)
+                    i -= 1
+            case _: i+=1
+
+
+    # блок сборки строки 
+
+    final = ""
+    for i in range(len(list)):
+        final = final + "/" + list[i]
+
+
+    # если в итоге мы ничего не сделали и остались в той же дирректории то вернем "/"
+    if final == "":
+        final = "/"
+    return final
