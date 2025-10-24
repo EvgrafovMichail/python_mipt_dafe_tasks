f8f37fd04de34a5030bcb8a4214c5834ebd97721
Provoda78 <nikbel.2007@mail.ru>
2025-10-24 23:45:26 +0300

    Task07

--- solutions/lesson05/task1.py
+++ solutions/lesson05/task1.py
@@ -1,3 +1,11 @@
 def is_palindrome(text: str) -> bool:
-    # ваш код
-    return False
+    if not text:
+        return True
+    
+    str_low = text.lower()
+    if str_low[0] == str_low[-1]:
+        return True
+    else:
+        return False
+    
+#print(is_palindrome(input("Введите палиндром ")))
