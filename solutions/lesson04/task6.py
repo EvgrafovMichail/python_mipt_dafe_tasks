ffdf32898a490aed1e3854b983adca8931e7adbd
dandandil <daniil182403@gmail.com>
2025-10-24 20:06:19 +0000

    tasks 1 2 3 4 6

--- solutions/lesson04/task6.py
+++ solutions/lesson04/task6.py
@@ -1,9 +1,8 @@
 def count_cycles(arr: list[int]) -> int:
     cnt = 0
-    mas = [0 for i in range(len(arr))]
     for i in range(len(arr)):
+        mas = [0 for i in range(len(arr))]
         while mas[arr[i]] != 1:
             mas[i], i = 1, arr[i]
         cnt += 1
-        mas = [0 for i in range(len(arr))]
     return cnt
--- solutions/lesson05/task5.py
+++ solutions/lesson05/task5.py
@@ -1,3 +1,30 @@
-def reg_validator(reg_expr: str, text: str) -> bool:  
-    # ваш код
-    return False
+def reg_validator(reg_expr: str, text: str) -> bool:
+    i = 0
+    j = 0
+    while i + 1 < len(reg_expr) and j + 1 < len(text):
+        if reg_expr[i] == "d":
+            while j < len(text) and text[j].isdigit():
+                j += 1
+            if not text[j].isdigit():
+                return False
+        if reg_expr[i] == "w":
+            while j < len(text) and text[j].isalpha():
+                j += 1
+            if text[j].isalpha():
+                return False
+        if reg_expr[i] == "s":
+            while j < len(text) and text[j].isalpha() or text[j].isdigit():
+                j += 1
+            if not (text[j].isalpha() or text[j].isdigit()):
+                return False
+        if not reg_expr[i].isalpha() and not reg_expr[i].isdigit():
+            if reg_expr[i] == text[j]:
+                j += 1
+                i += 1
+            else:
+                return False
+        i += 1
+    return i == len(reg_expr) and j == len(text)
+
+
+# print(reg_validator(input(), input()))
