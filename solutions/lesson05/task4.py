4732b26fc8db37e1dd61fd34e7ca9e40f0c9ce32
Savely <savvatrin@gmail.com>
2025-10-24 18:26:42 +0300

    RESH

--- solutions/lesson05/task4.py
+++ solutions/lesson05/task4.py
@@ -1,11 +1,11 @@
 def unzip(compress_text: str) -> str:
-    mass = compress_text.split( )
-    summ = ''
-    if len(compress_text)<=2:
+    mass = compress_text.split()
+    summ = ""
+    if len(compress_text) <= 2:
         return compress_text
     for i in mass:
-        if '*' in i:
-            summ += i.split('*')[0] * int(i.split('*')[1])
+        if "*" in i:
+            summ += i.split("*")[0] * int(i.split("*")[1])
         else:
             summ += i
     return summ
