4b33e1609a73971a7f199bc63dd6318f9019e5dc
Denis1325 <penkovdenis271@gmail.com>
2025-10-25 01:31:08 +0300

    sol1

--- solutions/lesson05/task1.py
+++ solutions/lesson05/task1.py
@@ -1,6 +1,6 @@
 def is_palindrome(text: str) -> bool:
     text = text.lower()
-    for i in range(len(text)//2):
-        if text[i] != text[-i-1]:
+    for i in range(len(text) // 2):
+        if text[i] != text[-i - 1]:
             return False
     return True
