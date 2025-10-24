ad6fc3ed9d6a2bbaa0d04ce946e540633861d674
melanikot <kotova.mv@phystech.edu>
2025-10-24 08:18:45 +0300

    less5

--- solutions/lesson05/task4.py
+++ solutions/lesson05/task4.py
@@ -1,3 +1,19 @@
 def unzip(compress_text: str) -> str:
-    # ваш код
-    return compress_text
+    lst = compress_text.split(" ")
+    unzip_lst = []
+    for i in lst:
+        s = i.split("*")
+        if len(s) > 1:
+            unzip_lst.append(s[0] * int(s[1]))
+        else:
+            unzip_lst.append(s[0])
+
+    unzip_text = ""
+
+    for i in unzip_lst:
+        unzip_text += i
+
+    return unzip_text
+
+
+# print(unzip("AbcD*4 ef GhI*2"))
