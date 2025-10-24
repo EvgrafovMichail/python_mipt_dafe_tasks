70ed145f74c10c17d7be081eb4424b1751a4ed9b
Mihail Barinov <dmemsmdmemsm@gmail.com>
2025-10-24 23:57:17 +0300

    Add solutions for lesson 5

--- solutions/lesson05/task4.py
+++ solutions/lesson05/task4.py
@@ -1,3 +1,9 @@
 def unzip(compress_text: str) -> str:
-    # ваш код
-    return compress_text
+    result = ""
+    for part in compress_text.split():
+        if "*" in part:
+            substr, repeat_num = part.split("*")
+            result += substr * int(repeat_num)
+        else:
+            result += part
+    return result
--- solutions/lesson05/task6.py
+++ solutions/lesson05/task6.py
@@ -1,3 +1,13 @@
 def simplify_path(path: str) -> str:
-    # ваш код
-    return path
+    current_path = []
+    for part in path.split("/"):
+        if part == "..":
+            if current_path:
+                current_path.pop()
+            else:
+                return ""
+        elif part == "." or part == "":
+            continue
+        else:
+            current_path.append(part)
+    return ("/" + "/".join(current_path)) if current_path else "/"
