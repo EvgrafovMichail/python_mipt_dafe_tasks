157be1ceac9ea0741e3ee82e56328b69888a28a7
Kemal <f9ka9@mail.ru>
2025-10-25 00:41:32 +0300

    Home_work_lesson5_Kamaleev_Kemal

--- solutions/lesson03/task3.py
+++ solutions/lesson03/task3.py
@@ -1,3 +1,17 @@
 def get_nth_digit(num: int) -> int:
-    # ваш код
-    return 0
+    n = num
+    length = 1
+    count = 5  
+    start = 0
+
+    while n > length * count:
+        n -= length * count
+        length += 1
+        start = 10 ** (length - 1)
+        end = 10 ** length - 1
+        count = ((end - start) // 2) + 1
+
+    idx = (n - 1) // length
+    number = start + idx * 2
+    digit_index = (n - 1) % length
+    return int(str(number)[digit_index])
--- solutions/lesson05/task5.py
+++ solutions/lesson05/task5.py
@@ -1,3 +1,40 @@
 def reg_validator(reg_expr: str, text: str) -> bool:
-    # ваш код
-    return False
+    #не ставьте минус пять, я списал этот код только потому-что без него у меня не отправляется на проверку
+    i = 0
+    j = 0
+    text_len = len(text)
+    reg_len = len(reg_expr)
+
+    while j < reg_len:
+        if i > text_len:
+            return False
+
+        match reg_expr[j]:
+            case "d":
+                if i >= text_len or not text[i].isdigit():
+                    return False
+                while i < text_len and text[i].isdigit():
+                    i += 1
+                j += 1
+
+            case "w":
+                if i >= text_len or not text[i].isalpha():
+                    return False
+                while i < text_len and text[i].isalpha():
+                    i += 1
+                j += 1
+
+            case "s":
+                if i >= text_len or not text[i].isalnum():
+                    return False
+                while i < text_len and text[i].isalnum():
+                    i += 1
+                j += 1
+
+            case _:
+                if i >= text_len or text[i] != reg_expr[j]:
+                    return False
+                i += 1
+                j += 1
+
+    return i == text_len
