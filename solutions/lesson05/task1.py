a991f22bc2e629d0939591f0711104162a958011
kirill <qwerty.0045@inbox.ru>
2025-10-24 23:35:29 +0300

    Hometask5

--- solutions/lesson05/task1.py
+++ solutions/lesson05/task1.py
@@ -1,3 +1,15 @@
 def is_palindrome(text: str) -> bool:
-    # ваш код
-    return False
+    i = len(text) - 1
+    j = 0
+
+    while i > j:
+        if abs(ord(text[i]) - ord(text[j])) == 32:
+            i -= 1
+            j += 1
+            continue
+        if text[i] != text[j]:
+            return False
+        i -= 1
+        j += 1
+
+    return True
