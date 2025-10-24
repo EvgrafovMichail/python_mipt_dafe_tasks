e69f273375e3262f44f9e301c6f001635fb5da4c
Vladimir <vl.mukhin21@gmail.com>
2025-10-24 19:18:40 +0300

    Сделано задание 6

--- solutions/lesson05/task6.py
+++ solutions/lesson05/task6.py
@@ -1,3 +1,22 @@
 def simplify_path(path: str) -> str:
-    # ваш код
-    return path
+    splitted_path = []
+    for sub_str in path.split("/"):
+        if (sub_str != "") and (sub_str != "."):
+            splitted_path.append(sub_str)
+
+    cleaned_path = []
+    for dir in splitted_path:
+        if dir != "..":
+            cleaned_path.append(dir)
+        else:
+            if cleaned_path == []:
+                return ""
+            cleaned_path.pop()
+
+    if cleaned_path == []:
+        return "/"
+
+    out_path = ""
+    for name in cleaned_path:
+        out_path += "/" + name
+    return out_path
