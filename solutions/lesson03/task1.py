6cac897cc102efbe094fcf72dfb632e4b9445663
Profizmatik <undel.xo17@gmail.com>
2025-10-24 15:25:16 +0300

    resave task4

--- solutions/lesson03/task1.py
+++ solutions/lesson03/task1.py
@@ -1,16 +1,15 @@
 def flip_bits_in_range(num: int, left_bit: int, right_bit: int) -> int:
     position = left_bit
-    
-    while position <= right_bit:
-        bit_weight = 2 ** (position - 1)         
-        bit_value = (num // bit_weight) % 2          
-        
-        if bit_value == 0:
-            num += bit_weight  
-        else:
-            num -= bit_weight 
-        
-        position += 1
-    
-    return num 
 
+    while position <= right_bit:
+        bit_weight = 2 ** (position - 1)
+        bit_value = (num // bit_weight) % 2
+
+        if bit_value == 0:
+            num += bit_weight
+        else:
+            num -= bit_weight
+
+        position += 1
+
+    return num
