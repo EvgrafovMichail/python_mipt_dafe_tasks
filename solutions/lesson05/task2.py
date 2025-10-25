36de74211e13a51e8e6a499345c963840cc72dc7
Poco-dev <vueprojector5@gmail.com>
2025-10-25 03:14:37 +0300

    colloquium day

--- solutions/lesson05/task2.py
+++ solutions/lesson05/task2.py
@@ -1,11 +1,7 @@
 def are_anagrams(word1: str, word2: str) -> bool:
-    if len(word1) != len(word2):
-        return False
     a = [0] * 58
     for i in word1:
         a[ord(i) - 65] += 1
     for i in word2:
-        if a[ord(i) - 65] == 0:
-            return False
         a[ord(i) - 65] -= 1
-    return True
+    return all(x == 0 for x in a)
