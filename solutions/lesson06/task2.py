f2919b062d51d5b990f6c7516621000d8319cf0d
Vladimir <vl.mukhin21@gmail.com>
2025-10-26 01:00:15 +0300

    Сделано задание 2

--- solutions/lesson06/task2.py
+++ solutions/lesson06/task2.py
@@ -1,3 +1,17 @@
 def get_len_of_longest_substring(text: str) -> int:
-    # ваш код
-    return 0
+    if text == "":
+        return 0
+
+    max_len = 1
+    nums = []
+
+    for start_ind in range(len(text)):
+        for i in range(start_ind, len(text)):
+            if text[i] not in nums:
+                nums.append(text[i])
+            else:
+                max_len = max(max_len, len(nums))
+                nums = []
+                break
+
+    return max_len
