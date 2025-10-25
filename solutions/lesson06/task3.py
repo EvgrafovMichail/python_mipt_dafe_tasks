f0cab0d9fa506a7448bc3059d87791771295f9d5
Vladimir <vl.mukhin21@gmail.com>
2025-10-26 01:26:28 +0300

    Сделано задание 3

--- solutions/lesson06/task3.py
+++ solutions/lesson06/task3.py
@@ -1,7 +1,13 @@
-def is_there_any_good_subarray(
-    nums: list[int],
-    k: int,
-) -> bool:
-    
-    # ваш код
+def is_there_any_good_subarray(nums: list[int], k: int) -> bool:
+    if (nums == "") or (k == 0):
+        return False
+
+    for start_ind in range(len(nums)):
+        sum = 0
+
+        for i in range(start_ind, len(nums)):
+            sum += nums[i]
+            if (i != start_ind) and (sum % k == 0):
+                return True
+
     return False
