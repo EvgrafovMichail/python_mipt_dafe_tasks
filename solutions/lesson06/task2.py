31da8867a0d3e29eece671f2bb0a55c6cbe3c9ae
Vladimir <vl.mukhin21@gmail.com>
2025-10-26 11:42:17 +0300

    Изменено название переменной

--- solutions/lesson06/task2.py
+++ solutions/lesson06/task2.py
@@ -3,15 +3,15 @@ def get_len_of_longest_substring(text: str) -> int:
         return 0
 
     max_len = 1
-    nums = []
+    sub_str = []
 
     for start_ind in range(len(text)):
         for i in range(start_ind, len(text)):
-            if text[i] not in nums:
-                nums.append(text[i])
+            if text[i] not in sub_str:
+                sub_str.append(text[i])
             else:
-                max_len = max(max_len, len(nums))
-                nums = []
+                max_len = max(max_len, len(sub_str))
+                sub_str = []
                 break
 
     return max_len
