68f4ddef3877c05ec6b26d5a377c7ad95238e65f
Prosto_Max_xX <maxim.nevinsky@yandex.ru>
2025-10-27 16:20:36 +0300

    made tasks for 6 lesson

--- solutions/lesson06/task3.py
+++ solutions/lesson06/task3.py
@@ -2,6 +2,18 @@ def is_there_any_good_subarray(
     nums: list[int],
     k: int,
 ) -> bool:
-    
-    # ваш код
+    for i in range(2, len(nums) + 1):
+        for j in range(len(nums) - i + 1):
+            summar = nums[j]
+            for elem in range(1, i):
+                summar += nums[j + elem]
+            if is_multiples(summar, k):
+                return True
     return False
+
+
+def is_multiples(num1: int, num2: int) -> bool:
+    if num1 == 0 or num2 == 0:
+        return True
+    return num1 % num2 == 0
+
