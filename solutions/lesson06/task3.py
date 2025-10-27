c1360df94474110f92724ad4996a97176f45565b
minskiyilja-dot <pooneee819@gmail.com>
2025-10-27 23:30:06 +0300

    homework lesson6

--- solutions/lesson06/task3.py
+++ solutions/lesson06/task3.py
@@ -2,6 +2,9 @@ def is_there_any_good_subarray(
     nums: list[int],
     k: int,
 ) -> bool:
-    
-    # ваш код
+    for i in range(len(nums)):
+        for j in range(i, len(nums)):
+            if i != j:
+                if sum(nums[i:j+1]) % k == 0:
+                    return True
     return False
