b028637c4efc1f78bf16f3ac4fac763fceb96380
Rafk123 <tabunanov.rr@phystech.edu>
2025-10-28 20:55:23 +0300

    the_fifth_contest

--- solutions/lesson06/task4.py
+++ solutions/lesson06/task4.py
@@ -1,3 +1,10 @@
 def count_unique_words(text: str) -> int:
-    # ваш код
-    return 0
+    text = text.lower()
+    punctuation_marks = "!\"#$%&()*+,-./:;<=>?@[]^_{|}~`'\\"
+    for mark in punctuation_marks:
+        text = text.replace(mark, "")
+    words = text.split()
+    dictionary = {}
+    for word in words:
+        dictionary[word] = 1
+    return len(dictionary)
