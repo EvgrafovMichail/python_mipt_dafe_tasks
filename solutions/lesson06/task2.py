d80a29408be40a5544893e250e6544fea1a47ed4
gubanovily66-beep <gubanovily66@gmail.com>
2025-10-29 22:09:36 +0300

    les6

--- solutions/lesson06/task2.py
+++ solutions/lesson06/task2.py
@@ -1,3 +1,17 @@
 def get_len_of_longest_substring(text: str) -> int:
-    # ваш код
-    return 0
+    if not text:
+        return 0
+
+    char_set = set()
+    max_length = 0
+    left = 0
+
+    for right in range(len(text)):
+        while text[right] in char_set:
+            char_set.remove(text[left])
+            left += 1
+        char_set.add(text[right])
+
+        max_length = max(max_length, right - left + 1)
+
+    return max_length
