9bf317d075753f540031fd64d887dcb1a876f7a3
Vyacheslav A <aleksakhin.viu@phystech.edu>
2025-10-30 15:11:04 +0300

    Solutions for every task

--- solutions/lesson06/task2.py
+++ solutions/lesson06/task2.py
@@ -1,3 +1,19 @@
 def get_len_of_longest_substring(text: str) -> int:
     # ваш код
-    return 0
+
+    res = 0
+    left = 0
+    unique_symbols = set()
+
+    for right in range(len(text)):
+        right_char = text[right]
+
+        while right_char in unique_symbols:
+            left_char = text[left]
+            unique_symbols.remove(left_char)
+            left += 1
+
+        unique_symbols.add(right_char)
+        res = max(res, right - left + 1)
+
+    return res
