51b951b9888f97a00b8f1f57d257155b418bcb7e
varlag <varya_lagutina@mail.ru>
2025-10-30 21:35:43 +0000

    lesson06

--- solutions/lesson06/task3.py
+++ solutions/lesson06/task3.py
@@ -1,7 +1,18 @@
 def is_there_any_good_subarray(
-    nums: list[int],
-    k: int,
+    s: list[int],
+    n: int,
 ) -> bool:
     
-    # ваш код
+    my_dict = {}
+    sum = 0
+    if len(s) == 2: 
+        return (s[0] + s[1]) % n == 0
+    
+    for i in range(len(s)):
+        sum += s[i]
+        if (sum % n) not in my_dict:
+            my_dict[sum % n] = i
+        else:
+            if i - my_dict[sum % n] >= 2:
+                return True
     return False
--- solutions/lesson06/task4.py
+++ solutions/lesson06/task4.py
@@ -1,3 +1,13 @@
-def count_unique_words(text: str) -> int:
-    # ваш код
-    return 0
+def count_unique_words(s: str) -> int:
+    import string
+
+    s = s.split()
+
+    unique_words = set()
+
+    for word in s:
+        word = word.lower().strip(string.punctuation)
+        if word != '':
+            unique_words.add(word)
+
+    return len(unique_words)
