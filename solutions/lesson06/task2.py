94b14b1ee7032b6ee09b1c4945682b9b76b8c681
Shanechka <grisacernyh@gmail.com>
2025-10-31 14:52:21 +0300

    01.11.2025 Homework

--- solutions/lesson06/task2.py
+++ solutions/lesson06/task2.py
@@ -1,3 +1,13 @@
 def get_len_of_longest_substring(text: str) -> int:
-    # ваш код
-    return 0
+    start = 0
+    maxlen = 0
+    while start < len(text):
+        substr = ""
+        for i in range(start, len(text)):
+            if text[i] not in substr:
+                substr += text[i]
+            else:
+                break
+        maxlen = max(maxlen, len(substr))
+        start += 1
+    return maxlen
--- solutions/lesson06/task3.py
+++ solutions/lesson06/task3.py
@@ -1,7 +1,19 @@
-def is_there_any_good_subarray(
-    nums: list[int],
-    k: int,
-) -> bool:
-    
-    # ваш код
+def is_there_any_good_subarray(nums: list[int], k: int) -> bool:
+    if len(nums) < 2:
+        return False
+    elif len(nums) == 2:
+        sum = 0
+        for subarr in nums:
+            sum += subarr
+        if sum % k == 0:
+            return True
+        else:
+            return False
+    for i in range(0, len(nums) - 1):
+        for j in range(i + 2, len(nums) + 1):
+            sum = 0
+            for subarr in nums[i:j]:
+                sum += subarr
+            if sum % k == 0:
+                return True
     return False
