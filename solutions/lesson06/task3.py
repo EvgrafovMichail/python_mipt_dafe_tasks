1e175db85e7f3ec8d78434e56466823410565a16
Ilya Ryabokon <ryabokon_ilya_2007@mail.ru>
2025-10-31 23:35:29 +0300

    Ryabokon Ilya Group B12-514 lesson5 and lesson6 (The lesson5 was solved on last week, but I decided to re-upload it with a new task for convenience

--- solutions/lesson06/task3.py
+++ solutions/lesson06/task3.py
@@ -2,6 +2,15 @@ def is_there_any_good_subarray(
     nums: list[int],
     k: int,
 ) -> bool:
-    
-    # ваш код
+    now_remains = 0
+    processing = set()
+    last_remains = 0
+
+    for num in nums:
+        now_remains = (now_remains + num) % k
+        if now_remains in processing:
+            return True
+        processing.add(last_remains)
+        last_remains = now_remains
+
     return False
