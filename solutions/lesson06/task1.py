4574d22169dbe008d610e57d186856e84cacbf2e
bulka0007 <bulat.ramazanov.2000@bk.ru>
2025-10-31 10:43:41 +0300

    lesson06 ccontest5 Ramazanov 512

--- solutions/lesson06/task1.py
+++ solutions/lesson06/task1.py
@@ -1,3 +1,22 @@
 def int_to_roman(num: int) -> str:
-    # ваш код
-    return ""
+    res = ''
+    razr = ['IVX', 'XLC', 'CDM', 'M--']
+    for i in range(3, -1, -1):
+        power = 10 ** i
+        digit = num // power
+        num %= power
+        if digit == 0:
+            continue
+        s = ''
+        if digit == 9:
+            s += 'ix'
+        elif digit >= 5:
+            s += 'v' + 'i' * (digit - 5)
+        elif digit == 4:
+            s += 'iv'
+        else:
+            s += 'i' * digit
+        for j in range(3):
+            s = s.replace('ivx'[j], razr[i][j])
+        res += s
+    return res
