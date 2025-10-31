d648e717e1f03fd1d760ae5ca1e9fa0f4984ea2b
dryzesolo <temirbulatovamin12@gmail.com>
2025-10-31 19:44:08 +0300

    Темирбулатов Амин Б12-514

--- solutions/lesson06/task1.py
+++ solutions/lesson06/task1.py
@@ -1,3 +1,27 @@
 def int_to_roman(num: int) -> str:
-    # ваш код
-    return ""
+    dict = {
+        1000: "M",
+        900: "CM",
+        500: "D",
+        400: "CD",
+        100: "C",
+        90: "XC",
+        50: "L",
+        40: "XL",
+        10: "X",
+        9: "IX",
+        5: "V",
+        4: "IV",
+        1: "I",
+    }
+    otv = ""
+    while num > 0:
+        for i in dict.keys():
+            if num >= i:
+                num -= i
+                otv += dict[i]
+                break
+    return otv
+
+
+print(int_to_roman(27))
--- solutions/lesson06/task3.py
+++ solutions/lesson06/task3.py
@@ -2,6 +2,18 @@ def is_there_any_good_subarray(
     nums: list[int],
     k: int,
 ) -> bool:
-    
+    dict = {}
+    s = 0
+    for i in range(len(nums)):
+        s += nums[i]
+        c = s % k
+        if c == 0 and i > 0:
+            return True
+        if c in dict.keys():
+            if i - dict[c] >= 2:
+                return True
+            dict[c] = min(dict[c], i)
+        else:
+            dict[c] = i
     # ваш код
     return False
