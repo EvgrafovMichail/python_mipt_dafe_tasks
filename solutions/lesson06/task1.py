bf3d63efbff12c4f3b812502554d17fbbb794af5
dandandil <daniil182403@gmail.com>
2025-10-31 16:38:36 +0000

    les6 tasks 1-4 ready

--- solutions/lesson06/task1.py
+++ solutions/lesson06/task1.py
@@ -1,3 +1,17 @@
 def int_to_roman(num: int) -> str:
-    # ваш код
-    return ""
+    ans = ""
+    i = 0
+    roman = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
+    while num:
+        if num % 10:
+            if num % 10 == 9:
+                ans = roman[1 * 10**i] + roman[10 * 10**i] + ans
+            elif num % 10 > 4:
+                ans = roman[5 * 10**i] + roman[1 * 10**i] * ((num % 10) - 5) + ans
+            elif num % 10 == 4:
+                ans = roman[1 * 10**i] + roman[5 * 10**i] + ans
+            else:
+                ans = roman[1 * 10**i] * (num % 10) + ans
+        i += 1
+        num //= 10
+    return ans
