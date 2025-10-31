0057e59f760f32771f2397ed0139591daed723d4
Акбулатов Артур Фаилевич <akbulatov_2007@mail.ru>
2025-10-31 15:59:26 +0300

    дз

--- solutions/lesson06/task3.py
+++ solutions/lesson06/task3.py
@@ -2,6 +2,8 @@ def is_there_any_good_subarray(
     nums: list[int],
     k: int,
 ) -> bool:
-    
-    # ваш код
+    for lenpods in range(1, len(nums) + 1):
+        for i in range(len(nums) - lenpods):
+            if sum(nums[i : i + lenpods + 1]) % k == 0:
+                return True
     return False
