950ed8079985ffe4a6585c1fea9d77bb82741490
Robert <robert.ibr0407@gmail.com>
2025-10-31 19:10:52 +0300

    Ибрагимов Роберт Б12-513 lesson06

--- solutions/lesson05/task3.py
+++ solutions/lesson05/task3.py
@@ -1,3 +1,8 @@
 def is_punctuation(text: str) -> bool:
-    # ваш код
-    return False
+    punctuation = "!\"#$%&'()*+,-./:;<=>?@[\]^_{|}~`"
+    if len(text) == 0:
+        return False
+    for i in text:
+        if i not in punctuation:
+            return False
+    return True
--- solutions/lesson06/task2.py
+++ solutions/lesson06/task2.py
@@ -1,3 +1,20 @@
 def get_len_of_longest_substring(text: str) -> int:
-    # ваш код
-    return 0
+    if not text:
+        return 0
+    unic_symbols = set()
+    lengths = set()
+    length_sub = 0
+    i = 0
+    while i < len(text):
+        if text[i] not in unic_symbols:
+            unic_symbols.add(text[i])
+            length_sub += 1
+            i += 1
+        else:
+            lengths.add(length_sub)
+            unic_symbols.remove(text[i - length_sub])
+            length_sub -= 1
+    lengths.add(length_sub)
+    return max(lengths)
+
+
