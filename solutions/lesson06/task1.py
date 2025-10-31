bf446426c3a923dfc141cae7675134710fb85511
murmassa <egorgalkin909@gmail.com>
2025-10-31 23:21:20 +0300

    lesson06

--- solutions/lesson06/task1.py
+++ solutions/lesson06/task1.py
@@ -1,3 +1,28 @@
 def int_to_roman(num: int) -> str:
-    # ваш код
-    return ""
+    # составяем список из числа и создаем список для римских цифр 
+    list_num = list(str(num))
+    grk_letters = ["I", "V", "X", "L", "C", "D", "M", "M", "M"]
+
+    # индекс для вывода нужных римских символов и str строка вывода
+    j = 0 
+    strin = ""
+
+
+    # по количеству символов далее начинаем составлять итоговое выражение 
+    for i in range(len(list_num)):
+
+        letter1 = grk_letters[j]
+        letter2 = grk_letters[j+1]
+        letter3 = grk_letters[j+2]
+
+        index = int(  list_num[  len(list_num)-i-1  ])
+
+        # блок для вычисления итогового числа в римских числах
+        if index < 4:     strin = letter1 * index + strin
+        elif index == 4:  strin = letter1 + letter2 + strin
+        elif index < 9:   strin = letter2 + (index - 5) * letter1 + strin
+        elif index == 9 : strin = letter1 + letter3 + strin
+        else:             strin = letter3 + strin
+        j+=2
+
+    return strin
--- solutions/lesson06/task2.py
+++ solutions/lesson06/task2.py
@@ -1,3 +1,27 @@
 def get_len_of_longest_substring(text: str) -> int:
-    # ваш код
-    return 0
+    # проверяем символы толкая вперед right 
+    # если символ повторился то притягиваем left на место повтора
+    left = 0
+    max = 0 # длинна самой длинной последовательности 
+    unique_symbols = {} # словарь с каждой буквой и ее порядковым номером 
+
+
+    for right in range(len(text)):
+        symbols = text[right]
+
+        # перетяжка left
+        if symbols in unique_symbols:
+            left = unique_symbols[symbols] + 1
+
+        # если все гуд то увеличиваем расстояние между right и left 
+        # также имеем ввиду max чтобы запоминать самую длинну последовательнось
+        else:
+            if max <= (right - left):
+                max += 1
+        
+        # запоминаем или обновляем порядковый номер буквы
+        unique_symbols[symbols] = right
+
+        
+
+    return max
