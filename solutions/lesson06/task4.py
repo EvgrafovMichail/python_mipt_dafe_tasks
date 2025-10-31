f21e587cf92c2b7e891ad314a17a807dcf80a852
Mihail Barinov <dmemsmdmemsm@gmail.com>
2025-10-31 23:42:05 +0300

    Add solutions for lesson 6

--- solutions/lesson06/task4.py
+++ solutions/lesson06/task4.py
@@ -1,3 +1,12 @@
+import string
+
+
 def count_unique_words(text: str) -> int:
-    # ваш код
-    return 0
+    unique_words = set()
+
+    for word in text.lower().split():
+        word = word.strip(string.punctuation)
+        if word not in unique_words and word != "":
+            unique_words.add(word)
+
+    return len(unique_words)
