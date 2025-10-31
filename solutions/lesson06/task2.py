a08405f47e4b1769460017d18f41f76a8503e47f
Vandam-create <beliakov.va@phystech.edu>
2025-10-31 23:03:09 +0300

    V1

--- solutions/lesson06/task2.py
+++ solutions/lesson06/task2.py
@@ -1,3 +1,20 @@
 def get_len_of_longest_substring(text: str) -> int:
-    # ваш код
-    return 0
+    max_len = 0
+    same = dict()
+    max_l = list()
+
+    if not text:
+        return 0
+
+    for i in range(0, len(text)):
+        counter = i
+        while counter < len(text) and not text[counter] in same:
+            same.update({text[counter]: "T"})
+            counter += 1
+            max_len += 1
+
+        max_l.append(max_len)
+        same.clear()
+        max_len = 0
+
+    return max(max_l)
--- solutions/lesson06/task4.py
+++ solutions/lesson06/task4.py
@@ -1,3 +1,12 @@
 def count_unique_words(text: str) -> int:
-    # ваш код
-    return 0
+    text2 = "".join([char for char in text if char.isalnum() or char == " "])
+    text2 = text2.lower()
+    split_str = text2.split(" ")
+    split_str2 = []
+    for i in split_str:
+        if i != "":
+            split_str2.append(i)
+
+    s = set(split_str2)
+
+    return len(s)
