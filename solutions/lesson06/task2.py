ed7e6e12617bb7a0c62b9ca91f44edd3e18cd826
Oleg <rozomazan069@gmail.com>
2025-10-31 15:56:17 +0300

    complete homework to dicts

--- solutions/lesson06/task2.py
+++ solutions/lesson06/task2.py
@@ -1,3 +1,12 @@
 def get_len_of_longest_substring(text: str) -> int:
-    # ваш код
-    return 0
+    symb = {}
+    maxlen = 0
+    left = 0
+    for right in range(len(text)):
+        while text[right] in symb:
+            symb.pop(text[left])
+            left += 1
+        if right - left + 1 > maxlen:
+            maxlen = right - left + 1
+        symb[text[right]] = right
+    return maxlen
