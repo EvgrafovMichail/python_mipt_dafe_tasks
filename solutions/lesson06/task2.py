4aaf677d0b65f61f544b5d176c6d486765825208
BogdanYeti <unrelise_original@mail.ru>
2025-10-31 19:17:05 +0400

    solutions for lesson06

--- solutions/lesson06/task2.py
+++ solutions/lesson06/task2.py
@@ -1,3 +1,20 @@
 def get_len_of_longest_substring(text: str) -> int:
-    # ваш код
-    return 0
+    d, cur, rec = dict(), str(), int()
+    for i in text:
+        ind = d.get(i, -1)
+        if ind == -1:
+            cur += i
+            d[i] = len(cur)
+            rec = max(rec, len(cur))
+        else:
+            cur = cur[ind:]
+            del d[i]
+            d = {i: d[i] - ind for i in d.keys()}
+            cur += i
+            d[i] = len(cur)
+    rec = max(rec, len(cur))
+    return rec
+
+
+if __name__ == "__main__":
+    print(get_len_of_longest_substring("abcabcbbxyz"))
--- solutions/lesson06/task4.py
+++ solutions/lesson06/task4.py
@@ -1,3 +1,24 @@
 def count_unique_words(text: str) -> int:
-    # ваш код
-    return 0
+    words = text.split()
+    d = {}
+    i = 0
+    for word in words:
+        word = word.lower()
+        while word:
+            if not (
+                65 <= ord(word[0]) <= 90 or 97 <= ord(word[0]) <= 122 or 48 <= ord(word[0]) <= 57
+            ):
+                word = word[1:]
+            else:
+                break
+        while word:
+            if not (
+                65 <= ord(word[-1]) <= 90 or 97 <= ord(word[-1]) <= 122 or 48 <= ord(word[-1]) <= 57
+            ):
+                word = word[:-1]
+            else:
+                break
+        if word not in d and word:
+            i += 1
+            d[word] = int()
+    return i
