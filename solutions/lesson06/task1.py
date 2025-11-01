51b932984cc25cd6d4bcedd0c9a2eacc0e2a6ffc
korolinakila <matvey.makarov07@mail.ru>
2025-11-01 20:59:27 +0300

    B12-514 Makarov Matvey

--- solutions/lesson06/task1.py
+++ solutions/lesson06/task1.py
@@ -1,3 +1,15 @@
 def int_to_roman(num: int) -> str:
-    # ваш код
-    return ""
+    rom_dict = {
+        1000: 'M', 900: 'CM', 500: 'D', 400: 'CD',
+        100: 'C', 90: 'XC', 50: 'L', 40: 'XL',
+        10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'
+    }
+
+    values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    
+    res = []
+    for value in values:
+        while num >= value:
+            result.append(rom_dict[value])
+            num -= value
+    return ''.join(res)
--- solutions/lesson06/task4.py
+++ solutions/lesson06/task4.py
@@ -1,3 +1,8 @@
 def count_unique_words(text: str) -> int:
-    # ваш код
-    return 0
+    text = text.lower()
+    res = ""
+    for i in range(len(text)):
+        if (text[i] >= 'a' and text[i] <= 'z') or (text[i] >= '0' and text[i] <= '9') or text[i] == " ":
+            res += text[i]
+
+    return len(set(res.split()))
