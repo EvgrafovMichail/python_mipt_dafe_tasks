83ad9d4aa4ef5076e66b64c0437fc46028f08b8b
Kodik Igor <ikodik34@gmail.com>
2025-11-09 03:09:17 +0300

    tasks for lesson 08

--- solutions/lesson08/task1.py
+++ solutions/lesson08/task1.py
@@ -1,5 +1,24 @@
 from typing import Callable
+from collections import deque
+import math
+
 
 def make_averager(accumulation_period: int) -> Callable[[float], float]:
-    # ваш код
-    pass
+    summa = 0
+    number = 0
+    deq = deque()
+
+    def get_avg(value: float) -> float:
+        nonlocal summa, number, deq
+        if number < accumulation_period:
+            number += 1
+            print(number)
+        else:
+            summa -= deq.popleft()
+            print(number)
+        summa += value
+        deq.append(value)
+        print(deq)
+        return summa / number
+
+    return get_avg
--- solutions/lesson08/task2.py
+++ solutions/lesson08/task2.py
@@ -1,10 +1,34 @@
 from typing import Callable, TypeVar
+from functools import wraps
+import time
+import math
 
 T = TypeVar("T")
 
-def collect_statistic(
-    statistics: dict[str, list[float, int]]
-) -> Callable[[T], T]:
-    
-    # ваш код
-    pass
+
+def collect_statistic(statistics: dict[str, list[float | int]]) -> Callable[[T], T]:
+    def decorator(func: Callable[[T], T]) -> Callable[[T], T]:
+        func_name = func.__name__
+        summa = 0
+        counter = 0
+
+        @wraps(func)
+        def wrapper(*args: T, **kwargs: T) -> T:
+            nonlocal summa, counter
+            start = time.time()
+            result = func(*args, **kwargs)
+            end = time.time()
+
+            working_time = end - start
+            summa += working_time
+            counter += 1
+
+            if func_name not in statistics:
+                statistics[func_name] = [working_time, counter]
+            else:
+                statistics[func_name] = [summa / counter, counter]
+            return result
+
+        return wrapper
+
+    return decorator
