875ad63085af0b7546259e90b9541ddf21b206db
kalium-cyanide <mrrrosssi@gmail.com>
2025-11-12 21:29:46 +0300

    hw8

--- solutions/lesson08/task1.py
+++ solutions/lesson08/task1.py
@@ -1,5 +1,25 @@
 from typing import Callable
 
+
 def make_averager(accumulation_period: int) -> Callable[[float], float]:
-    # ваш код
-    pass
+    queue = [0] * accumulation_period
+    back_position = 0
+    accumulator = 0
+    count = 0
+
+    def get_avg(value: float) -> float:
+        nonlocal accumulator
+        nonlocal back_position
+        nonlocal count
+
+        count = min(count + 1, accumulation_period)
+
+        back_position = (back_position + 1) % accumulation_period
+
+        accumulator += value - queue[back_position]
+
+        queue[back_position] = value
+
+        return accumulator / count
+
+    return get_avg
--- solutions/lesson08/task2.py
+++ solutions/lesson08/task2.py
@@ -1,10 +1,30 @@
+import time
+from functools import wraps
 from typing import Callable, TypeVar
 
 T = TypeVar("T")
 
-def collect_statistic(
-    statistics: dict[str, list[float, int]]
-) -> Callable[[T], T]:
-    
-    # ваш код
-    pass
+
+def collect_statistic(statistics: dict[str, list[float, int]]) -> Callable[[T], T]:
+    def deccorator(function: Callable):
+        accumulator, counter = 0, 0
+
+        @wraps(function)
+        def wrapper(*args: ..., **kwargs: ...) -> T:
+            nonlocal statistics
+            nonlocal accumulator, counter
+
+            start = time.time()
+            result = function(*args, **kwargs)
+            end = time.time()
+
+            counter += 1
+            accumulator += end - start
+
+            statistics[function.__name__] = [accumulator / counter, counter]
+
+            return result
+
+        return wrapper
+
+    return deccorator
