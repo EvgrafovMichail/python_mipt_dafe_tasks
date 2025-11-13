f160c86ed36a29b493a97501669e3eae912b1f67
Vladimir <vl.mukhin21@gmail.com>
2025-11-13 23:50:36 +0300

    Сделано задание 1

--- solutions/lesson08/task1.py
+++ solutions/lesson08/task1.py
@@ -1,5 +1,16 @@
 from typing import Callable
 
+
 def make_averager(accumulation_period: int) -> Callable[[float], float]:
-    # ваш код
-    pass
+    values = []
+
+    def get_avg(value_for_day: int) -> float:
+        if accumulation_period < 1:
+            return 0
+
+        values.append(value_for_day)
+        if len(values) > accumulation_period:
+            values.pop(0)
+        return sum(values) / len(values)
+
+    return get_avg
