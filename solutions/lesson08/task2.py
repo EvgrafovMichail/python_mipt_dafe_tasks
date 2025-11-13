db27782800f103fb190e493d3e4e0feab5f996f1
melanikot-mipt <kotova.mv@phystech.edu>
2025-11-13 21:12:57 +0300

    less08

--- solutions/lesson08/task2.py
+++ solutions/lesson08/task2.py
@@ -1,10 +1,30 @@
+import time
+from functools import wraps
 from typing import Callable, TypeVar
 
 T = TypeVar("T")
 
-def collect_statistic(
-    statistics: dict[str, list[float, int]]
-) -> Callable[[T], T]:
-    
-    # ваш код
-    pass
+
+def collect_statistic(statistics: dict[str, list[float, int]]) -> Callable[[T], T]:
+    def decorator(func: Callable[[T], T]):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            time_start = time.time()
+            result = func(*args, **kwargs)
+            time_end = time.time()
+
+            if func.__name__ in statistics:
+                statistics[func.__name__][1] += 1
+                statistics[func.__name__][0] = (
+                    statistics[func.__name__][0] * (statistics[func.__name__][1] - 1)
+                    + time_end
+                    - time_start
+                ) / statistics[func.__name__][1]
+            else:
+                statistics[func.__name__] = [time_end - time_start, 1]
+
+            return result
+
+        return wrapper
+
+    return decorator
