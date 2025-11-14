d6e478d1a529608d6779b355acc302ecbfdf6acb
Kaitashi <vaich18022888@gmail.com>
2025-11-14 17:34:06 +0300

    hw 6 fix

--- solutions/lesson08/task2.py
+++ solutions/lesson08/task2.py
@@ -1,7 +1,8 @@
 from typing import Callable, TypeVar, Any
 import time
 from functools import wraps
-
+# в комментариях буду указывать на каких стр в презентации что-то схожее
+# прост я сам до этого не додумался бы, а так легче аналогично делать
 T = TypeVar("T")
 
 
@@ -17,16 +18,16 @@ def collect_statistic(
             result = func(*args, **kwargs)  # 18-19
             execution_time = time.time() - time_start  # 18-19
 
-            if func_name not in statistics:
+            if func_name not in statistics:  # 13-14
                 statistics[func_name] = [execution_time, 1]
             else:
                 avg_time, call_count = statistics[func_name]  # 13-14
                 new_call_count = call_count + 1
                 new_avg_time = (avg_time * call_count + execution_time) / new_call_count
-                statistics[func_name] = [new_avg_time, new_call_count]
+                statistics[func_name] = [new_avg_time, new_call_count]  # 13-14
 
-            return result
+            return result  # 18-19
 
-        return wrapper
+        return wrapper  # 18-19
 
-    return decorator
+    return decorator  # 25-26
