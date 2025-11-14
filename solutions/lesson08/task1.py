83efdde8e778c287ea173a6398d07fec18d3f63f
Kaitashi <vaich18022888@gmail.com>
2025-11-14 17:18:20 +0300

    hw 8

--- solutions/lesson08/task1.py
+++ solutions/lesson08/task1.py
@@ -1,5 +1,20 @@
 from typing import Callable
 
 def make_averager(accumulation_period: int) -> Callable[[float], float]:
-    # ваш код
-    pass
+    values = []
+    current_index = 0
+    count = 0
+    
+    def get_avg(x: float) -> float:
+        nonlocal current_index, count
+        
+        if count < accumulation_period:
+            values.append(x)
+            count += 1
+        else:
+            values[current_index] = x
+            current_index = (current_index + 1) % accumulation_period
+        
+        return sum(values) / count
+    
+    return get_avg
