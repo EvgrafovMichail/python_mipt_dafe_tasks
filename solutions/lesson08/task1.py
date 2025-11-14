89385f91cc3e40399595ffa0e4948ceeeec0efbc
gvidon-7 <aleksandrlarionov71@icloud.com>
2025-11-14 10:29:08 +0300

    Ларионов, 512
    вторая один из тестов не проходит

--- solutions/lesson08/task1.py
+++ solutions/lesson08/task1.py
@@ -2,4 +2,13 @@ from typing import Callable
 
 def make_averager(accumulation_period: int) -> Callable[[float], float]:
     # ваш код
-    pass
+    income_num = []
+
+    def get_avg(x):
+        income_num.append(x)
+        if len(income_num) > accumulation_period:
+            income_num.pop(0)
+        result = sum(income_num) / len(income_num)
+        return result
+    
+    return get_avg
