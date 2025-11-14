f6a08675d59515a26bc390c6580ece478a4872f2
dandandil <daniil182403@gmail.com>
2025-11-14 21:22:19 +0300

    task1 task2 ready

--- solutions/lesson08/task1.py
+++ solutions/lesson08/task1.py
@@ -1,5 +1,13 @@
 from typing import Callable
 
+
 def make_averager(accumulation_period: int) -> Callable[[float], float]:
-    # ваш код
-    pass
+    data = []
+
+    def get_avg(delta: float) -> float:
+        data.append(delta)
+        if len(data) > accumulation_period:
+            data.pop(0)
+        return sum(data) / len(data)
+
+    return get_avg
--- solutions/lesson08/task2.py
+++ solutions/lesson08/task2.py
@@ -1,10 +1,23 @@
+import time
+from functools import wraps
 from typing import Callable, TypeVar
 
 T = TypeVar("T")
 
-def collect_statistic(
-    statistics: dict[str, list[float, int]]
-) -> Callable[[T], T]:
-    
-    # ваш код
-    pass
+
+def collect_statistic(statistics: dict[str, list[float, int]]) -> Callable[[T], T]:
+    calls = 0
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            nonlocal calls
+            calls += 1
+            start_time = time.time()
+            res = func(*args, **kwargs)
+            statistics[func.__name__] = [time.time() - start_time, calls]
+            return res
+
+        return wrapper
+
+    return decorator
