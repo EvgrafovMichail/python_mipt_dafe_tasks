e48277eea358107d105a775283544355784c0f0a
Bogdan <unrelise_original@mail.ru>
2025-11-14 16:11:34 +0300

    Solutions for lesson08

--- solutions/lesson08/task1.py
+++ solutions/lesson08/task1.py
@@ -1,5 +1,13 @@
 from typing import Callable
 
+
 def make_averager(accumulation_period: int) -> Callable[[float], float]:
-    # ваш код
-    pass
+    profit = []
+
+    def wrapper(n: float) -> float:
+        profit.append(n)
+        if len(profit) > accumulation_period:
+            del profit[0]
+        return sum(profit) / len(profit)
+
+    return wrapper
--- solutions/lesson08/task2.py
+++ solutions/lesson08/task2.py
@@ -1,10 +1,28 @@
-from typing import Callable, TypeVar
+from time import time
+from typing import Callable, TypeVar, ParamSpec
+from functools import wraps
 
-T = TypeVar("T")
+R = TypeVar("R")
+P = ParamSpec("P")
 
-def collect_statistic(
-    statistics: dict[str, list[float, int]]
-) -> Callable[[T], T]:
-    
-    # ваш код
-    pass
+
+def collect_statistic(statistics: dict[str, list[float | int]]) -> Callable[P, R]:
+    def wrapper(func: Callable[P, R]) -> Callable[P, R]:
+        @wraps(func)
+        def inner(*args: P.args, **kwargs: P.kwargs) -> R:
+            start = time()
+            result = func(*args, **kwargs)
+            during = time() - start
+            if func.__name__ in statistics:
+                prev_stat = statistics[func.__name__]
+                statistics[func.__name__] = [
+                    (prev_stat[0] * prev_stat[1] + during) / prev_stat[1] + 1,
+                    prev_stat[1] + 1,
+                ]
+            else:
+                statistics[func.__name__] = [during, 1]
+            return result
+
+        return inner
+
+    return wrapper
