cbd1d4bb392751d6a0eff6cefa7afc16d1769914
Rafk123 <tabunanov.rr@phystech.edu>
2025-11-14 13:54:52 +0300

    the_sixth_contest_fixed

--- solutions/lesson08/task2.py
+++ solutions/lesson08/task2.py
@@ -2,28 +2,29 @@ from typing import Callable, TypeVar, ParamSpec
 from functools import wraps
 import time
 
-T = TypeVar("T")
 P = ParamSpec("P")
 R = TypeVar("R")
 
 
-def collect_statistic(statistics: dict[str, list[float, int]]) -> Callable[[T], T]:
-    def get_function(function_: Callable[P, R]) -> Callable[[T], T]:
-        @wraps(function_)
+def collect_statistic(
+    statistics: dict[str, list[float, int]],
+) -> Callable[[Callable[P, R]], Callable[P, R]]:
+    def decorator(func: Callable[P, R]) -> Callable[P, R]:
+        @wraps(func)
         def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
             last_time = time.time()
-            val = function_(*args, **kwargs)
+            val = func(*args, **kwargs)
             period = time.time() - last_time
 
-            if not function_.__name__ in statistics:
-                statistics[function_.__name__] = [0, 0]
+            if func.__name__ not in statistics:
+                statistics[func.__name__] = [0, 0]
 
-            pair = statistics[function_.__name__]
-            statistics[function_.__name__][0] = (pair[0] * pair[1] + period) / (pair[1] + 1)
-            statistics[function_.__name__][1] = pair[1] + 1
+            pair = statistics[func.__name__]
+            pair[0] = (pair[0] * pair[1] + period) / (pair[1] + 1)
+            pair[1] += 1
 
             return val
 
         return wrapper
 
-    return get_function
+    return decorator
