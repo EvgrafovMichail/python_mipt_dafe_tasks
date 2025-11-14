d8241cad4820817cac4dddfe699aef352e7a33a4
varlag <varya_lagutina@mail.ru>
2025-11-14 16:22:59 +0000

    lesson08

--- solutions/lesson08/task1.py
+++ solutions/lesson08/task1.py
@@ -1,10 +1,13 @@
-def make_averager(n):
+from typing import Callable
+
+
+def make_averager(accumulation_period: int) -> Callable[[float], float]:
     ans = []
 
     def get_avg(value):
         ans.append(value)
 
-        if len(ans) > n:
+        if len(ans) > accumulation_period:
             ans.pop(0)
 
         return sum(ans) / len(ans)
