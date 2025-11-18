c9b4f266b83066dde8ba3d07cf07dbb3a92e899b
AntonLodygin <tonylodygin123@gmail.com>
2025-11-18 19:53:11 +0300

    solution homework task1

--- homeworks/hw1/aggregate_segmentation.py
+++ homeworks/hw1/aggregate_segmentation.py
@@ -1,8 +1,37 @@
+from uuid import UUID
+
 ALLOWED_TYPES = {
     "spotter_word",
     "voice_human",
     "voice_bot",
 }
+
+
+def is_valid_uuid(uuid):
+    try:
+        UUID(uuid)
+        return True
+    except ValueError:
+        return False
+
+
+def is_valid_segment(segment):
+    if "segment_id" not in segment:
+        return False
+
+    if not is_valid_uuid(segment["segment_id"]):
+        return False
+
+    if segment["type"] is None and segment["segment_start"] is None and segment["segment_end"] is None:
+        pass
+    elif (not isinstance(segment["type"], str) or not isinstance(segment["segment_start"], float) or
+          not isinstance(segment["segment_end"], float)):
+        return False
+
+    if segment["type"] not in ALLOWED_TYPES and segment["type"] is not None:
+        return False
+
+    return True
 
 
 def aggregate_segmentation(
@@ -24,5 +53,41 @@ def aggregate_segmentation(
         Список `audio_id` (str), которые требуют переразметки.
     """
 
-    # ваш код
-    return {}, []
+    audio = {}
+    not_valid_audio_id = set()
+
+    for i in segmentation_data:
+        audio_id = i["audio_id"]
+        segment_id = i["segment_id"]
+        start = i["segment_start"]
+        end = i["segment_end"]
+        segment_type = i["type"]
+
+        if is_valid_uuid(audio_id) and is_valid_segment(i):
+            if audio_id not in audio:
+                audio[audio_id] = {
+                    segment_id: {
+                        "end": end,
+                        "start": start,
+                        "type": segment_type
+                    }
+                }
+            else:
+                if segment_id not in audio[audio_id]:
+                    audio[audio_id][segment_id] = {
+                        "end": end,
+                        "start": start,
+                        "type": segment_type
+                    }
+                else:
+                    segment = audio[audio_id][segment_id]
+                    if segment["end"] != end or segment["start"] != start or segment["type"] != segment_type:
+                        not_valid_audio_id.add(audio_id)
+
+        else:
+            not_valid_audio_id.add(audio_id)
+
+    audio = {a_id: segments if not any(not any(s.values()) for s_id, s in segments.items()) else {} for a_id, segments
+             in audio.items()}
+
+    return audio, list(not_valid_audio_id)
