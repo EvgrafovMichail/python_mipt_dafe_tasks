be00c06d96edefe6dc5b80853318882539e6f5cc
Vika <kostrykova.v.a.12@gmail.com>
2025-11-28 22:29:13 +0300

    Решение Homework

--- homeworks/hw1/convert_exception.py
+++ homeworks/hw1/convert_exception.py
@@ -2,6 +2,7 @@ from typing import (
     Callable,
     ParamSpec,
     TypeVar,
+    Any 
 )
 
 P = ParamSpec("P")
@@ -11,18 +12,19 @@ R = TypeVar("R")
 def convert_exceptions_to_api_compitable_ones(
     exception_to_api_exception: dict[type[Exception], type[Exception] | Exception],
 ) -> Callable[[Callable[P, R]], Callable[P, R]]:
-    """
-    Параметризованный декоратор для замены внутренних исключений на API-исключении.
-
-    Args:
-        exception_to_api_exception: словарь:
-            ключи - внутренние исключения, которые надо заменить,
-            значения - API-исключения, которые надо возбудить
-                вместо внутренних исключений
-
-    Returns:
-        Декоратор для непосредственного использования.
-    """
-
-    # ваш код
-    pass
+    def decorator (func : Callable) -> Callable:
+        def wrapper (*args, **kwargs) -> Any:
+            try :
+                return func(*args, **kwargs)
+            except Exception as error:
+                error_type = type(error)
+                for old_error_type, api_error in exception_to_api_exception.items():
+                    if isinstance(error, old_error_type):
+                 #нашли перевод 
+                        if isinstance(api_error, type) and issubclass(api_error, Exception):
+                            raise api_error(str(error)) from error
+                        else:
+                            raise api_error from error
+                raise error 
+        return wrapper
+    return decorator
