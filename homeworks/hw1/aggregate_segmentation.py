46eb6c1286e1b1beb0e0332b811be596f8a77d28
Oleg <rozomazan069@gmail.com>
2025-11-28 18:09:18 +0300

    homework done

--- homeworks/hw1/aggregate_segmentation.py
+++ homeworks/hw1/aggregate_segmentation.py
@@ -8,21 +8,67 @@ ALLOWED_TYPES = {
 def aggregate_segmentation(
     segmentation_data: list[dict[str, str | float | None]],
 ) -> tuple[dict[str, dict[str, dict[str, str | float]]], list[str]]:
-    """
-    Функция для валидации и агрегации данных разметки аудио сегментов.
+    invalid_audio = set()
+    prev_keys = {}
+    valid_audio = {}
+    for segment in segmentation_data:
+        if "audio_id" not in segment:
+            continue
+        audio_id = segment.get("audio_id", None)
+        segment_id = segment.get("segment_id", None)
+        start = segment.get("segment_start", None)
+        end = segment.get("segment_end", None)
+        type = segment.get("type", None)
+        invalid = False
+        if audio_id in invalid_audio:
+            continue
+        elif segment_id is None:
+            invalid = True
+        elif (
+            not isinstance(type, str)
+            and type is not None
+            or not isinstance(start, float)
+            and start is not None
+            or not isinstance(end, float)
+            and end is not None
+        ):
+            invalid = True
+        #    print("типы не соответствуют",segment["audio_id"])
+        elif type not in ALLOWED_TYPES and type is not None:
+            invalid = True
+        elif (end is None or start is None or type is None) and not (
+            end is None and start is None and type is None
+        ):
+            invalid = True
+        else:
+            key = (audio_id, segment_id)
+            if key in prev_keys:
+                prev_data = prev_keys[key]
+                if (
+                    prev_data["type"] != type
+                    or prev_data["start"] != start
+                    or prev_data["end"] != end
+                ):
+                    # Нашли конфликт - добавляем audio_id в невалидные
+                    invalid = True
 
-    Args:
-        segmentation_data: словарь, данные разметки аудиосегментов с полями:
-            "audio_id" - уникальный идентификатор аудио.
-            "segment_id" - уникальный идентификатор сегмента.
-            "segment_start" - время начала сегмента.
-            "segment_end" - время окончания сегмента.
-            "type" - тип голоса в сегменте.
+            else:
+                prev_keys[key] = {"type": type, "start": start, "end": end}
 
-    Returns:
-        Словарь с валидными сегментами, объединёнными по `audio_id`;
-        Список `audio_id` (str), которые требуют переразметки.
-    """
-
-    # ваш код
-    return {}, []
+        # действие в зависимости от валидности
+        if invalid:
+            if audio_id in valid_audio:
+                valid_audio.pop(audio_id)
+            invalid_audio.add(audio_id)
+        elif audio_id in valid_audio:
+            if (
+                type is not None
+            ):  # проверяем только один вместо всех, так как если бы был только один или два,
+                # он был бы invalid
+                valid_audio[audio_id][segment_id] = {"start": start, "end": end, "type": type}
+        else:
+            if type is None:
+                valid_audio[audio_id] = {}
+            else:
+                valid_audio[audio_id] = {segment_id: {"start": start, "end": end, "type": type}}
+    return valid_audio, list(invalid_audio)
--- homeworks/hw1/cache.py
+++ homeworks/hw1/cache.py
@@ -8,6 +8,54 @@ P = ParamSpec("P")
 R = TypeVar("R")
 
 
+class Node:
+    def __init__(self, key, data):
+        self.key = key
+        self.data = data
+        self.prev = None
+        self.next = None
+
+
+class Deque:
+    def __init__(self):
+        self.begin = None
+        self.end = None
+
+    def add_to_end(self, node: Node):
+        if self.end is not None:
+            self.end.next = node
+            node.prev = self.end
+            self.end = node
+        else:
+            self.end = node
+            self.begin = node
+
+    def delete_begin(self):
+        if self.begin.next is None:
+            self.begin = None
+            self.end = None
+        else:
+            if self.begin.next is not None:
+                self.begin.next.prev = None
+                self.begin = self.begin.next
+            else:
+                self.begin = None
+                self.end = None
+
+    def move_to_end(self, node: Node):
+        if node.next is not None:
+            node.next.prev = node.prev
+
+            if node.prev is not None:
+                node.prev.next = node.next
+            else:
+                self.begin = node.next
+            node.prev = self.end
+            self.end.next = node
+            node.next = None
+            self.end = node
+
+
 def lru_cache(capacity: int) -> Callable[[Callable[P, R]], Callable[P, R]]:
     """
     Параметризованный декоратор для реализации LRU-кеширования.
@@ -23,5 +71,50 @@ def lru_cache(capacity: int) -> Callable[[Callable[P, R]], Callable[P, R]]:
             для получения целого числа.
         ValueError, если после округления capacity - число, меньшее 1.
     """
-    # ваш код
-    pass
+    # создание ошибок
+    try:
+        capacity = round(capacity)
+    except Exception:
+        raise TypeError("Недопустимый тип размера")
+    else:
+        if capacity < 1:
+            raise ValueError("Недопустимый размер")
+
+    def wrapper(func):
+        keys_to_obj = {}
+        priorities = Deque()
+
+        def func_with_cache(*args, **kwargs):
+            # формируем кортеж с набором аргументов
+
+            arguments = (args, tuple(sorted(kwargs.items())))
+            """for i in range(len(args)):
+                arguments[(func.__code__.co_varnames)[i]]=args[i] #получение позиционных аргументов
+            
+            for var in func.__code__.co_varnames[len(args):func.__code__.co_argcount]:
+                arguments[var]=kwargs[var]                          #получение ключевых аргументов
+            arguments=tuple(arguments.items())"""
+
+            if arguments in keys_to_obj:
+                priorities.move_to_end(keys_to_obj[arguments])
+
+                return keys_to_obj[arguments].data
+            else:
+                result = func(*args, **kwargs)
+                obj = Node(arguments, result)
+
+                if len(keys_to_obj) == capacity:
+                    keys_to_obj.pop(priorities.begin.key)
+                    priorities.delete_begin()
+
+                    priorities.add_to_end(obj)
+                    keys_to_obj[arguments] = obj
+                else:
+                    keys_to_obj[arguments] = obj
+                    priorities.add_to_end(obj)
+
+                return result
+
+        return func_with_cache
+
+    return wrapper
