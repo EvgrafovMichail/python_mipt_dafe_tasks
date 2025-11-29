73ed6aac9313889f73966bde631033a9d74b2418
Kodik Igor <ikodik34@gmail.com>
2025-11-29 20:55:20 +0300

    Tasks for November 30

--- homeworks/hw1/aggregate_segmentation.py
+++ homeworks/hw1/aggregate_segmentation.py
@@ -8,21 +8,78 @@ ALLOWED_TYPES = {
 def aggregate_segmentation(
     segmentation_data: list[dict[str, str | float | None]],
 ) -> tuple[dict[str, dict[str, dict[str, str | float]]], list[str]]:
-    """
-    Функция для валидации и агрегации данных разметки аудио сегментов.
+    audio_ids_re_marking = []
+    valid_segmentation_data = {}
 
-    Args:
-        segmentation_data: словарь, данные разметки аудиосегментов с полями:
-            "audio_id" - уникальный идентификатор аудио.
-            "segment_id" - уникальный идентификатор сегмента.
-            "segment_start" - время начала сегмента.
-            "segment_end" - время окончания сегмента.
-            "type" - тип голоса в сегменте.
+    for segment in segmentation_data:
+        # проверка audio_id
+        if "audio_id" not in segment:
+            continue
 
-    Returns:
-        Словарь с валидными сегментами, объединёнными по `audio_id`;
-        Список `audio_id` (str), которые требуют переразметки.
-    """
+        # проверка segment_id
+        if "segment_id" not in segment:
+            audio_ids_re_marking.append(segment["audio_id"])
 
-    # ваш код
-    return {}, []
+            if segment["audio_id"] in valid_segmentation_data:
+                del valid_segmentation_data[segment["audio_id"]]
+            continue
+
+        auxiliary_arr = [
+            segment.get("segment_start", None),
+            segment.get("segment_end", None),
+            segment.get("type", None),
+        ]
+
+        # голос найден
+        if any(auxiliary_arr) and not all(auxiliary_arr):
+            audio_ids_re_marking.append(segment["audio_id"])
+
+            if segment["audio_id"] in valid_segmentation_data:
+                del valid_segmentation_data[segment["audio_id"]]
+            continue
+
+        # обработка фрагмента без голоса
+        if not any(auxiliary_arr):
+            if segment["audio_id"] in valid_segmentation_data:
+                continue
+            valid_segmentation_data[segment["audio_id"]] = {}
+            continue
+
+        # проверка, что Тип type входит в ALLOWED_TYPES, и тип segment_start / segment_end float
+        if (
+            (segment.get("type", None) not in ALLOWED_TYPES)
+            or (not isinstance(segment.get("segment_start", None), float))
+            or (not isinstance(segment.get("segment_end", None), float))
+        ):
+            audio_ids_re_marking.append(segment["audio_id"])
+
+            if segment["audio_id"] in valid_segmentation_data:
+                del valid_segmentation_data[segment["audio_id"]]
+            continue
+
+        #  проверка для одного и того же audio_id и segment_id
+        #  встречаются ли разные значения start, end или type.
+        if (
+            segment.get("audio_id", None) in valid_segmentation_data
+            and segment.get("segment_id", None) in valid_segmentation_data[segment["audio_id"]]
+        ):
+            # если совпали
+            if list(
+                (valid_segmentation_data[segment["audio_id"]][segment["segment_id"]]).values()
+            ) == [segment["segment_start"], segment["segment_end"], segment["type"]]:
+                continue
+
+            audio_ids_re_marking.append(segment["audio_id"])
+            if segment["audio_id"] in valid_segmentation_data:
+                del valid_segmentation_data[segment["audio_id"]]
+            continue
+
+        valid_segmentation_data[segment["audio_id"]] = {
+            segment["segment_id"]: {
+                "start": segment["segment_start"],
+                "end": segment["segment_end"],
+                "type": segment["type"],
+            }
+        }
+
+    return valid_segmentation_data, audio_ids_re_marking
