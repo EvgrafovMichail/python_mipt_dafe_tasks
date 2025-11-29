f8c50b8457d09ce74972668bbe98eaaf58c77a1a
Bogdan <unrelise_original@mail.ru>
2025-11-29 20:56:22 +0300

    hm1

--- homeworks/hw1/cache.py
+++ homeworks/hw1/cache.py
@@ -1,27 +1,53 @@
-from typing import (
-    Callable,
-    ParamSpec,
-    TypeVar,
-)
+from functools import wraps
+from typing import Callable, ParamSpec, TypeVar
 
 P = ParamSpec("P")
 R = TypeVar("R")
 
 
 def lru_cache(capacity: int) -> Callable[[Callable[P, R]], Callable[P, R]]:
-    """
-    Параметризованный декоратор для реализации LRU-кеширования.
+    try:
+        capacity = round(capacity)
+    except (TypeError, ValueError):
+        raise TypeError("Capacity must be a roundable value")
 
-    Args:
-        capacity: целое число, максимальный возможный размер кеша.
+    if capacity < 1:
+        raise ValueError("Capacity must be at least 1")
 
-    Returns:
-        Декоратор для непосредственного использования.
+    def wrapper(func: Callable[P, R]) -> Callable[P, R]:
+        cache = {}
 
-    Raises:
-        TypeError, если capacity не может быть округлено и использовано
-            для получения целого числа.
-        ValueError, если после округления capacity - число, меньшее 1.
-    """
-    # ваш код
-    pass
+        @wraps(func)
+        def inner(*args: P.args, **kwargs: P.kwargs) -> R:
+            func_code = func.__code__
+            arg_names = func_code.co_varnames[: func_code.co_argcount]
+            if defaults := func.__defaults__:
+                defaults = ()
+            num_non_defaults = func_code.co_argcount - len(defaults)
+
+            key = []
+            for i, arg_name in enumerate(arg_names):
+                if i < len(args):
+                    key.append(args[i])
+                elif arg_name in kwargs:
+                    key.append(kwargs[arg_name])
+                elif i >= num_non_defaults:
+                    key.append(defaults[i - num_non_defaults])
+            key = tuple(key)
+
+            if key in cache:
+                result = cache[key]
+                del cache[key]
+                cache[key] = result
+                return result
+            else:
+                result = func(*args, **kwargs)
+                cache[key] = result
+                if len(cache) > capacity:
+                    first_key = next(iter(cache))
+                    del cache[first_key]
+                return result
+
+        return inner
+
+    return wrapper
--- homeworks/hw1/convert_exception.py
+++ homeworks/hw1/convert_exception.py
@@ -1,3 +1,4 @@
+from functools import wraps
 from typing import (
     Callable,
     ParamSpec,
@@ -11,18 +12,16 @@ R = TypeVar("R")
 def convert_exceptions_to_api_compitable_ones(
     exception_to_api_exception: dict[type[Exception], type[Exception] | Exception],
 ) -> Callable[[Callable[P, R]], Callable[P, R]]:
-    """
-    Параметризованный декоратор для замены внутренних исключений на API-исключении.
+    def wrapper(func: Callable[P, R]) -> Callable[P, R]:
+        @wraps(func)
+        def inner(*args: P.args, **kwargs: P.kwargs) -> R:
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                if (exception_type := type(e)) in exception_to_api_exception:
+                    raise exception_to_api_exception[exception_type]
+                raise e
 
-    Args:
-        exception_to_api_exception: словарь:
-            ключи - внутренние исключения, которые надо заменить,
-            значения - API-исключения, которые надо возбудить
-                вместо внутренних исключений
+        return inner
 
-    Returns:
-        Декоратор для непосредственного использования.
-    """
-
-    # ваш код
-    pass
+    return wrapper
