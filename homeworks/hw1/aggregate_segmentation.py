a20233ad76fda385f75afb6c61d1cd4bd164af41
Vladimir <vl.mukhin21@gmail.com>
2025-11-30 09:47:47 +0300

    Отлажен код

--- homeworks/hw1/aggregate_segmentation.py
+++ homeworks/hw1/aggregate_segmentation.py
@@ -5,6 +5,49 @@ ALLOWED_TYPES = {
 }
 
 
+def _is_segm_valid(segm: list[dict[str, str | float | None]]) -> bool:
+    """Функция для проверки сегмента на валидность"""
+    segm_start = segm.get("segment_start")
+    segm_end = segm.get("segment_end")
+    segm_type = segm.get("type")
+
+    if (segm_type is None) or (segm_start is None) or (segm_end is None):
+        return False
+
+    if segm_type not in ALLOWED_TYPES:
+        return False
+
+    if not (
+        isinstance(segm["type"], str)
+        and isinstance(segm["segment_start"], float)
+        and isinstance(segm["segment_end"], float)
+    ):
+        return False
+
+    if (segm_start < 0) or (segm_end < 0) or (segm_start > segm_end):
+        return False
+
+    return True
+
+
+def _add_to_valid_data(segm, valid_data):
+    """Функция для добавления сегмента в словарь с валидными сегментами"""
+    audio_id = segm.get("audio_id")
+    segm_id = segm.get("segment_id")
+    segm_start = segm.get("segment_start")
+    segm_end = segm.get("segment_end")
+    segm_type = segm.get("type")
+
+    if audio_id not in valid_data:
+        valid_data[audio_id] = {}
+
+    valid_data[audio_id][segm_id] = {
+        "start": segm_start,
+        "end": segm_end,
+        "type": segm_type,
+    }
+
+
 def aggregate_segmentation(
     segmentation_data: list[dict[str, str | float | None]],
 ) -> tuple[dict[str, dict[str, dict[str, str | float]]], list[str]]:
@@ -24,5 +67,46 @@ def aggregate_segmentation(
         Список `audio_id` (str), которые требуют переразметки.
     """
 
-    # ваш код
-    return {}, []
+    valid_data = {}
+    non_valid_audio_id = set()
+
+    for segm in segmentation_data:
+        audio_id = segm.get("audio_id")
+        segm_id = segm.get("segment_id")
+        segm_start = segm.get("segment_start")
+        segm_end = segm.get("segment_end")
+        segm_type = segm.get("type")
+
+        if audio_id is None:
+            continue
+
+        if segm_id is None:
+            non_valid_audio_id.add(audio_id)
+            continue
+
+        if (segm_type is None) and (segm_start is None) and (segm_end is None):
+            if audio_id not in valid_data:
+                valid_data[audio_id] = {}
+            continue
+
+        if not _is_segm_valid(segm):
+            non_valid_audio_id.add(audio_id)
+
+        if (audio_id in valid_data) and (segm_id in valid_data[audio_id]):
+            segm_in_valids = valid_data[audio_id][segm_id]
+
+            if (
+                (segm_start != segm_in_valids["start"])
+                or (segm_end != segm_in_valids["end"])
+                or (segm_type != segm_in_valids["type"])
+            ):
+                non_valid_audio_id.add(audio_id)
+                continue
+
+        _add_to_valid_data(segm, valid_data)
+
+    for segm in non_valid_audio_id:
+        if segm in valid_data:
+            valid_data.pop(segm)
+
+    return valid_data, list(non_valid_audio_id)
