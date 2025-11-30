3abece6149a6adb4de36fa1c211e3cd3b5673646
dryzesolo <temirbulatovamin12@gmail.com>
2025-11-30 11:44:45 +0300

    б12-514 Темирбулатов Амин

--- homeworks/hw1/aggregate_segmentation.py
+++ homeworks/hw1/aggregate_segmentation.py
@@ -25,4 +25,47 @@ def aggregate_segmentation(
     """
 
     # ваш код
-    return {}, []
+    need_remark = set()
+    right_data = {}
+    for segment in segmentation_data:
+        audio_id = segment["audio_id"]
+        segment_id = segment["segment_id"]
+        segment_start = segment["segment_start"]
+        segment_end = segment["segment_end"]
+        type = segment["type"]
+        if audio_id is not None:
+            if segment_id is None:
+                need_remark.add(audio_id)
+            elif segment_end is None and segment_start is None and type is None:
+                if not (audio_id in right_data):
+                    right_data[audio_id] = {}
+                continue
+            elif not isinstance(segment_start, float):
+                need_remark.add(audio_id)
+            elif not isinstance(segment_end, float):
+                need_remark.add(audio_id)
+            elif not isinstance(type, str):
+                need_remark.add(audio_id)
+            elif not (type in ALLOWED_TYPES):
+                need_remark.add(audio_id)
+            elif segment_end is None or segment_start is None or type is None:
+                need_remark.add(audio_id)
+            else:
+                if audio_id in right_data and segment_id in right_data[audio_id]:
+                    another_data = right_data[audio_id][segment_id]
+                    if (
+                        another_data["type"] != type
+                        or another_data["segment_end"] != segment_end
+                        or another_data["segment_start"] != segment_start
+                    ):
+                        need_remark.add(audio_id)
+                        right_data.pop([audio_id][segment_id])
+                else:
+                    if audio_id not in right_data:
+                        right_data[audio_id] = {}
+                    right_data[audio_id][segment_id] = {
+                        "type": type,
+                        "segment_start": segment_start,
+                        "segment_end": segment_end,
+                    }
+    return right_data, list(need_remark)
--- homeworks/hw1/cache.py
+++ homeworks/hw1/cache.py
@@ -24,4 +24,43 @@ def lru_cache(capacity: int) -> Callable[[Callable[P, R]], Callable[P, R]]:
         ValueError, если после округления capacity - число, меньшее 1.
     """
     # ваш код
-    pass
+    try:
+        capacity = round(capacity)
+    except Exception:
+        raise TypeError
+    if capacity < 1:
+        raise ValueError
+
+    def decorator(func):
+        cache = dict()
+
+        def wrapper(*args, **kwargs):
+            key = (args, tuple(sorted(kwargs.items()))) if kwargs else args
+            if key not in cache:
+                result = func(*args, **kwargs)
+                cache[key] = result
+                if len(cache) > capacity:
+                    first_key = next(iter(cache))
+                    del cache[first_key]
+                return result
+            result = cache.pop(key)
+            cache[key] = result
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+@lru_cache(2)
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n - 1) + fib(n - 2)
+
+
+@lru_cache(2)
+def fib2(n):
+    if n < 2:
+        return n
+    return fib(n - 1) + fib(n - 2)
