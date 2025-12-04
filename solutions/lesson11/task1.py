86005bf40e1f52ead6550f5d9c44d8f9bed9c303
Vandam-create <beliakov.va@phystech.edu>
2025-12-04 15:57:07 +0300

    ver 2

--- solutions/lesson11/task1.py
+++ solutions/lesson11/task1.py
@@ -3,9 +3,9 @@ from numbers import Real
 
 
 class Vector2D:
-    def __init__(self, abs=0.0, ord=0.0):
-        self.__abscissa = float(abs)
-        self.__ordinate = float(ord)
+    def __init__(self, abscissa=0.0, ordinate=0.0):
+        self.__abscissa = float(abscissa)
+        self.__ordinate = float(ordinate)
 
     def conj(self) -> "Vector2D":
         return Vector2D(self.abscissa, -self.ordinate)
@@ -30,7 +30,7 @@ class Vector2D:
         return self.__ordinate
 
     def __repr__(self) -> str:
-        return f"Vector2D(abscissa={self.abscissa}, ordinate={self.ordinate})"
+        return f"Vector2D(abscissa={self.abscissa:.1f}, ordinate={self.ordinate:.1f})".replace('.0', '')
 
     def __eq__(self, vec2: "Vector2D") -> bool:
         if not isinstance(vec2, Vector2D):
@@ -40,17 +40,37 @@ class Vector2D:
             self.ordinate, vec2.ordinate
         )
 
+    def __ne__(self, vec2: object) -> bool:
+        if not isinstance(vec2, Vector2D):
+            return NotImplemented
+        
+        return not (self == vec2)
+    
     def __gt__(self, vec2: "Vector2D") -> bool:
-        return self != vec2 and (
-            self.abscissa > vec2.abscissa
-            or (math.isclose(self.abscissa, vec2.abscissa) and self.ordinate > vec2.ordinate)
-        )
+        if not isinstance(vec2, Vector2D):
+            return NotImplemented
+        
+        if vec2 == self:
+            return False
+
+        if math.isclose(self.abscissa, vec2.abscissa):
+        
+           return self.ordinate > vec2.ordinate
+        else:
+            return self.abscissa > vec2.abscissa
 
     def __lt__(self, vec2: "Vector2D") -> bool:
-        return self != vec2 and (
-            self.abscissa < vec2.abscissa
-            or (math.isclose(self.abscissa, vec2.abscissa) and self.ordinate < vec2.ordinate)
-        )
+        if not isinstance(vec2, Vector2D):
+            return NotImplemented
+        
+        if vec2 == self:
+            return False
+
+        if math.isclose(self.abscissa, vec2.abscissa):
+        
+           return self.ordinate < vec2.ordinate
+        else:
+            return self.abscissa < vec2.abscissa
 
     def __ge__(self, vec2: "Vector2D") -> bool:
         return self == vec2 or self > vec2
@@ -62,7 +82,8 @@ class Vector2D:
         return math.sqrt(self.abscissa**2 + self.ordinate**2)
 
     def __bool__(self) -> bool:
-        return not math.isclose(abs(self), 0)
+        return not (math.isclose(self.abscissa, 0, abs_tol=1e-15) and 
+                    math.isclose(self.ordinate, 0, abs_tol=1e-15))
 
     def __mul__(self, scale: Real) -> "Vector2D":
         if not isinstance(scale, Real):
@@ -85,25 +106,27 @@ class Vector2D:
 
         return Vector2D(self.abscissa / scale, self.ordinate / scale)
 
-    def __add__(self, other: "Vector2D" | Real) -> "Vector2D":
-        if not isinstance(other, Vector2D | Real):
-            return NotImplemented
-
+    def __add__(self, other: "Vector2D") -> "Vector2D":
         if isinstance(other, Real):
             other = Vector2D(other, other)
+
+        if not isinstance(other, Vector2D):
+            return NotImplemented
+
 
         return Vector2D(other.abscissa + self.abscissa, other.ordinate + self.ordinate)
 
     def __radd__(self, other: Real) -> "Vector2D":
         return self + other
 
-    def __sub__(self, other: "Vector2D" | Real) -> "Vector2D":
-        if not isinstance(other, Vector2D | Real):
-            return NotImplemented
-
+    def __sub__(self, other: "Vector2D") -> "Vector2D":
         if isinstance(other, Real):
             other = Vector2D(other, other)
 
+        if not isinstance(other, Vector2D):
+            return NotImplemented
+
+        
         return Vector2D(self.abscissa - other.abscissa, self.ordinate - other.ordinate)
 
     def __neg__(self) -> "Vector2D":
