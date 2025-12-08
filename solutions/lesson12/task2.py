a360edda3857bdfc3cdf36e874b7f201a7eedd7e
Vladimir <vl.mukhin21@gmail.com>
2025-12-09 02:19:16 +0300

    Сделано задание 2

--- solutions/lesson12/task2.py
+++ solutions/lesson12/task2.py
@@ -2,5 +2,17 @@ from typing import Any, Generator, Iterable
 
 
 def circle(iterable: Iterable) -> Generator[Any, None, None]:
-    # ваш код
-    ...
+    item_list = []
+    for item in iterable:
+        item_list.append(item)
+
+        if len(item_list) >= 1e6:  # Condition to avoid infinite loop
+            break
+
+    if not item_list:
+        return
+
+    i = 0
+    while True:
+        yield item_list[i % len(item_list)]
+        i += 1
