fa48b5a472b8ea033451bb143b16e3f4bbc46c9d
Vladimir <vl.mukhin21@gmail.com>
2025-12-10 00:44:15 +0300

    Сделано задание 3

--- solutions/lesson12/task3.py
+++ solutions/lesson12/task3.py
@@ -2,11 +2,15 @@ import sys
 
 
 class FileOut:
-    def __init__(
-        self,
-        path_to_file: str,
-    ) -> None:
-        # ваш код
-        ...
+    def __init__(self, path_to_file: str) -> None:
+        self._path_to_file: str = path_to_file
 
-    # ваш код
+    def __enter__(self) -> "FileOut":
+        self._orig_stdout = sys.stdout
+        sys.stdout = open(self._path_to_file, "w")
+        return self
+
+    def __exit__(self, *args) -> bool:
+        sys.stdout.close()
+        sys.stdout = self._orig_stdout
+        return False
