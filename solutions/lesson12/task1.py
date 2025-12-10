f5da3ce73927964a4eb05c3f90afa9ba64c0672e
Prosto_Max_xX <maxim.nevinsky@yandex.ru>
2025-12-10 22:20:25 +0300

    Made tasks for lesson12

--- solutions/lesson12/task1.py
+++ solutions/lesson12/task1.py
@@ -2,5 +2,12 @@ from typing import Any, Generator, Iterable
 
 
 def chunked(iterable: Iterable, size: int) -> Generator[tuple[Any], None, None]:
-    # ваш код
-    ...
+    cort = []
+    for elem in iterable:
+        if len(cort) < size:
+            cort.append(elem)
+            continue
+        yield tuple(cort)
+        cort = [elem]
+    if cort:
+        yield tuple(cort)
--- solutions/lesson12/task2.py
+++ solutions/lesson12/task2.py
@@ -2,5 +2,19 @@ from typing import Any, Generator, Iterable
 
 
 def circle(iterable: Iterable) -> Generator[Any, None, None]:
-    # ваш код
-    ...
+    copy = []
+    flag = True
+    iterator = iter(iterable)
+    while True:
+        while flag:
+            try:
+                elem = next(iterator)
+                copy.append(elem)
+                yield elem
+            except StopIteration:
+                flag = False
+                break
+        if not copy:
+            break
+        for elem in copy:
+            yield elem
