a0c758ff2a1cbeb3db8d0bae8408b24efd627776
Vika <kostrykova.v.a.12@gmail.com>
2025-12-10 18:25:12 +0300

    Correct_дз

--- solutions/lesson12/task2.py
+++ solutions/lesson12/task2.py
@@ -2,6 +2,12 @@ from typing import Any, Generator, Iterable
 
 
 def circle(iterable: Iterable) -> Generator[Any, None, None]:
+    cache = []
+    for i in iterable:
+        cache.append(i)
+        yield i
+    if not cache:
+        return
     while True:
-        for i in iterable:
+        for i in cache:
             yield i
--- solutions/lesson12/task3.py
+++ solutions/lesson12/task3.py
@@ -5,7 +5,15 @@ class FileOut:
         self,
         path_to_file: str,
     ) -> None:
-        # ваш код
-        ...
+        self.path_to_file = path_to_file
 
-    # ваш код
+    def __enter__(self):
+        self.output_file = open(self.path_to_file, "w")
+        self.original_stdout = sys.stdout
+        sys.stdout = self.output_file
+        return self
+
+    def __exit__(self, *args):
+        sys.stdout = self.original_stdout
+        self.output_file.close()
+        return False
