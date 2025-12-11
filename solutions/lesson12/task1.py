0cc503c8ef96960cd5838f0f842ca96750690cca
korolinakila <matvey.makarov07@mail.ru>
2025-12-11 19:13:07 +0300

    lesson12

--- solutions/lesson12/task1.py
+++ solutions/lesson12/task1.py
@@ -1,6 +1,19 @@
+
 from typing import Any, Generator, Iterable
 
 
 def chunked(iterable: Iterable, size: int) -> Generator[tuple[Any], None, None]:
-    # ваш код
-    ...
+
+    iterator = iter(iterable)
+    
+    while True:
+        chunk = []
+        try:
+            for i in range(size):
+                chunk.append(next(iterator))
+        except StopIteration:
+            if chunk:
+                yield tuple(chunk)
+            break
+        
+        yield tuple(chunk)
