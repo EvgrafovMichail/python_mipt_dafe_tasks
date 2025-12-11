b0e8bd52ca29cfbc7f59ca64b66ca72fcbb99b2d
4gera4 <geraskinctepan@gmail.com>
2025-12-11 21:55:26 +0300

    Б12-512 Гераськин Степан

--- solutions/lesson12/task1.py
+++ solutions/lesson12/task1.py
@@ -2,5 +2,18 @@ from typing import Any, Generator, Iterable
 
 
 def chunked(iterable: Iterable, size: int) -> Generator[tuple[Any], None, None]:
-    # ваш код
-    ...
+    if not iterable:
+        return tuple()
+
+    my_iter = iter(iterable)
+
+    while True:
+        result = []
+        try:
+            for _ in range(size):
+                result.append(next(my_iter))
+            yield tuple(result)
+        except StopIteration:
+            if result:
+                yield tuple(result)
+            break
--- solutions/lesson12/task3.py
+++ solutions/lesson12/task3.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Any, Self
 
 
 class FileOut:
@@ -6,7 +7,18 @@ class FileOut:
         self,
         path_to_file: str,
     ) -> None:
-        # ваш код
-        ...
+        self._path = path_to_file
+        self._original_stdout = sys.stdout
+        self._file = None
 
-    # ваш код
+    def __enter__(self) -> Self:
+        self._file = open(self._path, "w")
+        sys.stdout = self._file
+        return self
+
+    def __exit__(self, *_: Any) -> bool:
+        if self._file is not None and not self._file.closed:
+            self._file.flush()
+            sys.stdout = self._original_stdout
+            self._file.close()
+        return False
