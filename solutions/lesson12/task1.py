2bcd00060218288a2af08b01389c52651049277e
Vyacheslav A <aleksakhin.viu@phystech.edu>
2025-12-11 22:03:26 +0300

    Solutions for every task

--- solutions/lesson12/task1.py
+++ solutions/lesson12/task1.py
@@ -1,6 +1,17 @@
 from typing import Any, Generator, Iterable
 
-
 def chunked(iterable: Iterable, size: int) -> Generator[tuple[Any], None, None]:
     # ваш код
-    ...
+    block = []
+    for element in iterable:
+        
+        block.append(element)
+        
+        if len(block) == size:
+            
+            yield (tuple(block))
+            block = []
+    
+    if block:
+        
+        yield tuple(block)
--- solutions/lesson12/task2.py
+++ solutions/lesson12/task2.py
@@ -1,6 +1,21 @@
 from typing import Any, Generator, Iterable
 
-
 def circle(iterable: Iterable) -> Generator[Any, None, None]:
     # ваш код
-    ...
+
+    elements = []
+    for element in iterable:
+        yield element
+        elements.append(element)
+
+    if not elements:
+        return
+
+    size = len(elements)
+    ind = 0
+    
+    while True:
+        
+        ind %= size
+        yield elements[ind]
+        ind += 1
