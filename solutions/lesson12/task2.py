75d6d5658a51fa57da415e74f11fc8628069c015
Robert <rob.ibr0407@gmail.com>
2025-12-12 18:22:01 +0300

    "Ibragimov Robert lesson12 Б12-513"

--- solutions/lesson12/task2.py
+++ solutions/lesson12/task2.py
@@ -2,5 +2,16 @@ from typing import Any, Generator, Iterable
 
 
 def circle(iterable: Iterable) -> Generator[Any, None, None]:
-    # ваш код
-    ...
+    elements = []
+    for elem in iterable:
+        elements.append(elem)
+        yield elem
+
+    # Если iterable был пуст
+    if not elements:
+        return
+
+    # Бесконечный цикл: на каждом круге создаём новый итератор по кэшированным элементам
+    while True:
+        for elem in elements:
+            yield elem
