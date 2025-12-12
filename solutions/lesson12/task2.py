b9825eb1d3c0f937c1e222e5a40c84028e971cc2
Ruslan Bateryakov <ryslanbateryakov@gmail.com>
2025-12-12 23:36:05 +0300

    HW_Lesson12

--- solutions/lesson12/task2.py
+++ solutions/lesson12/task2.py
@@ -2,5 +2,22 @@ from typing import Any, Generator, Iterable
 
 
 def circle(iterable: Iterable) -> Generator[Any, None, None]:
-    # ваш код
-    ...
+    buffer = []
+    it = iter(iterable)
+    collecting = True
+
+    while True:
+        if collecting:
+            try:
+                value = next(it)
+                buffer.append(value)
+                yield value
+                continue
+            except StopIteration:
+                collecting = False
+
+        if not buffer:
+            return
+
+        for value in buffer:
+            yield value
--- solutions/lesson12/task3.py
+++ solutions/lesson12/task3.py
@@ -1,12 +1,28 @@
 import sys
+from types import TracebackType
+from typing import Optional, Self
 
 
 class FileOut:
-    def __init__(
-        self,
-        path_to_file: str,
-    ) -> None:
-        # ваш код
-        ...
+    def __init__(self, path_to_file: str) -> None:
+        self.path_to_file = path_to_file
+        self.file = None
+        self._original_stdout = None
+    
+    def __enter__(self) -> Self:
+        self.original_stdout = sys.stdout
+        self.file = open(self.path_to_file, "w")
+        sys.stdout = self.file
+        return self
 
-    # ваш код
+    def __exit__(
+        self,
+        exc_type: Optional[type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> Optional[bool]:
+        if self.original_stdout is not None:
+            sys.stdout = self.original_stdout
+        if self.file is not None:
+            self.file.close()
+        return False
