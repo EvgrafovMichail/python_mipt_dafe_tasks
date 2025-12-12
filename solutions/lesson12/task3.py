fb82837775a2f43a273c53424650fbf26191f4e9
Mihail Barinov <dmemsmdmemsm@gmail.com>
2025-12-13 02:07:03 +0300

    Add solutions for lesson 12

--- solutions/lesson12/task3.py
+++ solutions/lesson12/task3.py
@@ -1,12 +1,32 @@
 import sys
+from typing import TextIO, Self, Any
 
 
 class FileOut:
+    _prev_stdout: TextIO | None
+    _file_path: str
+    _file: TextIO | None
+
     def __init__(
         self,
         path_to_file: str,
     ) -> None:
-        # ваш код
-        ...
+        self._prev_stdout = None
+        self._file_path = path_to_file
+        self._file = None
 
-    # ваш код
+    def __enter__(self) -> Self:
+        if self._file:
+            raise RuntimeError("File already opened")
+        self._prev_stdout = sys.stdout
+        self._file = open(self._file_path, "w")
+        sys.stdout = self._file
+        return self
+
+    def __exit__(self, *_: Any) -> bool:
+        if not self._file:
+            raise RuntimeError("File not opened")
+        sys.stdout = self._prev_stdout
+        self._file.close()
+        self._file = None
+        return False
