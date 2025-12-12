f62e1e040a38ff6fd3b53c74d4fbc5d4ed7e85da
RobBot1337 <rob.ibr0407@gmail.com>
2025-12-12 18:24:10 +0300

    Update task2.py

--- solutions/lesson12/task2.py
+++ solutions/lesson12/task2.py
@@ -6,12 +6,8 @@ def circle(iterable: Iterable) -> Generator[Any, None, None]:
     for elem in iterable:
         elements.append(elem)
         yield elem
-
-    # Если iterable был пуст
     if not elements:
         return
-
-    # Бесконечный цикл: на каждом круге создаём новый итератор по кэшированным элементам
     while True:
         for elem in elements:
             yield elem
