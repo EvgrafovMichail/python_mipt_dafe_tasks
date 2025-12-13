660313837b13ae6dbe981abd891dce8e0f02588f
U3SU <u3.su@yandex.ru>
2025-12-13 13:24:25 +0300

    Less12

--- solutions/lesson12/task3.py
+++ solutions/lesson12/task3.py
@@ -2,11 +2,16 @@ import sys
 
 
 class FileOut:
-    def __init__(
-        self,
-        path_to_file: str,
-    ) -> None:
-        # ваш код
-        ...
+    def __init__(self, path_to_file: str) -> None:
+        self.path = path_to_file
 
-    # ваш код
+    def __enter__(self):
+        self.old = sys.stdout
+        self.file = open(self.path, "w")
+        sys.stdout = self.file
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        sys.stdout = self.old
+        self.file.close()
+        return False
