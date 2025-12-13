1795da98d4db77c27c2fe3ff714151bb8a685cde
Ilya Ryabokon <ryabokon_ilya_2007@mail.ru>
2025-12-13 10:33:13 +0300

    Ryabokon Ilya Group B12-514 lesson12

--- solutions/lesson12/task3.py
+++ solutions/lesson12/task3.py
@@ -6,7 +6,18 @@ class FileOut:
         self,
         path_to_file: str,
     ) -> None:
-        # ваш код
-        ...
+        self.path_to_file = path_to_file
+        self.file = None
+        self.old_stdout = None
 
-    # ваш код
+    def __enter__(self):
+        self.file = open(self.path_to_file, "w", encoding="utf-8")
+        self.old_stdout = sys.stdout
+        sys.stdout = self.file
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout = self.old_stdout
+        self.file.close()
+
+        return False
