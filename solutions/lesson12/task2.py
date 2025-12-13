8a8f63c27f5074f78275ff80d3376f8b06f571b1
Poco-dev <vueprojector5@gmail.com>
2025-12-13 13:20:38 +0300

    lesson12

--- solutions/lesson12/task2.py
+++ solutions/lesson12/task2.py
@@ -2,5 +2,16 @@ from typing import Any, Generator, Iterable
 
 
 def circle(iterable: Iterable) -> Generator[Any, None, None]:
-    # ваш код
-    ...
+    cache = []
+
+    for elem in iterable:
+        cache.append(elem)
+        yield elem
+
+    if not cache:
+        return
+
+    idx = 0
+    while True:
+        yield cache[idx]
+        idx = (idx + 1) % len(cache)
--- solutions/lesson12/task3.py
+++ solutions/lesson12/task3.py
@@ -1,12 +1,32 @@
 import sys
+from types import TracebackType
+from typing import Optional, Self
 
 
 class FileOut:
-    def __init__(
-        self,
-        path_to_file: str,
-    ) -> None:
-        # ваш код
-        ...
+    def __init__(self, path_to_file: str) -> None:
+        self._path_to_file = path_to_file
+        self._stdout = None
+        self._file = None
 
-    # ваш код
+    def __enter__(self) -> Self:
+        self._stdout = sys.stdout
+        self._file = open(self._path_to_file, "w", encoding="utf-8")
+        sys.stdout = self._file
+        return self
+
+    def __exit__(
+        self,
+        exc_type: Optional[type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> bool:
+        if self._stdout is not None:
+            sys.stdout = self._stdout
+            self._stdout = None
+
+        if self._file is not None:
+            self._file.close()
+            self._file = None
+
+        return False
