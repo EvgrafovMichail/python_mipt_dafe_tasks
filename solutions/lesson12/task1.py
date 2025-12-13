57e1df2ef5b62cb160c4d61805d6b3127570d1fe
Provoda78 <nikbel.2007@mail.ru>
2025-12-13 10:39:00 +0300

    task12

--- solutions/lesson12/task1.py
+++ solutions/lesson12/task1.py
@@ -2,5 +2,17 @@ from typing import Any, Generator, Iterable
 
 
 def chunked(iterable: Iterable, size: int) -> Generator[tuple[Any], None, None]:
-    # ваш код
-    ...
+    iterator = iter(iterable)
+    while True:
+        try:
+            chunk = []
+            for _ in range(size):
+                chunk.append(next(iterator))
+        except StopIteration:
+            if chunk:
+                yield tuple(chunk)
+            break
+        else:
+            yield tuple(chunk)
+            
+#print(tuple(chunked([1, 2, 3, 4, 5], 2)))
--- solutions/lesson12/task3.py
+++ solutions/lesson12/task3.py
@@ -1,12 +1,39 @@
 import sys
-
+from contextlib import contextmanager
 
 class FileOut:
+    path: str
+    file: None
+    stdout: None
+    
+    _is_close: bool
     def __init__(
         self,
         path_to_file: str,
     ) -> None:
-        # ваш код
-        ...
+        self.path = path_to_file
+        self._is_close = True
+        
+    def __enter__(self):
+        try:
+            self.file = open(self.path, "w")
+            self.stdout = sys.stdout
+            sys.stdout = self.file
+        except Exception as exp:
+            if self.stdout is not None:
+                sys.stdout = self.stdout
+            if self.file is not None:
+                self.file.close()
+                raise exp
+            
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.stdout is not None:
+            sys.stdout = self.stdout
+        if self.file is not None:
+            self.file.close()
+        
+        return False
+        
 
-    # ваш код
