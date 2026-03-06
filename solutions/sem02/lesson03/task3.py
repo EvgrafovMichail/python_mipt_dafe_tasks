a9a5dca73c90ed55fe6cb3ce67fa84802e2b1918
Vladimir <vl.mukhin21@gmail.com>
2026-03-06 21:17:02 +0300

    Solutions task 1

--- solutions/sem02/lesson03/task3.py
+++ solutions/sem02/lesson03/task3.py
@@ -3,4 +3,16 @@ import numpy as np
 
 def get_extremum_indices(
     ordinates: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray]:
+    if ordinates.shape[0] < 3:
+        raise ValueError
+
+    indices = np.arange(1, ordinates.shape[0] - 1)
+    prev = ordinates[indices - 1]
+    curr = ordinates[indices]
+    next = ordinates[indices + 1]
+
+    min_mask = (curr < prev) & (curr < next)
+    max_mask = (curr > prev) & (curr > next)
+
+    return (indices[min_mask], indices[max_mask])
