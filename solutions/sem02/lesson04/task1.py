603c8eec2018b65f903765d67686318a37131f9d
gubanovily66-beep <gubanovily66@gmail.com>
2026-03-13 21:57:53 +0300

    les 4

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,16 +2,62 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < 1:
+        raise ValueError
+
+    if image.ndim == 2:
+        height, weight = image.shape
+        result = np.zeros((height + 2 * pad_size, weight + 2 * pad_size), dtype=image.dtype)
+        result[pad_size : height + pad_size, pad_size : weight + pad_size] = image
+
+    elif image.ndim == 3:
+        height, weight, c = image.shape
+        result = np.zeros((height + 2 * pad_size, weight + 2 * pad_size, c), dtype=image.dtype)
+        result[pad_size : height + pad_size, pad_size : weight + pad_size, :] = image
+
+    return result
 
 
 def blur_image(
     image: np.ndarray,
     kernel_size: int,
 ) -> np.ndarray:
-    # ваш код
-    return image
+    if kernel_size % 2 == 0 or kernel_size < 1:
+        raise ValueError
+    if kernel_size == 1:
+        return image
+    pad = kernel_size // 2
+    h, w = image.shape[0], image.shape[1]
+    ks = kernel_size
+    ks_area = ks * ks
+    image_with_padding = pad_image(image, pad)
+    image_cumsum = np.cumsum(np.cumsum(image_with_padding, axis=0), axis=1)
+    i = np.arange(h)[:, np.newaxis]
+    j = np.arange(w)[np.newaxis, :]
+    if image.ndim == 2:
+        edited_image_cumsum = np.zeros((image_cumsum.shape[0] + 1, image_cumsum.shape[1] + 1))
+        edited_image_cumsum[1:, 1:] = image_cumsum
+        sums_for_window = (
+            edited_image_cumsum[i + ks, j + ks]
+            - edited_image_cumsum[i, j + ks]
+            - edited_image_cumsum[i + ks, j]
+            + edited_image_cumsum[i, j]
+        )
+
+    else:
+        c = image.shape[2]
+        edited_image_cumsum = np.zeros((image_cumsum.shape[0] + 1, image_cumsum.shape[1] + 1, c))
+        edited_image_cumsum[1:, 1:, :] = image_cumsum
+
+        sums_for_window = (
+            edited_image_cumsum[i + ks, j + ks, :]
+            - edited_image_cumsum[i, j + ks, :]
+            - edited_image_cumsum[i + ks, j, :]
+            + edited_image_cumsum[i, j, :]
+        )
+
+    result_image = sums_for_window / ks_area
+    return result_image.astype(np.uint8)
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -5,6 +5,24 @@ def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
 
-    return 0, 0
+    unique_pixels, pixel_counts = np.unique(image, return_counts=True)
+    extended_pixel_counts = np.zeros(256, dtype=np.int32)
+    extended_pixel_counts[unique_pixels] = pixel_counts
+
+    max_sum = 0
+    popular_color = unique_pixels[0]
+
+    for center in unique_pixels:
+        left = max(0, int(center) - threshold + 1)
+        right = min(255, int(center) + threshold - 1)
+        current_sum = np.sum(extended_pixel_counts[left : right + 1])
+
+        if current_sum > max_sum:
+            max_sum = current_sum
+            popular_color = center
+
+    percentage = (max_sum / image.size) * 100
+    return popular_color, float(percentage)
