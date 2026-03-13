a7e0343fa899902c09ec19997bc31741c4611614
kalium-cyanide <mrrrosssi@gmail.com>
2026-03-13 23:59:43 +0300

    l4 hw

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,16 +2,42 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < 1:
+        raise ValueError
+
+    expanded_shape = np.array(image.shape)
+
+    expanded_shape[:2] += 2 * pad_size
+    
+    expanded_image = np.zeros(expanded_shape, dtype=image.dtype)
+    
+    expanded_image[pad_size : -pad_size, pad_size : -pad_size, ...] = image
+    
+    return expanded_image
 
 
 def blur_image(
     image: np.ndarray,
     kernel_size: int,
 ) -> np.ndarray:
-    # ваш код
-    return image
+    
+    if kernel_size % 2 == 0 or kernel_size < 1:
+        raise ValueError
+    
+    if kernel_size == 1:
+        return image
+    
+    padded_image = pad_image(image, kernel_size // 2)
+    width, height = image.shape[:2]
+
+    accum_matrix = np.zeros(image.shape, dtype=np.float64)
+    
+    for x_offset in range(kernel_size):
+        for y_offset in range(kernel_size):
+            accum_matrix += padded_image[x_offset : x_offset + width, y_offset : y_offset + height, ...]
+
+
+    return np.array(accum_matrix / kernel_size**2, dtype=np.uint8)
 
 
 if __name__ == "__main__":
@@ -23,5 +49,7 @@ if __name__ == "__main__":
     current_directory = Path(__file__).resolve().parent
     image = get_image(os.path.join(current_directory, "images", "circle.jpg"))
     image_blured = blur_image(image, kernel_size=21)
+
+    pad_image(image, 10)
 
     compare_images(image, image_blured)
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -5,6 +5,22 @@ def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
+    
+    image = np.array(image, dtype=np.int64)
+    
+    u_values, u_counts = np.unique(image, return_counts=True)
+    
+    diff_matrix = u_values[..., np.newaxis] - u_values[np.newaxis, ...]
+    
+    mask_matrix = abs(diff_matrix) < threshold
+    
+    count_of_sml = np.sum(mask_matrix * u_counts, axis=1)
+    
+    best_i = np.argmax(count_of_sml)
 
-    return 0, 0
+    result_color = u_values[best_i]
+    result_percent = (count_of_sml[best_i] / image.size) * 100
+
+    return np.uint8(result_color), float(result_percent)
