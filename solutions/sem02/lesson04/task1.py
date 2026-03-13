8a8ae386dd38d9d08876f82cf0fee0d74148e4a2
Taisia <taisia0106@gmail.com>
2026-03-13 23:49:57 +0300

    homework 14.03

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,16 +2,53 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < 1:
+        raise ValueError
+
+    if image.ndim == 2:
+        i, j = image.shape
+        newimage = np.zeros((i + 2 * pad_size, j + 2 * pad_size), dtype=np.uint8)
+        newimage[pad_size : pad_size + i, pad_size : pad_size + j] = image
+
+    else:
+        i, j, k = image.shape
+        newimage = np.zeros((i + 2 * pad_size, j + 2 * pad_size, k), dtype=np.uint8)
+        newimage[pad_size : pad_size + i, pad_size : pad_size + j, :] = image
+
+    return newimage
 
 
 def blur_image(
     image: np.ndarray,
     kernel_size: int,
 ) -> np.ndarray:
-    # ваш код
-    return image
+    if kernel_size % 2 == 0 or kernel_size < 1:
+        raise ValueError
+
+    if kernel_size == 1:
+        return image
+
+    newimage = pad_image(image, kernel_size // 2)
+
+    if image.ndim == 2:
+        i, j = image.shape
+        result = np.zeros((i, j), dtype=np.float32)
+
+        for a in range(i):
+            for b in range(j):
+                result[a, b] = np.mean(newimage[a : a + kernel_size, b : b + kernel_size])
+
+    else:
+        i, j, k = image.shape
+        result = np.zeros((i, j, k), dtype=np.float32)
+
+        for a in range(i):
+            for b in range(j):
+                result[a, b] = np.mean(
+                    newimage[a : a + kernel_size, b : b + kernel_size, :], axis=(0, 1)
+                )
+
+    return result.astype(image.dtype)
 
 
 if __name__ == "__main__":
