c6387c61d557f389aac595062e8a4ec7dc296f8c
Vera <verakondurina@gmail.com>
2026-03-13 22:45:52 +0300

    Add homework 2

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,16 +2,66 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < 1:
+        raise ValueError
+
+    if image.ndim == 2:
+        height = image.shape[0]
+        lenght = image.shape[1]
+
+        with_padding = np.zeros((height + 2 * pad_size, lenght + 2 * pad_size), dtype=np.uint8)
+        with_padding[pad_size : height + pad_size, pad_size : lenght + pad_size] = image
+
+    elif image.ndim == 3:
+        height = image.shape[0]
+        lenght = image.shape[1]
+        width = image.shape[2]
+
+        with_padding = np.zeros(
+            (height + 2 * pad_size, lenght + 2 * pad_size, width), dtype=np.uint8
+        )
+        with_padding[pad_size : height + pad_size, pad_size : lenght + pad_size, :] = image
+
+    return with_padding
 
 
 def blur_image(
     image: np.ndarray,
     kernel_size: int,
 ) -> np.ndarray:
-    # ваш код
-    return image
+    if kernel_size % 2 == 0 or kernel_size < 1:
+        raise ValueError
+
+    if kernel_size == 1:
+        return image
+
+    if image.ndim == 2:
+        height = image.shape[0]
+        lenght = image.shape[1]
+        answer = np.zeros((height, lenght), dtype=image.dtype)
+
+        new_image = pad_image(image, kernel_size // 2)
+
+        for i in range(height):
+            for j in range(lenght):
+                window = new_image[i : i + kernel_size, j : j + kernel_size]
+                answer[i, j] = int(np.mean(window))
+
+        return answer
+
+    elif image.ndim == 3:
+        height, lenght, width = image.shape[0], image.shape[1], image.shape[2]
+        answer = np.zeros((height, lenght, width), dtype=np.uint8)
+
+        new_image = pad_image(image, kernel_size // 2)
+
+        for i in range(height):
+            for j in range(lenght):
+                for k in range(width):
+                    window = new_image[i : i + kernel_size, j : j + kernel_size, k]
+                    answer[i, j, k] = int(np.mean(window))
+
+        return answer
 
 
 if __name__ == "__main__":
