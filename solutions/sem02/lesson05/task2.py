bfb8d4a522f7e99b3e7c00188656604a0bb1c62c
danberx <danberx490@gmail.com>
2026-03-15 23:54:18 +0300

    lesson05 tasks

--- solutions/sem02/lesson05/task2.py
+++ solutions/sem02/lesson05/task2.py
@@ -8,4 +8,18 @@ class ShapeMismatchError(Exception):
 def get_projections_components(
     matrix: np.ndarray,
     vector: np.ndarray,
-) -> tuple[np.ndarray | None, np.ndarray | None]: ...
+) -> tuple[np.ndarray | None, np.ndarray | None]:
+    if matrix.shape[0] != matrix.shape[1]:
+        raise ShapeMismatchError
+
+    if matrix.shape[1] != vector.size:
+        raise ShapeMismatchError
+
+    if not np.linalg.det(matrix):
+        return None, None
+
+    scalars = matrix @ vector
+    norms = np.linalg.norm(matrix, axis=1)
+    projections = matrix * scalars[..., np.newaxis] / norms[..., np.newaxis] ** 2
+    components = vector - projections
+    return projections, components
--- solutions/sem02/lesson05/task3.py
+++ solutions/sem02/lesson05/task3.py
@@ -9,4 +9,15 @@ def adaptive_filter(
     Vs: np.ndarray,
     Vj: np.ndarray,
     diag_A: np.ndarray,
-) -> np.ndarray: ...
+) -> np.ndarray:
+    if np.ndim(Vs) != 2 or np.ndim(Vj) != 2 or np.ndim(diag_A) != 1:
+        raise ShapeMismatchError
+
+    if Vj.shape[0] != Vs.shape[0] or Vj.shape[1] != diag_A.size:
+        raise ShapeMismatchError
+
+    A = np.diag(diag_A)
+    I_k = np.eye(diag_A.size)
+    Vj_H = (Vj.real - Vj.imag).T
+    R__1 = np.linalg.inv((I_k + Vj @ A @ Vj_H))
+    return R__1 @ Vs
