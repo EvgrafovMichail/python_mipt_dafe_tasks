2b1d4f901933b694dde83492d2724f2441f10e30
Robert <rob.ibr0407@gmail.com>
2026-03-16 23:03:05 +0300

    "Б12-513 Ибрагимов Роберт lesson05"

--- solutions/sem02/lesson05/task1.py
+++ solutions/sem02/lesson05/task1.py
@@ -9,4 +9,8 @@ def can_satisfy_demand(
     costs: np.ndarray,
     resource_amounts: np.ndarray,
     demand_expected: np.ndarray,
-) -> bool: ...
+) -> bool:
+    rows_count, column_count = costs.shape
+    if rows_count != resource_amounts.shape[0] or column_count != demand_expected.shape[0]:
+        raise ShapeMismatchError
+    return np.all(costs @ demand_expected <= resource_amounts)
