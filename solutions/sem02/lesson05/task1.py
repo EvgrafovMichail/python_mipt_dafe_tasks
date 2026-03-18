0e71eba7dfdd93d328ae1f3e80b0afd062227efc
gubanovily66-beep <gubanovily66@gmail.com>
2026-03-18 20:29:18 +0300

    les5

--- solutions/sem02/lesson05/task1.py
+++ solutions/sem02/lesson05/task1.py
@@ -9,4 +9,11 @@ def can_satisfy_demand(
     costs: np.ndarray,
     resource_amounts: np.ndarray,
     demand_expected: np.ndarray,
-) -> bool: ...
+) -> bool:
+    M, N = costs.shape
+    if len(resource_amounts) != M:
+        raise ShapeMismatchError
+    if len(demand_expected) != N:
+        raise ShapeMismatchError
+    amount_resources_needed = np.sum(costs * demand_expected, axis=1)
+    return all(amount_resources_needed <= resource_amounts)
