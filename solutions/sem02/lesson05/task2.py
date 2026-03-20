5f6d209a9ed7fbbca6c63581441745b16b4641c9
Vandam-create <beliakov.va@phystech.edu>
2026-03-20 21:46:47 +0300

    V1

--- solutions/sem02/lesson05/task2.py
+++ solutions/sem02/lesson05/task2.py
@@ -8,4 +8,19 @@ class ShapeMismatchError(Exception):
 def get_projections_components(
     matrix: np.ndarray,
     vector: np.ndarray,
-) -> tuple[np.ndarray | None, np.ndarray | None]: ...
+) -> tuple[np.ndarray | None, np.ndarray | None]:
+    if matrix.shape[0] != matrix.shape[1]:
+        raise ShapeMismatchError()
+    if matrix.shape[1] != vector.shape[0]:
+        raise ShapeMismatchError()
+
+    if abs(np.linalg.det(matrix)) < 1e-10:
+        return (None, None)
+
+    dot = matrix @ vector
+    norms_sq = np.sum(matrix * matrix, axis=1)
+    coeffs = dot / norms_sq
+    pr = coeffs[:, np.newaxis] * matrix
+    ort = vector - pr
+
+    return pr, ort
