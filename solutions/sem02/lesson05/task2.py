04178f9ad1c7be9e07d8fc2bf7008b663015231e
AntonLodygin <tonylodygin123@gmail.com>
2026-03-20 23:25:35 +0300

    hw-lesson05 solution

--- solutions/sem02/lesson05/task2.py
+++ solutions/sem02/lesson05/task2.py
@@ -8,4 +8,16 @@ class ShapeMismatchError(Exception):
 def get_projections_components(
     matrix: np.ndarray,
     vector: np.ndarray,
-) -> tuple[np.ndarray | None, np.ndarray | None]: ...
+) -> tuple[np.ndarray | None, np.ndarray | None]:
+    if matrix.shape[0] != matrix.shape[1] or matrix.shape[1] != vector.size:
+        raise ShapeMismatchError
+
+    if np.linalg.det(matrix) == 0:
+        return (None, None)
+
+    mult = matrix @ vector
+    length = np.sum(matrix**2, axis=1)
+    coeff = (mult / length)[:, np.newaxis]
+    projections = coeff * matrix
+
+    return (projections, vector - projections)
--- solutions/sem02/lesson05/task3.py
+++ solutions/sem02/lesson05/task3.py
@@ -9,4 +9,10 @@ def adaptive_filter(
     Vs: np.ndarray,
     Vj: np.ndarray,
     diag_A: np.ndarray,
-) -> np.ndarray: ...
+) -> np.ndarray:
+    if Vs.shape[0] != Vj.shape[0] or Vj.shape[1] != diag_A.shape[0]:
+        raise ShapeMismatchError
+
+    Vj_h = np.conj(Vj).transpose()
+    R_inv = np.linalg.inv(np.eye(Vj.shape[1]) + Vj_h @ Vj @ np.diag(diag_A))
+    return Vs - Vj @ (R_inv @ (Vj_h @ Vs))
