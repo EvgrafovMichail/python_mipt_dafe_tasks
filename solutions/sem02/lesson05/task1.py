da0fb619212ed7e607ae8b64cb65ec800e11d126
bulka0007 <bulat.ramazanov.2000@bk.ru>
2026-03-20 23:06:47 +0300

    Lesson05_tasks

--- solutions/sem02/lesson05/task1.py
+++ solutions/sem02/lesson05/task1.py
@@ -9,4 +9,15 @@ def can_satisfy_demand(
     costs: np.ndarray,
     resource_amounts: np.ndarray,
     demand_expected: np.ndarray,
-) -> bool: ...
+) -> bool:
+    if np.ndim(costs) != 2:
+        raise ShapeMismatchError
+    M, N = costs.shape
+    if np.ndim(resource_amounts) != 1 or resource_amounts.size != M:
+        raise ShapeMismatchError
+
+    if np.ndim(demand_expected) != 1 or demand_expected.size != N:
+        raise ShapeMismatchError
+
+    resources_needed = np.sum(costs * demand_expected, axis=1)
+    return all(resources_needed <= resource_amounts)
--- solutions/sem02/lesson05/task3.py
+++ solutions/sem02/lesson05/task3.py
@@ -9,4 +9,14 @@ def adaptive_filter(
     Vs: np.ndarray,
     Vj: np.ndarray,
     diag_A: np.ndarray,
-) -> np.ndarray: ...
+) -> np.ndarray:
+    if np.ndim(Vs) != 2 or np.ndim(Vj) != 2 or np.ndim(diag_A) != 1:
+        raise ShapeMismatchError
+
+    if Vj.shape[0] != Vs.shape[0] or Vj.shape[1] != diag_A.size:
+        raise ShapeMismatchError
+
+    A = np.diag(diag_A)
+    Vj_H = (Vj.real - 1j * Vj.imag).T
+    R_1 = np.linalg.inv((np.eye(diag_A.size) + Vj_H @ Vj @ A))
+    return Vs - Vj @ R_1 @ (Vj_H @ Vs)
