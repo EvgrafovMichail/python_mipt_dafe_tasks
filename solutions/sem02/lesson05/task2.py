4241383601b143181930ad7d13fafa9cc1f30b87
Oleg <oskolkov.web@gmail.com>
2026-03-20 22:16:31 +0300

    Сделал дз

--- solutions/sem02/lesson05/task2.py
+++ solutions/sem02/lesson05/task2.py
@@ -8,4 +8,18 @@ class ShapeMismatchError(Exception):
 def get_projections_components(
     matrix: np.ndarray,
     vector: np.ndarray,
-) -> tuple[np.ndarray | None, np.ndarray | None]: ...
+) -> tuple[np.ndarray | None, np.ndarray | None]:
+    if matrix.ndim != 2 or matrix.shape[0] != matrix.shape[1] or matrix.shape[1] != vector.shape[0]:
+        raise ShapeMismatchError
+
+    if np.linalg.matrix_rank(matrix) < matrix.shape[0]:
+        return None, None
+
+    dots = matrix @ vector
+    norms = np.sum(matrix**2, axis=1)
+    sc = dots / norms
+
+    proj = sc[:, np.newaxis] * matrix
+    ort = vector[np.newaxis, :] - proj
+
+    return proj, ort
--- solutions/sem02/lesson05/task3.py
+++ solutions/sem02/lesson05/task3.py
@@ -9,4 +9,22 @@ def adaptive_filter(
     Vs: np.ndarray,
     Vj: np.ndarray,
     diag_A: np.ndarray,
-) -> np.ndarray: ...
+) -> np.ndarray:
+    if (
+        Vs.ndim != 2
+        or Vj.ndim != 2
+        or diag_A.ndim != 1
+        or Vs.shape[0] != Vj.shape[0]
+        or Vj.shape[1] != diag_A.shape[0]
+    ):
+        raise ShapeMismatchError
+
+    K = Vj.shape[1]
+
+    VjH = Vj.conj().T
+    A = np.diag(diag_A)
+
+    E = np.eye(K, dtype=Vj.dtype)
+
+    inner = E + VjH @ Vj @ A
+    return Vs - Vj @ np.linalg.inv(inner) @ (VjH @ Vs)
