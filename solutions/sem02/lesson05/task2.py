071b03061af1b13f359472b9ee5916cfb31b26d4
Игорь Кодик <kodik.ia@phystech.edu>
2026-03-20 22:50:24 +0300

    done tasks lesson05

--- solutions/sem02/lesson05/task2.py
+++ solutions/sem02/lesson05/task2.py
@@ -8,4 +8,13 @@ class ShapeMismatchError(Exception):
 def get_projections_components(
     matrix: np.ndarray,
     vector: np.ndarray,
-) -> tuple[np.ndarray | None, np.ndarray | None]: ...
+) -> tuple[np.ndarray | None, np.ndarray | None]:
+    if (matrix.shape[0] != matrix.shape[1]) or (matrix.shape[1] != vector.shape[0]):
+        raise ShapeMismatchError
+
+    if np.linalg.matrix_rank(matrix) != matrix.shape[0]:
+        return None, None
+
+    proekziya = ((matrix @ vector) / (np.linalg.norm(matrix, axis=1) ** 2))[:, np.newaxis] * matrix
+
+    return proekziya, vector - proekziya
