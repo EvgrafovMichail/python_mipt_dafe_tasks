198f9886bb4b2e190541a71ec9b80d160afb3351
kiritonator <kir.shishkin.shkolnik@yandex.ru>
2026-03-20 21:10:17 +0300

    useless commit to fix merge problems

--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -4,7 +4,4 @@ import numpy as np
 def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
-) -> tuple[np.uint8, float]:
-    if threshold < 1:
-        raise ValueError()
-
+) -> tuple[np.uint8, float]: ...
