a1e00ee4f01aabab8782237fdcf3cab4f22ba193
Profizmatik <undel.xo17@gmail.com>
2026-03-20 18:43:56 +0300

    урок 5

--- solutions/sem02/lesson05/task1.py
+++ solutions/sem02/lesson05/task1.py
@@ -9,4 +9,15 @@ def can_satisfy_demand(
     costs: np.ndarray,
     resource_amounts: np.ndarray,
     demand_expected: np.ndarray,
-) -> bool: ...
+) -> bool:
+    M, N = costs.shape
+    if resource_amounts.shape[0] != M or demand_expected.shape[0] != N:
+        raise ShapeMismatchError
+    
+    for i in range(M):
+        schet = 0
+        for j in range(N):
+            schet += costs[i, j] * demand_expected[j]
+        if schet > resource_amounts[i]:
+            return False
+    return True
--- solutions/sem02/lesson05/task2.py
+++ solutions/sem02/lesson05/task2.py
@@ -8,4 +8,20 @@ class ShapeMismatchError(Exception):
 def get_projections_components(
     matrix: np.ndarray,
     vector: np.ndarray,
-) -> tuple[np.ndarray | None, np.ndarray | None]: ...
+) -> tuple[np.ndarray | None, np.ndarray | None]:
+    m, n = matrix.shape
+    if m != n:
+        raise ShapeMismatchError
+    if m != vector.shape[0]:
+        raise ShapeMismatchError
+    proections = []
+    ortog_sost = []
+    if np.linalg.det(matrix) == 0:
+        return None, None
+    else:
+        for i in range(m):
+            module = np.sum((matrix[i]) ** 2)
+            pr_a_basis = (matrix[i] @ vector / module) * matrix[i]
+            proections.append(pr_a_basis)
+            ortog_sost.append(vector - pr_a_basis)
+        return np.array(proections), np.array(ortog_sost)
