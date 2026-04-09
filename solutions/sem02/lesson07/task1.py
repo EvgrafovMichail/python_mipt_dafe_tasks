50e714b58ed9b63efbbf8e8e38b8814bade9c0b6
danberx <danberx490@gmail.com>
2026-04-09 22:01:26 +0300

    lesson07 tasks

--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -13,8 +13,86 @@ def visualize_diagrams(
     ordinates: np.ndarray,
     diagram_type: Any,
 ) -> None:
-    # ваш код
-    pass
+    if abscissa.shape != ordinates.shape:
+        raise ShapeMismatchError
+
+    if diagram_type not in ["hist", "violin", "box"]:
+        raise ValueError
+
+    figure = plt.figure(figsize=(8, 8))
+    grid = plt.GridSpec(4, 4, wspace=space, hspace=space)
+
+    axis_scatter = figure.add_subplot(grid[:-1, 1:])
+    axis_vert = figure.add_subplot(
+        grid[:-1, 0],
+        sharey=axis_scatter,
+    )
+    axis_hor = figure.add_subplot(
+        grid[-1, 1:],
+        sharex=axis_scatter,
+    )
+
+    axis_scatter.scatter(abscissa, ordinates, color="mediumseagreen", alpha=0.35)
+    axis_scatter.set_title("Distribution", fontsize=17, fontweight="bold", c="dimgray")
+
+    if diagram_type == "hist":
+        axis_hor.hist(
+            abscissa,
+            bins=50,
+            color="aquamarine",
+            density=True,
+            alpha=0.7,
+        )
+        axis_vert.hist(
+            ordinates,
+            bins=50,
+            color="aquamarine",
+            orientation="horizontal",
+            density=True,
+            alpha=0.7,
+        )
+        axis_hor.invert_yaxis()
+        axis_vert.invert_xaxis()
+
+    elif diagram_type == "violin":
+        violin_parts_vert = axis_vert.violinplot(abscissa, vert=True, showmedians=True)
+        violin_parts_hor = axis_hor.violinplot(ordinates, vert=False, showmedians=True)
+        for body in violin_parts_vert["bodies"]:
+            body.set_facecolor("mediumseagreen")
+            body.set_edgecolor("aquamarine")
+
+        for part in violin_parts_vert:
+            if part == "bodies":
+                continue
+
+            violin_parts_vert[part].set_edgecolor("mediumseagreen")
+
+        for body in violin_parts_hor["bodies"]:
+            body.set_facecolor("mediumseagreen")
+            body.set_edgecolor("aquamarine")
+
+        for part in violin_parts_hor:
+            if part == "bodies":
+                continue
+
+            violin_parts_hor[part].set_edgecolor("mediumseagreen")
+
+    elif diagram_type == "box":
+        axis_vert.boxplot(
+            abscissa,
+            vert=True,
+            patch_artist=True,
+            boxprops=dict(facecolor="mediumseagreen", alpha=0.7),
+        )
+        axis_hor.boxplot(
+            ordinates,
+            vert=False,
+            patch_artist=True,
+            boxprops=dict(facecolor="mediumseagreen", alpha=0.7),
+        )
+
+    axis_vert.set_ylabel("Vertical", fontsize=17, fontweight="bold", c="dimgray")
+    axis_hor.set_xlabel("Horizontal", fontsize=17, fontweight="bold", c="dimgray")
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson07/task2.py
+++ solutions/sem02/lesson07/task2.py
@@ -1,1 +1,64 @@
-# ваш код (используйте функции или классы для решения данной задачи)
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def read_from_json(file_name: str) -> tuple[list[int], list[int]]:
+    with open(file_name, "r") as file:
+        data = json.load(file)
+
+    before = data["before"]
+    after = data["after"]
+
+    return before, after
+
+
+def get_counts_of_groups(before: list[int], after: list[int]) -> tuple[np.ndarray, np.ndarray]:
+    labels_before, counts_before = np.unique(np.array(before), return_counts=True)
+    labels_after, counts_after = np.unique(np.array(after), return_counts=True)
+
+    return counts_before, counts_after
+
+
+def visualize_diagrams(counts_before: np.ndarray, counts_after: np.ndarray):
+    figure, axis = plt.subplots(figsize=(9, 9))
+    axis: plt.Axes
+
+    labels = np.array(["I", "II", "III", "IV"])
+
+    bar1 = axis.bar(
+        np.arange(counts_before.size) - 0.2,
+        counts_before,
+        color="peachpuff",
+        edgecolor="orange",
+        width=0.4,
+    )
+    bar2 = axis.bar(
+        np.arange(counts_before.size) + 0.2,
+        counts_after,
+        color="plum",
+        edgecolor="purple",
+        width=0.4,
+    )
+    axis.set_xticks(
+        np.arange(labels.size),
+        labels=labels,
+        weight="bold",
+    )
+    axis.tick_params(axis="x", labelsize=14, labelcolor="dimgray")
+
+    axis.set_title("Mitral disease stages", fontsize=17, fontweight="bold", c="black")
+    axis.set_xlabel("Groups", fontsize=14, fontweight="bold", c="dimgray")
+    axis.set_ylabel("Amount of people", fontsize=14, fontweight="bold", c="dimgray")
+    axis.legend((bar1, bar2), ["before", "after"])
+
+    figure.savefig("Mitral disease stages.png", bbox_inches="tight")
+
+
+if __name__ == "__main__":
+    data = read_from_json("data/medic_data.json")
+    counts = get_counts_of_groups(*data)
+    visualize_diagrams(*counts)
+
+    plt.show()
