378cbfd385ce33864d550e25b6a6b4bd2d7f8688
Vera <verakondurina@gmail.com>
2026-04-10 23:55:17 +0300

    Homework diagramms

--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -3,9 +3,60 @@ from typing import Any
 import matplotlib.pyplot as plt
 import numpy as np
 
+plt.style.use("ggplot")
+
 
 class ShapeMismatchError(Exception):
     pass
+
+
+def apply_custom_style(ax):
+    ax.set_facecolor("#F0F0F0")
+    ax.grid(True, color="#6E6E6E", linestyle="-", linewidth=0.5)
+    ax.set_axisbelow(True)
+
+
+def draw_hist(fig, grid, labels, colors):
+    ax = fig.add_subplot(grid[:-1, 1:])
+    apply_custom_style(ax)
+    ax.hist(abscissa, bins=50, alpha=0.5, label=labels[0], color=colors[0])
+    ax.hist(ordinates, bins=50, alpha=0.5, label=labels[1], color=colors[1])
+    ax.set_title("Гистограмма")
+    ax.legend()
+
+
+def draw_violin(fig, grid, labels, colors):
+    ax = fig.add_subplot(grid[:-1, 0])
+    apply_custom_style(ax)
+    violin_parts = ax.violinplot([abscissa, ordinates], vert=True, showmedians=True)
+
+    for i, body in enumerate(violin_parts["bodies"]):
+        body.set_facecolor(colors[i])
+        body.set_edgecolor(colors[i])
+        body.set_alpha(0.6)
+
+    for part_name in violin_parts:
+        if part_name != "bodies":
+            violin_parts[part_name].set_edgecolor("black")
+            violin_parts[part_name].set_linewidth(1.5)
+
+    violin_parts["cmedians"].set_linewidth(2.5)
+    ax.set_xticks([1, 2])
+    ax.set_xticklabels(labels)
+    ax.set_title("Скрипичная диаграмма")
+
+
+def draw_box(fig, grid, labels, colors):
+    ax = fig.add_subplot(grid[-1, 1:])
+    apply_custom_style(ax)
+    box_parts = ax.boxplot(
+        [abscissa, ordinates], labels=labels, vert=False, patch_artist=True, notch=True
+    )
+
+    for patch, color in zip(box_parts["boxes"], colors):
+        patch.set_facecolor(color)
+        patch.set_alpha(0.7)
+    ax.set_title("Ящик с усами")
 
 
 def visualize_diagrams(
@@ -13,16 +64,36 @@ def visualize_diagrams(
     ordinates: np.ndarray,
     diagram_type: Any,
 ) -> None:
-    # ваш код
-    pass
+    if abscissa.shape != ordinates.shape:
+        raise ShapeMismatchError
+
+    if diagram_type not in ["hist", "violin", "box", "all"]:
+        raise ValueError
+
+    colors = ["#0A29C2", "#EAC90F"]
+    labels = ["X data", "Y data"]
+    space = 0.4
+
+    fig = plt.figure(figsize=(12, 10))
+    grid = plt.GridSpec(4, 4, wspace=space, hspace=2 * space, height_ratios=[1, 1, 1, 1.5])
+
+    types_to_draw = ["hist", "violin", "box"] if diagram_type == "all" else [diagram_type]
+
+    for d_type in types_to_draw:
+        if d_type == "hist":
+            draw_hist(fig, grid, labels, colors)
+
+        if d_type == "violin":
+            draw_violin(fig, grid, labels, colors)
+
+        if d_type == "box":
+            draw_box(fig, grid, labels, colors)
 
 
 if __name__ == "__main__":
     mean = [2, 3]
     cov = [[1, 1], [1, 2]]
-    space = 0.2
-
     abscissa, ordinates = np.random.multivariate_normal(mean, cov, size=1000).T
 
-    visualize_diagrams(abscissa, ordinates, "hist")
+    visualize_diagrams(abscissa, ordinates, "all")
     plt.show()
--- solutions/sem02/lesson07/task2.py
+++ solutions/sem02/lesson07/task2.py
@@ -1,1 +1,51 @@
-# ваш код (используйте функции или классы для решения данной задачи)
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use("ggplot")
+
+
+def apply_custom_style(ax):
+    ax.set_facecolor("#F0F0F0")
+    ax.grid(True, color="#A5A5A51B", linestyle="-", linewidth=0.5)
+    ax.set_axisbelow(True)
+
+
+def draw_mitral_diagram(ax: plt.Axes, categories: list[str], before: list[int], after: list[int]):
+    x = np.arange(len(categories))
+    width = 0.4
+    colors = ["#F772C6", "#1CEED2", "#080808"]
+    labels = ["До установки импланта", "После установки импланта"]
+    apply_custom_style(ax)
+
+    column1 = ax.bar(x - width / 2, before, width, label=labels[0], color=colors[0])
+    column2 = ax.bar(x + width / 2, after, width, label=labels[1], color=colors[1])
+
+    ax.set_title("Эффективность импланта при митральной недостаточности")
+    ax.set_ylabel("Количество пациентов", color=colors[2])
+    ax.set_xticks(x)
+    ax.set_xticklabels(categories)
+    ax.legend()
+
+    ax.bar_label(column1)
+    ax.bar_label(column2)
+
+
+def visualize_medic_data(f_path: str):
+    with open(f_path, "r") as f:
+        data = json.load(f)
+
+    deg_mitral_reg = ["I", "II", "III", "IV"]
+    before_impl_ins = [data["before"].count(i) for i in deg_mitral_reg]
+    after_impl_ins = [data["after"].count(i) for i in deg_mitral_reg]
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    draw_mitral_diagram(ax, deg_mitral_reg, before_impl_ins, after_impl_ins)
+
+    fig.savefig("solutions/sem02/lesson07/medic_hist.png", bbox_inches="tight")
+    plt.show()
+
+
+if __name__ == "__main__":
+    visualize_medic_data("solutions/sem02/lesson07/data/medic_data.json")
