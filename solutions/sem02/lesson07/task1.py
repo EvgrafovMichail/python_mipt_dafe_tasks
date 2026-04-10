c220cd9373cbbabe3ed757da9dec315f89a62bbc
Ssssserge <yanchevskiy07@gmail.com>
2026-04-11 02:08:22 +0300

    7 контест

--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -13,8 +13,35 @@ def visualize_diagrams(
     ordinates: np.ndarray,
     diagram_type: Any,
 ) -> None:
-    # ваш код
-    pass
+    if abscissa.shape != ordinates.shape:
+        raise ShapeMismatchError
+
+    plt.style.use("ggplot")
+    figure = plt.figure(figsize=(12, 10))
+    grid = plt.GridSpec(4, 4, wspace=0.2, hspace=0.2)
+
+    axis_scatter = figure.add_subplot(grid[:-1, 1:])
+    axis_y = figure.add_subplot(grid[:-1, 0], sharey=axis_scatter)
+    axis_x = figure.add_subplot(grid[-1, 1:], sharex=axis_scatter)
+
+    axis_scatter.scatter(abscissa, ordinates, color="cornflowerblue", alpha=0.5)
+
+    match diagram_type:
+
+        case "hist":
+            axis_x.hist(abscissa, bins = 112, alpha = 0.5, color = 'b')
+            axis_y.hist(ordinates, orientation="horizontal", bins = 112, alpha = 0.5, color = 'b')
+
+        case "box":
+            axis_x.boxplot(abscissa, orientation = "horizontal")
+            axis_y.boxplot(ordinates)
+
+        case "violin":
+            axis_x.violinplot(abscissa, orientation="horizontal")
+            axis_y.violinplot(ordinates)
+
+        case _:
+            raise ValueError
 
 
 if __name__ == "__main__":
@@ -24,5 +51,6 @@ if __name__ == "__main__":
 
     abscissa, ordinates = np.random.multivariate_normal(mean, cov, size=1000).T
 
-    visualize_diagrams(abscissa, ordinates, "hist")
+    visualize_diagrams(abscissa, ordinates, "violin")
     plt.show()
+print("done")
--- solutions/sem02/lesson07/task2.py
+++ solutions/sem02/lesson07/task2.py
@@ -1,1 +1,51 @@
 # ваш код (используйте функции или классы для решения данной задачи)
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+file_path = "solutions/sem02/lesson07/data/medic_data.json"
+
+def medical_plot_func():
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        before = data['before']
+        after = data['after']
+    before_dist = np.zeros(4, dtype = int)
+    after_dist = np.zeros(4, dtype = int)
+    for roman in before:
+        match roman:
+            case 'I':
+                before_dist[0] += 1
+            case 'II':
+                before_dist[1] += 1
+            case 'III':
+                before_dist[2] += 1
+            case 'IV':
+                before_dist[3] += 1
+
+    for roman in after:                        
+        match roman:
+            case 'I':
+                after_dist[0] += 1
+            case 'II':
+                after_dist[1] += 1
+            case 'III':
+                after_dist[2] += 1
+            case 'IV':
+                after_dist[3] += 1
+
+
+    degrees = ['I', 'II', 'III', 'IV']
+    x = np.arange(len(degrees))
+    width = 0.35
+
+    plt.style.use("ggplot")
+    figure, ax = plt.subplots(figsize = (16, 9))
+    ax.bar(x - width/2, before_dist, width, label='До', color='red')
+    ax.bar(x + width/2, after_dist, width, label='После', color='blue')
+    ax.set_xticks(x)
+    ax.set_xticklabels(degrees)
+    ax.legend()
+    
+    plt.savefig('distribution.png')
+    plt.show()
+           
