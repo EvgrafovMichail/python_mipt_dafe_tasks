66fb311dfeacf5b86af62b93ff15508b14e176b8
bulka0007 <bulat.ramazanov.2000@bk.ru>
2026-04-17 23:42:55 +0300

    lesson08_tasks(r)

--- solutions/sem02/lesson08/task1.py
+++ solutions/sem02/lesson08/task1.py
@@ -7,21 +7,11 @@ from matplotlib.animation import FuncAnimation
 
 
 def create_modulation_animation(
-    modulation, 
-    fc, 
-    num_frames, 
-    plot_duration, 
-    time_step=0.001, 
-    animation_step=0.01,
-    save_path=""
+    modulation, fc, num_frames, plot_duration, time_step=0.001, animation_step=0.01, save_path=""
 ) -> FuncAnimation:
     def update_frame(
-            frame_number,
-            *,
-            line: plt.Line2D,
-            axis: plt.Axes,
-            abscissa: np.ndarray
-            ) -> tuple[plt.Line2D, plt.Axes]:
+        frame_number, *, line: plt.Line2D, axis: plt.Axes, abscissa: np.ndarray
+    ) -> tuple[plt.Line2D, plt.Axes]:
         t_start = frame_number * time_step
         t_end = t_start + plot_duration
         abscissa = np.linspace(t_start, t_end, int(plot_duration / time_step))
@@ -29,6 +19,7 @@ def create_modulation_animation(
         line.set_data(abscissa, ordinate)
         axis.set_xlim(t_start, t_end)
         return line, axis
+
     abscissa = np.linspace(0, plot_duration, int(plot_duration / time_step))
     figure, axis = plt.subplots()
     axis.set_xlabel("Время, с")
@@ -36,29 +27,28 @@ def create_modulation_animation(
     axis.set_title("Модулированный сигнал")
     axis.set_xlim(0, plot_duration)
     axis.set_ylim(min(modulation(abscissa)) - 1, max(modulation(abscissa)) + 1)
-    line, = axis.plot(
-        abscissa,
-        modulation(abscissa) * np.sin(2 * np.pi * fc * abscissa)
-        )
+    (line,) = axis.plot(abscissa, modulation(abscissa) * np.sin(2 * np.pi * fc * abscissa))
     animation = FuncAnimation(
         figure,
         partial(update_frame, line=line, axis=axis, abscissa=abscissa),
         frames=num_frames,
-        interval=animation_step * 1000
+        interval=animation_step * 1000,
     )
     if save_path != "":
         animation.save(save_path, writer="pillow", fps=24)
     return animation
 
-if __name__ == "__main__":
-    def modulation_function(t):
-        return np.cos(t * 6) 
 
-    num_frames = 100  
-    plot_duration = np.pi / 2 
-    time_step = 0.001  
-    animation_step = np.pi / 200 
-    fc = 50  
+if __name__ == "__main__":
+
+    def modulation_function(t):
+        return np.cos(t * 6)
+
+    num_frames = 100
+    plot_duration = np.pi / 2
+    time_step = 0.001
+    animation_step = np.pi / 200
+    fc = 50
     save_path_with_modulation = "modulated_signal.gif"
 
     animation = create_modulation_animation(
@@ -68,6 +58,6 @@ if __name__ == "__main__":
         plot_duration=plot_duration,
         time_step=time_step,
         animation_step=animation_step,
-        save_path=save_path_with_modulation
+        save_path=save_path_with_modulation,
     )
     HTML(animation.to_jshtml())
--- solutions/sem02/lesson08/task2.py
+++ solutions/sem02/lesson08/task2.py
@@ -14,21 +14,25 @@ def create_mesh(maze: np.ndarray, axis: plt.Axes):
     for i in range(vline_count):
         axis.axvline(i, color="black")
 
+
 def get_neighbors(maze: np.ndarray, current: tuple[int, int]):
     neighbors = []
-    for x,y in [(0,1), (0,-1), (1,0), (-1,0)]:
-        if 0 <= current[0] + x < maze.shape[0] \
-            and 0 <= current[1] + y < maze.shape[1]\
-            and maze[current[0] + x][current[1] + y] == 1:
+    for x, y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        if (
+            0 <= current[0] + x < maze.shape[0]
+            and 0 <= current[1] + y < maze.shape[1]
+            and maze[current[0] + x][current[1] + y] == 1
+        ):
             neighbors.append((current[0] + x, current[1] + y))
     return neighbors
 
-def draw_circle(length: int, color: str,position: tuple[int, int], axis: plt.Axes):
+
+def draw_circle(length: int, color: str, position: tuple[int, int], axis: plt.Axes):
     center_x = position[0] + 0.5
     center_y = position[1] + 0.5
     axis.add_patch(plt.Circle((center_x, center_y), 0.3, color=color))
-    axis.text(center_x, center_y, str(length), color='white',
-              ha='center', va='center', fontsize=12)
+    axis.text(center_x, center_y, str(length), color="white", ha="center", va="center", fontsize=12)
+
 
 def count_steps(maze: np.ndarray, start: tuple[int, int], end: tuple[int, int]):
     count = 1
@@ -45,52 +49,53 @@ def count_steps(maze: np.ndarray, start: tuple[int, int], end: tuple[int, int]):
         count += 1
     return count
 
-def bfs(frame_number: int,
-        *,
-        queue: list[tuple[int, int]],
-        distance: dict[tuple[int, int], int],
-        maze: np.ndarray,
-        end: tuple[int, int],
-        axis: plt.Axes) -> plt.Axes:
+
+def bfs(
+    frame_number: int,
+    *,
+    queue: list[tuple[int, int]],
+    distance: dict[tuple[int, int], int],
+    maze: np.ndarray,
+    end: tuple[int, int],
+    axis: plt.Axes,
+) -> plt.Axes:
     if queue:
         current = queue.pop(0)
         if current == end:
-            draw_circle(distance[current], 'green', current, axis)
+            draw_circle(distance[current], "green", current, axis)
             return axis
         for neighbor in get_neighbors(maze, current):
             if neighbor not in distance:
                 distance[neighbor] = distance[current] + 1
                 queue.append(neighbor)
         if distance[current] != 0:
-            draw_circle(distance[current], 'red', current, axis)
+            draw_circle(distance[current], "red", current, axis)
         return axis
     return axis
 
+
 def animate_wave_algorithm(
-    maze: np.ndarray, 
-    start: tuple[int, int], 
-    end: tuple[int, int], 
-    save_path: str = ""
-)  -> FuncAnimation:
+    maze: np.ndarray, start: tuple[int, int], end: tuple[int, int], save_path: str = ""
+) -> FuncAnimation:
     figure, axis = plt.subplots()
     create_mesh(maze, axis)
     axis.set_title("Algorithm Li")
     for col in range(maze.shape[0]):
         for row in range(maze.shape[1]):
             if maze[col][row] == 0:
-                axis.add_patch(Rectangle((col, row), 1, 1, facecolor='black', edgecolor='none'))
+                axis.add_patch(Rectangle((col, row), 1, 1, facecolor="black", edgecolor="none"))
 
     queue = [start]
     distance = {start: 0}
 
     axis.set_xlim(0, maze.shape[0])
     axis.set_ylim(0, maze.shape[1])
-    axis.set_aspect('equal')
+    axis.set_aspect("equal")
     axis.grid(False)
-    
+
     animation = FuncAnimation(
-        fig=figure, 
-        func=partial(bfs, maze=maze, queue=queue, distance=distance, end=end, axis=axis), 
+        fig=figure,
+        func=partial(bfs, maze=maze, queue=queue, distance=distance, end=end, axis=axis),
         frames=count_steps(maze, start, end),
         interval=100,
     )
@@ -98,17 +103,20 @@ def animate_wave_algorithm(
         animation.save(save_path, writer="pillow", fps=10)
     return animation
 
+
 if __name__ == "__main__":
     # Пример 1
-    maze = np.array([
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 1, 0],
-        [1, 1, 0, 1, 0, 1, 0],
-        [0, 0, 1, 1, 0, 1, 0],
-        [0, 0, 0, 0, 0, 1, 0],
-        [1, 1, 1, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-    ])
+    maze = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 1, 1, 0],
+            [1, 1, 0, 1, 0, 1, 0],
+            [0, 0, 1, 1, 0, 1, 0],
+            [0, 0, 0, 0, 0, 1, 0],
+            [1, 1, 1, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
 
     start = (2, 0)
     end = (5, 0)
@@ -116,9 +124,9 @@ if __name__ == "__main__":
 
     animation = animate_wave_algorithm(maze, start, end, save_path)
     HTML(animation.to_jshtml())
-    
+
     # Пример 2
-    
+
     maze_path = "./data/maze.npy"
     loaded_maze = np.load(maze_path)
 
@@ -129,5 +137,3 @@ if __name__ == "__main__":
 
     loaded_animation = animate_wave_algorithm(loaded_maze, start, end, loaded_save_path)
     HTML(loaded_animation.to_jshtml())
-    
-    
