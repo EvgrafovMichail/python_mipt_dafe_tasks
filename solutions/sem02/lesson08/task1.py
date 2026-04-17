5ece644ca83336074a21269401a0cb964b928b59
Vera <verakondurina@gmail.com>
2026-04-18 00:12:51 +0300

    Homework animation

--- solutions/sem02/lesson08/task1.py
+++ solutions/sem02/lesson08/task1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from IPython.display import HTML
 from matplotlib.animation import FuncAnimation
 
@@ -6,8 +7,41 @@ from matplotlib.animation import FuncAnimation
 def create_modulation_animation(
     modulation, fc, num_frames, plot_duration, time_step=0.001, animation_step=0.01, save_path=""
 ) -> FuncAnimation:
-    # ваш код
-    return FuncAnimation()
+    fig, ax = plt.subplots()
+    line, = ax.plot([], [], lw=1, color="#058D29")
+    
+    ax.set_xlim(0, plot_duration)
+    ax.set_ylim(-1.5, 1.5)
+    ax.set_xlabel("Время (с)")
+    ax.set_ylabel("Амплитуда")
+    ax.set_title("Амплитудная модуляция" if modulation else "Несущий сигнал")
+    ax.grid(True)
+
+    def update(frame):
+        start_time = frame * animation_step
+        end_time = start_time + plot_duration
+        
+        t = np.arange(start_time, end_time, time_step)
+        carrier = np.sin(2 * np.pi * fc * t)
+        
+        if modulation is not None:
+            m_t = modulation(t)
+            signal = m_t * carrier
+        else:
+            signal = carrier
+            
+        line.set_data(t, signal)
+        ax.set_xlim(start_time, end_time)
+        
+        return line,
+
+    anim = FuncAnimation(fig, update, frames=num_frames, interval=50, blit=True)
+
+    if save_path:
+        anim.save(save_path, writer='pillow')
+    
+    plt.close(fig) 
+    return anim
 
 
 if __name__ == "__main__":
@@ -20,7 +54,7 @@ if __name__ == "__main__":
     time_step = 0.001
     animation_step = np.pi / 200
     fc = 50
-    save_path_with_modulation = "modulated_signal.gif"
+    save_path_with_modulation = "solutions/sem02/lesson08/modulated_signal.gif"
 
     animation = create_modulation_animation(
         modulation=modulation_function,
--- solutions/sem02/lesson08/task2.py
+++ solutions/sem02/lesson08/task2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from IPython.display import HTML
 from matplotlib.animation import FuncAnimation
 
@@ -6,9 +7,79 @@ from matplotlib.animation import FuncAnimation
 def animate_wave_algorithm(
     maze: np.ndarray, start: tuple[int, int], end: tuple[int, int], save_path: str = ""
 ) -> FuncAnimation:
-    # ваш код
-    return FuncAnimation()
+    rows, cols = maze.shape
+    grid = maze.copy()
+    grid[:] = -1
+    grid[start] = 0
+    current = 0
+    found = False
+    frames = []
+    frames.append(grid.copy())
+    
+    while np.any(current == grid):
+        front = (current == grid)
+        
+        if grid[end] != -1:
+            found = True
+            break
+        
+        neighbors = np.zeros(shape = front.shape, dtype=front.dtype)
+        neighbors[1:, :] |= front[:-1, :] 
+        neighbors[:-1, :] |= front[1:, :] 
+        neighbors[:, 1:] |= front[:, :-1] 
+        neighbors[:, :-1] |= front[:, 1:] 
+        
+        new_point = neighbors & (maze == 1) & (grid == -1)
+        
+        grid[new_point] = current + 1
+        frames.append(grid.copy())
+        
+        current += 1
+        
+    if not found:
+        print("Пути не существует")
+        return 0
+    
+    fig, ax = plt.subplots(figsize=(rows, cols))
+    
+    def prepare_data(data):
+        display = data.astype(float)
+        display[maze == 0] = -10
+        display[(maze == 1) & (data == -1)] = -5
+        return display
 
+    im = ax.imshow(prepare_data(frames[0]), cmap='viridis')
+    
+    ax.set_xticks(np.arange(-.5, cols, 1), minor=True)
+    ax.set_yticks(np.arange(-.5, rows, 1), minor=True)
+    ax.grid(which='minor', color="#FFF200FF", linestyle='-', linewidth=0.5)
+
+    text_objects = [[ax.text(j, i, "", ha="center", va="center", fontsize=12, fontweight='bold') 
+                     for j in range(cols)] for i in range(rows)]
+
+    def update(frame_idx):
+        data = frames[frame_idx]
+        im.set_array(prepare_data(data))
+
+        for i in range(rows):
+            for j in range(cols):
+                val = data[i, j]
+                if val >= 0:
+                    text_objects[i][j].set_text(str(val))
+                    text_objects[i][j].set_color("black")
+                else:
+                    text_objects[i][j].set_text("")
+        return [im]
+
+   
+    ani = FuncAnimation(fig, update, frames=len(frames), interval=150, blit=False)
+
+    if save_path:
+        ani.save(save_path, writer='pillow')
+    
+    # plt.show()
+    return ani
+   
 
 if __name__ == "__main__":
     # Пример 1
@@ -26,20 +97,20 @@ if __name__ == "__main__":
 
     start = (2, 0)
     end = (5, 0)
-    save_path = "labyrinth.gif"  # Укажите путь для сохранения анимации
+    save_path = "solutions/sem02/lesson08/labyrinth.gif"  # Укажите путь для сохранения анимации
 
     animation = animate_wave_algorithm(maze, start, end, save_path)
     HTML(animation.to_jshtml())
 
     # Пример 2
 
-    maze_path = "./data/maze.npy"
+    maze_path = "solutions/sem02/lesson08/data/maze.npy"
     loaded_maze = np.load(maze_path)
 
     # можете поменять, если захотите запустить из других точек
     start = (2, 0)
     end = (5, 0)
-    loaded_save_path = "loaded_labyrinth.gif"
+    loaded_save_path = "solutions/sem02/lesson08/loaded_labyrinth.gif"
 
     loaded_animation = animate_wave_algorithm(loaded_maze, start, end, loaded_save_path)
     HTML(loaded_animation.to_jshtml())
